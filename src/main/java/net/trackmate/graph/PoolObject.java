ff3175ed8e7c4b2abd32e430cf99a5191d529722
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2014-09-28 06:34:59 +0200

    add javadoc

--- src/main/java/net/trackmate/graph/PoolObject.java
+++ src/main/java/net/trackmate/graph/PoolObject.java
@@ -1,9 +1,20 @@
 package net.trackmate.graph;
 
+import net.trackmate.graph.mempool.ByteMappedElement;
 import net.trackmate.graph.mempool.MappedElement;
 import net.trackmate.graph.mempool.MemPool;
 
-
+/**
+ * A proxy object that uses a {@link MappedElement} access to store its data in
+ * a {@link MemPool}. The data block that it references to can be set by
+ * {@link #updateAccess(MemPool, int)}. Methods to modify the data itself are
+ * defined in subclasses.
+ *
+ * @param <T>
+ *            the MappedElement type, for example {@link ByteMappedElement}.
+ *
+ * @author Tobias Pietzsch <tobias.pietzsch@gmail.com>
+ */
 public abstract class PoolObject< T extends MappedElement >
 {
 	protected final T access;
