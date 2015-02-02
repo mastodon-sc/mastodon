9de4e1f449256c5678d32b016df831fd83961125
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-02-02 17:14:36 +0100

    Expose the vertex pool of the TrackScheme graph.
    So that I can at least play with it as a test object.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/net/trackmate/trackscheme/TrackSchemeGraph.java
+++ src/main/java/net/trackmate/trackscheme/TrackSchemeGraph.java
@@ -145,7 +145,7 @@ public class TrackSchemeGraph extends GraphImp< TrackSchemeGraph.TrackSchemeVert
 		return sb.toString();
 	}
 
-	protected TrackSchemeVertexPool getVertexPool()
+	public TrackSchemeVertexPool getVertexPool()
 	{
 		return vertexPool;
 	}
