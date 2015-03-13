d948edca4339943ac971fcfe76bced50b4936fb2
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-03-13 16:15:38 +0100

    let PoolObjectCollection extend RefCollection

--- src/main/java/net/trackmate/graph/PoolObjectCollection.java
+++ src/main/java/net/trackmate/graph/PoolObjectCollection.java
@@ -4,9 +4,10 @@ import gnu.trove.TIntCollection;
 
 import java.util.Collection;
 
+import net.trackmate.graph.collection.RefCollection;
 import net.trackmate.graph.mempool.MappedElement;
 
-public interface PoolObjectCollection< O extends PoolObject< O, T >, T extends MappedElement > extends Collection< O >
+public interface PoolObjectCollection< O extends PoolObject< O, T >, T extends MappedElement > extends RefCollection< O >
 {
 	public TIntCollection getIndexCollection();
 }
