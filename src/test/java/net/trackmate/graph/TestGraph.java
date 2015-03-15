407ba60e4aa032ce62f7778ddcbc79301993e650
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-03-16 00:35:11 +0100

    add TestGraph for building graphs in unit tests

---
+++ src/test/java/net/trackmate/graph/TestGraph.java
@@ -0,0 +1,16 @@
+package net.trackmate.graph;
+
+import net.trackmate.graph.mempool.ByteMappedElement;
+
+public class TestGraph extends GraphImp< TestVertexPool, TestEdgePool, TestVertex, TestEdge, ByteMappedElement >
+{
+	public TestGraph( final int initialCapacity )
+	{
+		super( new TestEdgePool( initialCapacity, new TestVertexPool( initialCapacity ) ) );
+	}
+
+	public TestGraph()
+	{
+		this( 10 );
+	}
+}
