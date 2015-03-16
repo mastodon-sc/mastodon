ecffdf4c76680770762f7564e45198ecc5277da0
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-03-16 14:23:55 +0100

    Helper for doing variable assignment for both ref and normal objects

---
+++ src/main/java/net/trackmate/graph/algorithm/Assigner.java
@@ -0,0 +1,41 @@
+package net.trackmate.graph.algorithm;
+
+import net.trackmate.graph.PoolObject;
+
+public abstract class Assigner< O >
+{
+	public abstract O assign( final O value, final O target );
+
+	@SuppressWarnings( "unchecked" )
+	public static < O > Assigner< O > getFor( final O obj )
+	{
+		if ( obj instanceof PoolObject )
+			return RefAssign.instance;
+		else
+			return ObjectAssign.instance;
+	}
+
+	static class RefAssign< O extends PoolObject< O, ? > > extends Assigner< O >
+	{
+		@Override
+		public O assign( final O value, final O target )
+		{
+			return target.refTo( value );
+		}
+
+		@SuppressWarnings( "rawtypes" )
+		static RefAssign instance = new RefAssign();
+	}
+
+	static class ObjectAssign< O > extends Assigner< O >
+	{
+		@Override
+		public O assign( final O value, final O target )
+		{
+			return value;
+		}
+
+		@SuppressWarnings( "rawtypes" )
+		static ObjectAssign instance = new ObjectAssign();
+	}
+}
