9be28bf249f00b24699e1728c40f8306b0cf43ce
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-03-16 16:27:36 +0100

    adding back DepthFirstIterator unit tests with ObjectGraph. We should verify that algorithms work with both ref based and object based graphs.

--- src/test/java/net/trackmate/graph/algorithm/DepthFirstIteratorTest.java
+++ src/test/java/net/trackmate/graph/algorithm/DepthFirstIteratorTest.java
@@ -11,6 +11,9 @@ import net.trackmate.graph.TestEdge;
 import net.trackmate.graph.TestGraph;
 import net.trackmate.graph.TestVertex;
 import net.trackmate.graph.collection.CollectionUtils;
+import net.trackmate.graph.object.ObjectEdge;
+import net.trackmate.graph.object.ObjectGraph;
+import net.trackmate.graph.object.ObjectVertex;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -136,6 +139,60 @@ public class DepthFirstIteratorTest
 		assertEquals( iter.next().getId(), 11 );
 		assertEquals( iter.next().getId(), 111 );
 		assertEquals( iter.next().getId(), 12 );
+		assertFalse( iter.hasNext() );
+	}
+
+	@Test
+	public void testDepthFirstIteratorWithObjectGraph()
+	{
+		final ObjectGraph< Integer > graph = new ObjectGraph< Integer >();
+		final ObjectVertex< Integer > v1 = graph.addVertex().init( 1 );
+		final ObjectVertex< Integer > v11 = graph.addVertex().init( 11 );
+		final ObjectVertex< Integer > v12 = graph.addVertex().init( 12 );
+		final ObjectVertex< Integer > v111 = graph.addVertex().init( 111 );
+		final ObjectVertex< Integer > v112 = graph.addVertex().init( 112 );
+		final ObjectVertex< Integer > v121 = graph.addVertex().init( 121 );
+		final ObjectVertex< Integer > v122 = graph.addVertex().init( 122 );
+
+		graph.addEdge( v12, v122 );
+		graph.addEdge( v12, v121 );
+		graph.addEdge( v11, v112 );
+		graph.addEdge( v11, v111 );
+		graph.addEdge( v1, v12 );
+		graph.addEdge( v1, v11 );
+
+		final DepthFirstIterator< ObjectVertex< Integer >, ObjectEdge< Integer > > iter = DepthFirstIterator.create( v1, graph );
+
+		assertEquals( iter.next().getContent().intValue(), 1 );
+		assertEquals( iter.next().getContent().intValue(), 11 );
+		assertEquals( iter.next().getContent().intValue(), 111 );
+		assertEquals( iter.next().getContent().intValue(), 112 );
+		assertEquals( iter.next().getContent().intValue(), 12 );
+		assertEquals( iter.next().getContent().intValue(), 121 );
+		assertEquals( iter.next().getContent().intValue(), 122 );
+		assertFalse( iter.hasNext() );
+	}
+
+	@Test
+	public void testDepthFirstIteratorCycleWithObjectGraph()
+	{
+		final ObjectGraph< Integer > graph = new ObjectGraph< Integer >();
+		final ObjectVertex< Integer > v1 = graph.addVertex().init( 1 );
+		final ObjectVertex< Integer > v11 = graph.addVertex().init( 11 );
+		final ObjectVertex< Integer > v12 = graph.addVertex().init( 12 );
+		final ObjectVertex< Integer > v111 = graph.addVertex().init( 111 );
+
+		graph.addEdge( v11, v111 );
+		graph.addEdge( v12, v111 );
+		graph.addEdge( v1, v12 );
+		graph.addEdge( v1, v11 );
+
+		final DepthFirstIterator< ObjectVertex< Integer >, ObjectEdge< Integer > > iter = DepthFirstIterator.create( v1, graph );
+
+		assertEquals( iter.next().getContent().intValue(), 1 );
+		assertEquals( iter.next().getContent().intValue(), 11 );
+		assertEquals( iter.next().getContent().intValue(), 111 );
+		assertEquals( iter.next().getContent().intValue(), 12 );
 		assertFalse( iter.hasNext() );
 	}
 }
