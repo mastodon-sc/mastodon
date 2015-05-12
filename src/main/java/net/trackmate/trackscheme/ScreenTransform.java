e2c4b3c409800043c123ba815f6ebc4c46932c7d
Jean-Yves Tinevez <tinevez@pasteur.fr>
2015-05-12 11:02:02 +0200

    Code style changes.
    The formatter completely screws the code indentation, but this is to
    avoid having this bug while doing real changes to the code.
    
    Signed-off-by: Jean-Yves Tinevez <tinevez@pasteur.fr>

--- src/main/java/net/trackmate/trackscheme/ScreenTransform.java
+++ src/main/java/net/trackmate/trackscheme/ScreenTransform.java
@@ -54,8 +54,8 @@ public class ScreenTransform
 
 	void setScreenTranslated( final int dX, final int dY, final ScreenTransform source )
 	{
-		final double xInvScale = ( double ) ( maxX - minX ) / ( screenWidth - 1 );
-		final double yInvScale = ( double )  ( maxY - minY ) / ( screenHeight - 1 );
+		final double xInvScale = ( maxX - minX ) / ( screenWidth - 1 );
+		final double yInvScale = ( maxY - minY ) / ( screenHeight - 1 );
 		minX = source.minX + xInvScale * dX;
 		maxX = source.maxX + xInvScale * dX;
 		minY = source.minY + yInvScale * dY;
@@ -64,13 +64,13 @@ public class ScreenTransform
 
 	double screenToLayoutX( final int x )
 	{
-		final double xInvScale = ( double ) ( maxX - minX ) / ( screenWidth - 1 );
+		final double xInvScale = ( maxX - minX ) / ( screenWidth - 1 );
 		return minX + xInvScale * x;
 	}
 
 	double screenToLayoutY( final int y )
 	{
-		final double yInvScale = ( double )  ( maxY - minY ) / ( screenHeight - 1 );
+		final double yInvScale = ( maxY - minY ) / ( screenHeight - 1 );
 		return minY + yInvScale * y;
 	}
 
@@ -80,8 +80,8 @@ public class ScreenTransform
 		final double lY = screenToLayoutY( y );
 		final double newSizeX = ( maxX - minX ) * scale;
 		final double newSizeY = ( maxY - minY ) * scale;
-		final double newXInvScale = newSizeX / ( double ) ( screenWidth - 1 );
-		final double newYInvScale = newSizeY / ( double ) ( screenHeight - 1 );
+		final double newXInvScale = newSizeX / ( screenWidth - 1 );
+		final double newYInvScale = newSizeY / ( screenHeight - 1 );
 		minX = lX - newXInvScale * x;
 		maxX = minX + newSizeX;
 		minY = lY - newYInvScale * y;
@@ -92,7 +92,7 @@ public class ScreenTransform
 	{
 		final double lX = screenToLayoutX( x );
 		final double newSizeX = ( maxX - minX ) * scale;
-		final double newXInvScale = newSizeX / ( double ) ( screenWidth - 1 );
+		final double newXInvScale = newSizeX / ( screenWidth - 1 );
 		minX = lX - newXInvScale * x;
 		maxX = minX + newSizeX;
 	}
@@ -101,7 +101,7 @@ public class ScreenTransform
 	{
 		final double lY = screenToLayoutY( y );
 		final double newSizeY = ( maxY - minY ) * scale;
-		final double newYInvScale = newSizeY / ( double ) ( screenHeight - 1 );
+		final double newYInvScale = newSizeY / ( screenHeight - 1 );
 		minY = lY - newYInvScale * y;
 		maxY = minY + newSizeY;
 	}
@@ -109,226 +109,249 @@ public class ScreenTransform
 	public static class ScreenTransformEventHandler extends MouseAdapter implements KeyListener, TransformEventHandler< ScreenTransform >
 	{
 		final static private TransformEventHandlerFactory< ScreenTransform > factory = new TransformEventHandlerFactory< ScreenTransform >()
-		{
+				{
 			@Override
 			public TransformEventHandler< ScreenTransform > create( final TransformListener< ScreenTransform > transformListener )
 			{
 				return new ScreenTransformEventHandler( transformListener );
 			}
-		};
+				};
 
-		public static TransformEventHandlerFactory< ScreenTransform > factory()
-		{
-			return factory;
-		}
-
-		/**
-		 * Current source to screen transform.
-		 */
-		final protected ScreenTransform transform = new ScreenTransform();
-
-		/**
-		 * Copy of {@link #affine current transform} when mouse dragging started.
-		 */
-		final protected ScreenTransform transformDragStart = new ScreenTransform();
-
-		/**
-		 * Whom to notify when the current transform is changed.
-		 */
-		protected TransformListener< ScreenTransform > listener;
-
-		/**
-		 * Whom to notify when selecting stuff.
-		 */
-		protected SelectionListener selectionListener;
-
-		/**
-		 * Coordinates where mouse dragging started.
-		 */
-		protected int oX, oY;
-
-		/**
-		 * The screen size of the canvas (the component displaying the image and
-		 * generating mouse events).
-		 */
-		protected int canvasW = 1, canvasH = 1;
-
-		/**
-		 * Screen coordinates to keep centered while zooming or rotating with the
-		 * keyboard. For example set these to
-		 * <em>(screen-width/2, screen-height/2)</em>
-		 */
-		protected int centerX = 0, centerY = 0;
-
-		public ScreenTransformEventHandler( final TransformListener< ScreenTransform > listener )
-		{
-			this.listener = listener;
-			this.selectionListener = null;
-		}
-
-		@Override
-		public ScreenTransform getTransform()
-		{
-			synchronized ( transform )
-			{
-				return transform.copy();
-			}
-		}
-
-		@Override
-		public void setTransform( final ScreenTransform t )
-		{
-			synchronized ( transform )
-			{
-				transform.set( t );
-			}
-		}
-
-		@Override
-		public void setCanvasSize( final int width, final int height, final boolean updateTransform )
-		{
-			// TODO Auto-generated method stub
-			canvasW = width;
-			canvasH = height;
-			centerX = width / 2;
-			centerY = height / 2;
-			synchronized ( transform )
-			{
-				transform.screenWidth = canvasW;
-				transform.screenHeight = canvasH;
-
-				update();
-			}
-		}
-
-		@Override
-		public void setTransformListener( final TransformListener< ScreenTransform > transformListener )
-		{
-			listener = transformListener;
-		}
-
-		public void setSelectionListener( final SelectionListener selectionListener )
-		{
-			this.selectionListener = selectionListener;
-		}
-
-		@Override
-		public String getHelpString()
-		{
-			return null;
-		}
-
-		/**
-		 * notifies {@link #listener} that the current transform changed.
-		 */
-		protected void update()
-		{
-			if ( listener != null )
-				listener.transformChanged( transform );
-		}
-
-		// ================ KeyListener =============================
-
-		private boolean shiftPressed = false;
-
-		@Override
-		public void keyPressed( final KeyEvent e )
-		{
-			if ( e.getKeyCode() == KeyEvent.VK_SHIFT )
-				shiftPressed = true;
-		}
-
-		@Override
-		public void keyReleased( final KeyEvent e )
-		{
-			if ( e.getKeyCode() == KeyEvent.VK_SHIFT )
-				shiftPressed = false;
-		}
-
-		@Override
-		public void keyTyped( final KeyEvent e )
-		{
-			// TODO Auto-generated method stub
-
-		}
-
-		// ================ MouseAdapter ============================
-
-		@Override
-		public void mousePressed( final MouseEvent e )
-		{
-			oX = e.getX();
-			oY = e.getY();
-			synchronized ( transform )
-			{
-				transformDragStart.set( transform );
-
-				if ( selectionListener != null )
-					if ( e.getButton() == MouseEvent.BUTTON1 )
-						selectionListener.selectAt( transform, e.getX(), e.getY() );
-			}
-		}
-
-		@Override
-		public void mouseDragged( final MouseEvent e )
-		{
-			synchronized ( transform )
-			{
-				final int modifiers = e.getModifiersEx();
-
-				if ( ( modifiers & ( MouseEvent.BUTTON2_DOWN_MASK | MouseEvent.BUTTON3_DOWN_MASK ) ) != 0 ) // translate
+				public static TransformEventHandlerFactory< ScreenTransform > factory()
 				{
-					final int dX = oX - e.getX();
-					final int dY = oY - e.getY();
-					transform.setScreenTranslated( dX, dY, transformDragStart );
+					return factory;
 				}
 
-				update();
-			}
-		}
+				/**
+				 * Current source to screen transform.
+				 */
+				final protected ScreenTransform transform = new ScreenTransform();
 
-		@Override
-		public void mouseWheelMoved( final MouseWheelEvent e )
-		{
-			synchronized ( transform )
-			{
-				final double dScale = 1.1;
-				final int modifiers = e.getModifiersEx();
-				final int s = e.getWheelRotation();
-				final boolean ctrlPressed = ( modifiers & KeyEvent.CTRL_DOWN_MASK ) != 0;
-				final boolean altPressed = ( modifiers & KeyEvent.ALT_DOWN_MASK ) != 0;
-				final boolean metaPressed = ( ( modifiers & KeyEvent.META_DOWN_MASK ) != 0 ) || ( ctrlPressed && shiftPressed );
-				if ( metaPressed ) // zoom both axes
+				/**
+				 * Copy of {@link #affine current transform} when mouse dragging
+				 * started.
+				 */
+				final protected ScreenTransform transformDragStart = new ScreenTransform();
+
+				/**
+				 * Whom to notify when the current transform is changed.
+				 */
+				protected TransformListener< ScreenTransform > listener;
+
+				/**
+				 * Whom to notify when selecting stuff.
+				 */
+				protected SelectionListener selectionListener;
+
+				/**
+				 * Coordinates where mouse dragging started.
+				 */
+				protected int oX, oY;
+
+				/**
+				 * The screen size of the canvas (the component displaying the image and
+				 * generating mouse events).
+				 */
+				protected int canvasW = 1, canvasH = 1;
+
+				/**
+				 * Screen coordinates to keep centered while zooming or rotating with
+				 * the keyboard. For example set these to
+				 * <em>(screen-width/2, screen-height/2)</em>
+				 */
+				protected int centerX = 0, centerY = 0;
+
+				public ScreenTransformEventHandler( final TransformListener< ScreenTransform > listener )
 				{
-					if ( s > 0 )
-						transform.scale( 1.0 / dScale, e.getX(), e.getY() );
-					else
-						transform.scale( dScale, e.getX(), e.getY() );
-				}
-				else if ( shiftPressed ) // zoom X axis
-				{
-					if ( s > 0 )
-						transform.scaleX( 1.0 / dScale, e.getX(), e.getY() );
-					else
-						transform.scaleX( dScale, e.getX(), e.getY() );
-				}
-				else if ( ctrlPressed || altPressed ) // zoom Y axis
-				{
-					if ( s > 0 )
-						transform.scaleY( 1.0 / dScale, e.getX(), e.getY() );
-					else
-						transform.scaleY( dScale, e.getX(), e.getY() );
-				}
-				else
-				{
-					final int d = s * 15;
-					final boolean dirX = ( modifiers & KeyEvent.SHIFT_DOWN_MASK ) != 0;
-					transform.setScreenTranslated( dirX ? d : 0, dirX ? 0 : d, transform );
+					this.listener = listener;
+					this.selectionListener = null;
 				}
 
-				update();
-			}
-		}
+				@Override
+				public ScreenTransform getTransform()
+				{
+					synchronized ( transform )
+					{
+						return transform.copy();
+					}
+				}
+
+				@Override
+				public void setTransform( final ScreenTransform t )
+				{
+					synchronized ( transform )
+					{
+						transform.set( t );
+					}
+				}
+
+				@Override
+				public void setCanvasSize( final int width, final int height, final boolean updateTransform )
+				{
+					// TODO Auto-generated method stub
+					canvasW = width;
+					canvasH = height;
+					centerX = width / 2;
+					centerY = height / 2;
+					synchronized ( transform )
+					{
+						transform.screenWidth = canvasW;
+						transform.screenHeight = canvasH;
+
+						update();
+					}
+				}
+
+				@Override
+				public void setTransformListener( final TransformListener< ScreenTransform > transformListener )
+				{
+					listener = transformListener;
+				}
+
+				public void setSelectionListener( final SelectionListener selectionListener )
+				{
+					this.selectionListener = selectionListener;
+				}
+
+				@Override
+				public String getHelpString()
+				{
+					return null;
+				}
+
+				/**
+				 * notifies {@link #listener} that the current transform changed.
+				 */
+				protected void update()
+				{
+					if ( listener != null )
+					{
+						listener.transformChanged( transform );
+					}
+				}
+
+				// ================ KeyListener =============================
+
+				private boolean shiftPressed = false;
+
+				@Override
+				public void keyPressed( final KeyEvent e )
+				{
+					if ( e.getKeyCode() == KeyEvent.VK_SHIFT )
+					{
+						shiftPressed = true;
+					}
+				}
+
+				@Override
+				public void keyReleased( final KeyEvent e )
+				{
+					if ( e.getKeyCode() == KeyEvent.VK_SHIFT )
+					{
+						shiftPressed = false;
+					}
+				}
+
+				@Override
+				public void keyTyped( final KeyEvent e )
+				{
+					// TODO Auto-generated method stub
+
+				}
+
+				// ================ MouseAdapter ============================
+
+				@Override
+				public void mousePressed( final MouseEvent e )
+				{
+					oX = e.getX();
+					oY = e.getY();
+					synchronized ( transform )
+					{
+						transformDragStart.set( transform );
+
+						if ( selectionListener != null )
+						{
+							if ( e.getButton() == MouseEvent.BUTTON1 )
+							{
+								selectionListener.selectAt( transform, e.getX(), e.getY() );
+							}
+						}
+					}
+				}
+
+				@Override
+				public void mouseDragged( final MouseEvent e )
+				{
+					synchronized ( transform )
+					{
+						final int modifiers = e.getModifiersEx();
+
+						if ( ( modifiers & ( MouseEvent.BUTTON2_DOWN_MASK | MouseEvent.BUTTON3_DOWN_MASK ) ) != 0 ) // translate
+						{
+							final int dX = oX - e.getX();
+							final int dY = oY - e.getY();
+							transform.setScreenTranslated( dX, dY, transformDragStart );
+						}
+
+						update();
+					}
+				}
+
+				@Override
+				public void mouseWheelMoved( final MouseWheelEvent e )
+				{
+					synchronized ( transform )
+					{
+						final double dScale = 1.1;
+						final int modifiers = e.getModifiersEx();
+						final int s = e.getWheelRotation();
+						final boolean ctrlPressed = ( modifiers & KeyEvent.CTRL_DOWN_MASK ) != 0;
+						final boolean altPressed = ( modifiers & KeyEvent.ALT_DOWN_MASK ) != 0;
+						final boolean metaPressed = ( ( modifiers & KeyEvent.META_DOWN_MASK ) != 0 ) || ( ctrlPressed && shiftPressed );
+						if ( metaPressed ) // zoom both axes
+						{
+							if ( s > 0 )
+							{
+								transform.scale( 1.0 / dScale, e.getX(), e.getY() );
+							}
+							else
+							{
+								transform.scale( dScale, e.getX(), e.getY() );
+							}
+						}
+						else if ( shiftPressed ) // zoom X axis
+						{
+							if ( s > 0 )
+							{
+								transform.scaleX( 1.0 / dScale, e.getX(), e.getY() );
+							}
+							else
+							{
+								transform.scaleX( dScale, e.getX(), e.getY() );
+							}
+						}
+						else if ( ctrlPressed || altPressed ) // zoom Y axis
+						{
+							if ( s > 0 )
+							{
+								transform.scaleY( 1.0 / dScale, e.getX(), e.getY() );
+							}
+							else
+							{
+								transform.scaleY( dScale, e.getX(), e.getY() );
+							}
+						}
+						else
+						{
+							final int d = s * 15;
+							final boolean dirX = ( modifiers & KeyEvent.SHIFT_DOWN_MASK ) != 0;
+							transform.setScreenTranslated( dirX ? d : 0, dirX ? 0 : d, transform );
+						}
+
+						update();
+					}
+				}
 
 	}
 }
