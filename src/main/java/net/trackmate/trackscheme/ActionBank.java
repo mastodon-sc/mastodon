7d6c6b482e80d42304258cbf96bcf2f0082aef23
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-05-14 14:39:44 +0200

    fix "deleteSelection": first remove edges, then remove vertices

--- src/main/java/net/trackmate/trackscheme/ActionBank.java
+++ src/main/java/net/trackmate/trackscheme/ActionBank.java
@@ -11,21 +11,22 @@ public class ActionBank
 			private static final long serialVersionUID = 7237432299287538771L;
 
 			@Override
-			public void actionPerformed( ActionEvent event )
+			public void actionPerformed( final ActionEvent event )
 			{
 				final SelectionModel< TrackSchemeVertex, TrackSchemeEdge > selectionModel = trackscheme.selectionModel;
 				final TrackSchemeGraph graph = trackscheme.graph;
-				for ( final TrackSchemeVertex v : selectionModel.getSelectedVertices() )
-				{
-					graph.remove( v );
-				}
 				for ( final TrackSchemeEdge e : selectionModel.getSelectedEdges() )
 				{
 					graph.remove( e );
 				}
+				for ( final TrackSchemeVertex v : selectionModel.getSelectedVertices() )
+				{
+					graph.remove( v );
+				}
 				selectionModel.clearSelection();
 				trackscheme.layout.layoutX();
 				trackscheme.order.build();
+				trackscheme.frame.repaint();
 			}
 		};
 	}
