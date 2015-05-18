b2f9686f90287ab92fbf5803173fb443cf1385e6
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2015-05-18 18:02:42 +0200

    Fix the ZoomHandler.
    Fix the transform changes.
    Also fix mouse handling on hopefully all platforms.
    
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/net/trackmate/trackscheme/ZoomBoxHandler.java
+++ src/main/java/net/trackmate/trackscheme/ZoomBoxHandler.java
@@ -7,11 +7,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import net.imglib2.ui.OverlayRenderer;
+import net.imglib2.ui.TransformEventHandler;
 import net.imglib2.ui.TransformListener;
 
 public class ZoomBoxHandler extends MouseAdapter implements TransformListener< ScreenTransform >
 {
 
+	private static final int MOUSE_MASK = InputEvent.BUTTON1_DOWN_MASK +  InputEvent.ALT_DOWN_MASK;
+	
 	private final ZoomBoxOverlay zoomOverlay = new ZoomBoxOverlay();
 
 	/**
@@ -24,24 +27,24 @@ public class ZoomBoxHandler extends MouseAdapter implements TransformListener< S
 	 */
 	private int eX, eY;
 
-	private final TransformListener< ScreenTransform > listener;
+	private final TransformEventHandler<ScreenTransform> transformEventHandler;
 
 	private boolean zoomStarted = false;
 
 	private ScreenTransform transform;
 
-	private final SelectionListener selectionListener;
+	private final ShowTrackScheme trackscheme;
 
-	public ZoomBoxHandler( TransformListener< ScreenTransform > listener, SelectionListener selectionListener )
+	public ZoomBoxHandler( TransformEventHandler<ScreenTransform> transformEventHandler, ShowTrackScheme selectionListener )
 	{
-		this.listener = listener;
-		this.selectionListener = selectionListener;
+		this.transformEventHandler = transformEventHandler;
+		this.trackscheme = selectionListener;
 	}
 
 	@Override
 	public void mouseDragged( MouseEvent e )
 	{
-		if ( e.getButton() == MouseEvent.BUTTON1 && ( e.getModifiersEx() & InputEvent.ALT_DOWN_MASK ) == InputEvent.ALT_DOWN_MASK && !zoomStarted )
+		if ( (e.getModifiersEx() == MOUSE_MASK ) && !zoomStarted )
 		{
 			oX = e.getX();
 			oY = e.getY();
@@ -49,7 +52,7 @@ public class ZoomBoxHandler extends MouseAdapter implements TransformListener< S
 		}
 		eX = e.getX();
 		eY = e.getY();
-		selectionListener.refresh();
+		trackscheme.refresh();
 	}
 
 	@Override
@@ -69,7 +72,8 @@ public class ZoomBoxHandler extends MouseAdapter implements TransformListener< S
 			transform.maxY = Math.max( maxY, minY ) + 0.1;
 			transform.minY = Math.min( maxY, minY );
 
-			listener.transformChanged( transform );
+			transformEventHandler.setTransform( transform );
+			trackscheme.refresh();
 		}
 	}
 
