754e923bc4cc26e01c2ffd4a3e17de44e117f7f3
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2015-05-22 21:55:49 +0200

    Add a navigate up & down action for keys.
    Just moving to child and parent now.
    The repainting of the frame does not work. I don;t understand why as it
    works for the DefaultSelectionHandler.
    
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/net/trackmate/trackscheme/ActionBank.java
+++ src/main/java/net/trackmate/trackscheme/ActionBank.java
@@ -2,6 +2,10 @@ package net.trackmate.trackscheme;
 
 import java.awt.event.ActionEvent;
 
+import net.trackmate.graph.IncomingEdges;
+import net.trackmate.graph.OutgoingEdges;
+import net.trackmate.graph.collection.RefSet;
+
 public class ActionBank
 {
 	public static final AbstractNamedAction getDeleteSelectionAction( final ShowTrackScheme trackscheme )
@@ -27,6 +31,66 @@ public class ActionBank
 				trackscheme.layout.layoutX();
 				trackscheme.order.build();
 				trackscheme.frame.repaint();
+			}
+		};
+	}
+
+	public static final AbstractNamedAction getNavigateToChildAction( final ShowTrackScheme trackscheme )
+	{
+		return new AbstractNamedAction( "navigateToChild" )
+		{
+			private static final long serialVersionUID = -8748889039516159858L;
+
+			@Override
+			public void actionPerformed( final ActionEvent event )
+			{
+				final SelectionModel< TrackSchemeVertex, TrackSchemeEdge > selectionModel = trackscheme.selectionModel;
+				final RefSet< TrackSchemeVertex > vertices = selectionModel.getSelectedVertices();
+				if ( vertices.size() == 1 )
+				{
+					final TrackSchemeVertex vertex = vertices.iterator().next();
+					final OutgoingEdges< TrackSchemeEdge > edges = vertex.outgoingEdges();
+					if ( edges.size() > 0 )
+					{
+						final TrackSchemeEdge edge = edges.get( 0 );
+						final TrackSchemeVertex child = edge.getTarget();
+						selectionModel.clearSelection();
+						vertex.setSelected( false );
+						selectionModel.add( child );
+						child.setSelected( true );
+						trackscheme.refresh();
+					}
+				}
+			}
+		};
+	}
+
+	public static final AbstractNamedAction getNavigateToParentAction( final ShowTrackScheme trackscheme )
+	{
+		return new AbstractNamedAction( "navigateToParent" )
+		{
+			private static final long serialVersionUID = -819879165845995978L;
+
+			@Override
+			public void actionPerformed( final ActionEvent event )
+			{
+				final SelectionModel< TrackSchemeVertex, TrackSchemeEdge > selectionModel = trackscheme.selectionModel;
+				final RefSet< TrackSchemeVertex > vertices = selectionModel.getSelectedVertices();
+				if ( vertices.size() == 1 )
+				{
+					final TrackSchemeVertex vertex = vertices.iterator().next();
+					final IncomingEdges< TrackSchemeEdge > edges = vertex.incomingEdges();
+					if ( edges.size() > 0 )
+					{
+						final TrackSchemeEdge edge = edges.get( 0 );
+						final TrackSchemeVertex child = edge.getSource();
+						selectionModel.clearSelection();
+						vertex.setSelected( false );
+						selectionModel.add( child );
+						child.setSelected( true );
+						trackscheme.refresh();
+					}
+				}
 			}
 		};
 	}
--- src/main/java/net/trackmate/trackscheme/KeyHandler.java
+++ src/main/java/net/trackmate/trackscheme/KeyHandler.java
@@ -1,12 +1,14 @@
 package net.trackmate.trackscheme;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.KeyStroke;
 
 public class KeyHandler
 {
 	private final ShowTrackScheme trackscheme;
 
-	public KeyHandler( ShowTrackScheme trackscheme )
+	public KeyHandler( final ShowTrackScheme trackscheme )
 	{
 		this.trackscheme = trackscheme;
 		install();
@@ -16,9 +18,16 @@ public class KeyHandler
 	{
 		final KeyStroke ks = KeyStroke.getKeyStroke( 'd' );
 		registerAction( ks, ActionBank.getDeleteSelectionAction( trackscheme ) );
+
+		final KeyStroke arrowDown = KeyStroke.getKeyStroke( KeyEvent.VK_DOWN, 0 );
+		registerAction( arrowDown, ActionBank.getNavigateToChildAction( trackscheme ) );
+
+		final KeyStroke arrowUp = KeyStroke.getKeyStroke( KeyEvent.VK_UP, 0 );
+		registerAction( arrowUp, ActionBank.getNavigateToParentAction( trackscheme ) );
+
 	}
 
-	private void registerAction( KeyStroke ks, AbstractNamedAction action )
+	private void registerAction( final KeyStroke ks, final AbstractNamedAction action )
 	{
 		trackscheme.canvas.getInputMap().put( ks, action.name() );
 		AbstractNamedAction.put( trackscheme.canvas.getActionMap(), action );
