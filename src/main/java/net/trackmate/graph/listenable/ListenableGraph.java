50c11802e079664f25e46c020f314c42d66f9fdd
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-10-15 23:47:16 +0200

    add TODO

--- src/main/java/net/trackmate/graph/listenable/ListenableGraph.java
+++ src/main/java/net/trackmate/graph/listenable/ListenableGraph.java
@@ -4,6 +4,7 @@ import net.trackmate.graph.Edge;
 import net.trackmate.graph.Graph;
 import net.trackmate.graph.Vertex;
 
+// TODO: this should rather extend ReadOnlyGraph!
 public interface ListenableGraph< V extends Vertex< E >, E extends Edge< V > > extends Graph< V, E >
 {
 	public boolean addGraphListener( GraphListener< V, E > listener );
