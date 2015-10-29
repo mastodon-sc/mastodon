83b6ca79f9dc8b466912413d5a55f43fda4ab733
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-10-29 19:08:03 +0100

    bugfix: SpatioTemporalIndexWrapper.getSpatialIndex() handles nulls

--- src/main/java/net/trackmate/revised/bdv/overlay/wrap/SpatioTemporalIndexWrapper.java
+++ src/main/java/net/trackmate/revised/bdv/overlay/wrap/SpatioTemporalIndexWrapper.java
@@ -5,7 +5,6 @@ import java.util.concurrent.locks.Lock;
 
 import net.trackmate.graph.Edge;
 import net.trackmate.graph.Vertex;
-import net.trackmate.spatial.HasTimepoint;
 import net.trackmate.spatial.SpatialIndex;
 import net.trackmate.spatial.SpatioTemporalIndex;
 
@@ -37,13 +36,20 @@ public class SpatioTemporalIndexWrapper< V extends Vertex< E >, E extends Edge< 
 	@Override
 	public SpatialIndex< OverlayVertexWrapper< V, E > > getSpatialIndex( final int timepoint )
 	{
-		return new SpatialIndexWrapper< V, E >( graphWrapper, wrappedIndex.getSpatialIndex( timepoint ) );
+		final SpatialIndex< V > index = wrappedIndex.getSpatialIndex( timepoint );
+		if ( index == null )
+			return null;
+		else
+			return new SpatialIndexWrapper< V, E >( graphWrapper, index );
 	}
 
 	@Override
 	public SpatialIndex< OverlayVertexWrapper< V, E > > getSpatialIndex( final int fromTimepoint, final int toTimepoint )
 	{
-		return new SpatialIndexWrapper< V, E >( graphWrapper, wrappedIndex.getSpatialIndex( fromTimepoint, toTimepoint ) );
+		final SpatialIndex< V > index = wrappedIndex.getSpatialIndex( fromTimepoint, toTimepoint );
+		if ( index == null )
+			return null;
+		else
+			return new SpatialIndexWrapper< V, E >( graphWrapper, index );
 	}
-
 }
