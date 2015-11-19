d553471c27e4b3a725cf225944a87ef8a60ffe2c
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-11-19 16:54:35 +0100

    Clean MaMuT class.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/net/trackmate/revised/MaMuT.java
+++ src/main/java/net/trackmate/revised/MaMuT.java
@@ -43,83 +43,10 @@ import bdv.viewer.ViewerPanel;
 
 public class MaMuT
 {
-	public static void main1( final String[] args )
-	{
-		final Model model = new Model();
-		final double[] pos = new double[] { 0, 0, 0 };
-		final double[][] cov = new double[][] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
-
-		final ListenableGraph< Spot, Link > graph = model.getGraph();
-		final GraphIdBimap< Spot, Link > idmap = model.getGraphIdBimap();
-		final Selection< Spot, Link > selection = new Selection<>( graph, idmap );
-		final DefaultModelGraphProperties< Spot, Link > properties = new DefaultModelGraphProperties<>( graph, idmap, selection );
-		final TrackSchemeGraph< Spot, Link > trackSchemeGraph = new TrackSchemeGraph<>( graph, idmap, properties );
-
-		final Spot s0 = model.addSpot( 0, pos, cov, model.getGraph().vertexRef() );
-		final Spot s1 = model.addSpot( 1, pos, cov, model.getGraph().vertexRef() );
-		final Spot s2 = model.addSpot( 1, pos, cov, model.getGraph().vertexRef() );
-		final Spot s3 = model.addSpot( 2, pos, cov, model.getGraph().vertexRef() );
-		final Spot s4 = model.addSpot( 2, pos, cov, model.getGraph().vertexRef() );
-		final Spot s5 = model.addSpot( 3, pos, cov, model.getGraph().vertexRef() );
-
-		final Link l0 = model.addLink( s0, s1, model.getGraph().edgeRef() );
-		final Link l1 = model.addLink( s0, s2, model.getGraph().edgeRef() );
-		final Link l2 = model.addLink( s2, s3, model.getGraph().edgeRef() );
-		final Link l3 = model.addLink( s4, s5, model.getGraph().edgeRef() );
-
-		System.out.println( trackSchemeGraph );
-	}
-
-	public static void main2( final String[] args )
-	{
-		final Model model = new Model();
-		final double[] pos = new double[] { 0, 0, 0 };
-		final double[][] cov = new double[][] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
-
-		final Spot s0 = model.addSpot( 0, pos, cov, model.getGraph().vertexRef() );
-		final Spot s1 = model.addSpot( 1, pos, cov, model.getGraph().vertexRef() );
-		final Spot s2 = model.addSpot( 1, pos, cov, model.getGraph().vertexRef() );
-		final Spot s3 = model.addSpot( 2, pos, cov, model.getGraph().vertexRef() );
-		final Spot s4 = model.addSpot( 2, pos, cov, model.getGraph().vertexRef() );
-		final Spot s5 = model.addSpot( 3, pos, cov, model.getGraph().vertexRef() );
-
-		final Link l0 = model.addLink( s0, s1, model.getGraph().edgeRef() );
-		final Link l1 = model.addLink( s0, s2, model.getGraph().edgeRef() );
-		final Link l2 = model.addLink( s2, s3, model.getGraph().edgeRef() );
-		final Link l3 = model.addLink( s4, s5, model.getGraph().edgeRef() );
-
-		final ListenableGraph< Spot, Link > graph = model.getGraph();
-		final GraphIdBimap< Spot, Link > idmap = model.getGraphIdBimap();
-		final Selection< Spot, Link > selection = new Selection<>( graph, idmap );
-		final DefaultModelGraphProperties< Spot, Link > properties = new DefaultModelGraphProperties<>( graph, idmap, selection );
-		final TrackSchemeGraph< Spot, Link > trackSchemeGraph = new TrackSchemeGraph<>( graph, idmap, properties );
-
-		System.out.println( trackSchemeGraph );
-	}
-
-	public static void main3( final String[] args ) throws IOException
-	{
-		final File modelFile = new File( "/Users/pietzsch/TGMM/data/tifs/model_revised.raw" );
-
-		final Model model = new Model();
-		model.loadRaw( modelFile );
-
-
-		final ListenableGraph< Spot, Link > graph = model.getGraph();
-		final GraphIdBimap< Spot, Link > idmap = model.getGraphIdBimap();
-		final Selection< Spot, Link > selection = new Selection<>( graph, idmap );
-		final DefaultModelGraphProperties< Spot, Link > properties = new DefaultModelGraphProperties<>( graph, idmap, selection );
-		final TrackSchemeGraph< Spot, Link > trackSchemeGraph = new TrackSchemeGraph<>( graph, idmap, properties );
-
-		System.out.println( trackSchemeGraph );
-	}
-
 	public static void main4( final String[] args ) throws IOException, SpimDataException
 	{
-		//		final String bdvFile = "/Users/pietzsch/TGMM/data/tifs/datasethdf5.xml";
-		//		final String modelFile = "/Users/pietzsch/TGMM/data/tifs/model_revised.raw";
-		final String bdvFile = "D:/Users/Jean-Yves/Desktop/mitosis.xml";
-		final String modelFile = ""; // "/Volumes/Data/model_revised.raw";
+		final String bdvFile = "/Volumes/Data/datasethdf5.xml";
+		final String modelFile = "/Volumes/Data/model_revised.raw";
 		final int initialTimepointIndex = 10;
 
 		/*
