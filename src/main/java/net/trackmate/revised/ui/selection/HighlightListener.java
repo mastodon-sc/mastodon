0444e98952b527ffc7c96bf1eacc79653fd7a624
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-11-30 17:49:10 +0100

    Basic javadoc for HighlightModel & friend.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/net/trackmate/revised/ui/selection/HighlightListener.java
+++ src/main/java/net/trackmate/revised/ui/selection/HighlightListener.java
@@ -1,6 +1,13 @@
 package net.trackmate.revised.ui.selection;
 
+/**
+ * Interface for listener of the {@link HighlightListener}.
+ */
 public interface HighlightListener
 {
+	/**
+	 * Notifies when the vertex highlighted in a registered highlight model has
+	 * changed.
+	 */
 	public void highlightChanged();
 }
--- src/main/java/net/trackmate/revised/ui/selection/SelectionListener.java
+++ src/main/java/net/trackmate/revised/ui/selection/SelectionListener.java
@@ -1,6 +1,13 @@
 package net.trackmate.revised.ui.selection;
 
+/**
+ * Interface for listeners of a {@link Selection}.
+ *
+ */
 public interface SelectionListener
 {
+	/**
+	 * Notifies when the selection of a registered model has changed.
+	 */
 	public void selectionChanged();
 }
