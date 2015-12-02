21571cfbbf1588cec8465c05ed1c9751505618ce
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-12-02 19:20:14 +0100

    add TODO

--- src/main/java/net/trackmate/revised/trackscheme/display/TrackSchemePanel.java
+++ src/main/java/net/trackmate/revised/trackscheme/display/TrackSchemePanel.java
@@ -383,7 +383,7 @@ public class TrackSchemePanel extends JPanel implements
 	@Override
 	public void navigateToVertex( final TrackSchemeVertex v )
 	{
-		focus.focusVertex( v );
+		focus.focusVertex( v ); // TODO: focusVertex is done here AND in TrackSchemeNavigator.rightSibling(). Once is enough. Which one?
 		final double lx = v.getLayoutX();
 		final double ly = v.getTimepoint();
 
