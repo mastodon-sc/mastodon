809b03d1ac5b357db0ef9bb912fb12a6573fcee9
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-12-07 18:32:47 +0100

    Implement IntPoolObjectArrayMap.keySet().
    Not tested ey. Probably very buggy.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/net/trackmate/graph/IntPoolObjectArrayMap.java
+++ src/main/java/net/trackmate/graph/IntPoolObjectArrayMap.java
@@ -1,5 +1,6 @@
 package net.trackmate.graph;
 
+import gnu.trove.TIntCollection;
 import gnu.trove.function.TObjectFunction;
 import gnu.trove.impl.Constants;
 import gnu.trove.iterator.TIntIterator;
@@ -12,7 +13,9 @@ import gnu.trove.procedure.TObjectProcedure;
 import gnu.trove.set.TIntSet;
 
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import net.trackmate.graph.collection.IntRefMap;
 
@@ -33,6 +36,7 @@ import net.trackmate.graph.collection.IntRefMap;
  */
 public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V >
 {
+
 	/**
 	 * Int value for no entry. We use -1 because ref objects cannot have an
 	 * internal pool index lower than 0.
@@ -52,7 +56,7 @@ public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V
 		this( pool, Constants.DEFAULT_CAPACITY );
 	}
 
-	public IntPoolObjectArrayMap( final RefPool< V> pool, final int initialCapacity )
+	public IntPoolObjectArrayMap( final RefPool< V > pool, final int initialCapacity )
 	{
 		this.pool = pool;
 		keyToIndexMap = new TIntArrayList( initialCapacity, NO_ENTRY_VALUE );
@@ -203,7 +207,7 @@ public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V
 	{
 		final V ref = pool.createRef();
 		final TIntIterator it = map.keySet().iterator();
-		while(it.hasNext())
+		while ( it.hasNext() )
 		{
 			final int key = it.next();
 			put( key, map.get( key ), ref );
@@ -211,13 +215,10 @@ public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V
 		pool.releaseRef( ref );
 	}
 
-	// === TODO === UNIMPLEMENTED ========
-
 	@Override
 	public TIntSet keySet()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new KeySetView();
 	}
 
 	@Override
@@ -241,6 +242,8 @@ public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V
 		}
 		return array;
 	}
+
+	// === TODO === UNIMPLEMENTED ========
 
 	@Override
 	public Collection< V > valueCollection()
@@ -316,7 +319,7 @@ public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V
 		str.append( " { " );
 		final int[] keys = keys();
 		final V ref = pool.createRef();
-		str.append( keys[0] + " -> " + get( keys[0], ref ) );
+		str.append( keys[ 0 ] + " -> " + get( keys[ 0 ], ref ) );
 		for ( int i = 1; i < keys.length; i++ )
 		{
 			final int key = keys[ i ];
@@ -324,5 +327,300 @@ public class IntPoolObjectArrayMap< V extends Ref< V > > implements IntRefMap< V
 		}
 		str.append( " }" );
 		return str.toString();
+	}
+
+	/*
+	 * PRIVATE CLASS
+	 */
+
+	private final class KeySetView implements TIntSet
+	{
+
+		@Override
+		public int getNoEntryValue()
+		{
+			return NO_ENTRY_KEY;
+		}
+
+		@Override
+		public int size()
+		{
+			return size;
+		}
+
+		@Override
+		public boolean isEmpty()
+		{
+			return size == 0;
+		}
+
+		@Override
+		public boolean contains( final int entry )
+		{
+			if ( entry == NO_ENTRY_KEY )
+				return false;
+			return keyToIndexMap.contains( entry );
+		}
+
+		@Override
+		public TIntIterator iterator()
+		{
+			return new TIntIterator()
+			{
+
+				/** Index of element to be returned by subsequent call to next. */
+				private int cursor = 0;
+
+				/**
+				 * Index of element returned by most recent call to next or
+				 * previous. Reset to -1 if this element is deleted by a call to
+				 * remove.
+				 */
+				int lastRet = -1;
+
+				/** {@inheritDoc} */
+				@Override
+				public boolean hasNext()
+				{
+					return cursor < size();
+				}
+
+				/** {@inheritDoc} */
+				@Override
+				public int next()
+				{
+					try
+					{
+						while ( keyToIndexMap.get( cursor ) < 0 )
+						{
+							cursor++;
+						}
+						final int next = keyToIndexMap.get( cursor );
+						lastRet = cursor++;
+						// Advance to next now.
+						while ( cursor < keyToIndexMap.size() && keyToIndexMap.get( cursor ) < 0 )
+						{
+							cursor++;
+						}
+						return next;
+					}
+					catch ( final IndexOutOfBoundsException e )
+					{
+						throw new NoSuchElementException();
+					}
+				}
+
+				/** {@inheritDoc} */
+				@Override
+				public void remove()
+				{
+					if ( lastRet == -1 )
+						throw new IllegalStateException();
+
+					try
+					{
+						keyToIndexMap.set( lastRet, NO_ENTRY_KEY );
+						if ( lastRet < cursor )
+							cursor--;
+						lastRet = -1;
+					}
+					catch ( final IndexOutOfBoundsException e )
+					{
+						throw new ConcurrentModificationException();
+					}
+				}
+			};
+		}
+
+		@Override
+		public int[] toArray()
+		{
+			return keys();
+		}
+
+		@Override
+		public int[] toArray( final int[] dest )
+		{
+			return keys( dest );
+		}
+
+		@Override
+		public boolean add( final int entry )
+		{
+			throw new UnsupportedOperationException( "add is not supported for keyset view." );
+		}
+
+		@Override
+		public boolean remove( final int entry )
+		{
+			final V ref = pool.createRef();
+			final V removed = IntPoolObjectArrayMap.this.remove( entry, ref );
+			pool.releaseRef( ref );
+			return ( removed != null );
+		}
+
+		@Override
+		public boolean containsAll( final Collection< ? > collection )
+		{
+			return keyToIndexMap.containsAll( collection );
+		}
+
+		@Override
+		public boolean containsAll( final TIntCollection collection )
+		{
+			return keyToIndexMap.containsAll( collection );
+		}
+
+		@Override
+		public boolean containsAll( final int[] array )
+		{
+			return keyToIndexMap.containsAll( array );
+		}
+
+		@Override
+		public boolean addAll( final Collection< ? extends Integer > collection )
+		{
+			throw new UnsupportedOperationException( "addAll is not supported for keyset view." );
+		}
+
+		@Override
+		public boolean addAll( final TIntCollection collection )
+		{
+			throw new UnsupportedOperationException( "addAll is not supported for keyset view." );
+		}
+
+		@Override
+		public boolean addAll( final int[] array )
+		{
+			throw new UnsupportedOperationException( "addAll is not supported for keyset view." );
+		}
+
+		@Override
+		public boolean retainAll( final Collection< ? > collection )
+		{
+			boolean changed = false;
+			final V ref = pool.createRef();
+			for ( final int entry : keys() )
+			{
+				if ( !collection.contains( entry ) )
+				{
+					final V removed = IntPoolObjectArrayMap.this.remove( entry, ref );
+					if ( removed != null )
+						changed = true;
+				}
+			}
+			pool.releaseRef( ref );
+			return changed;
+		}
+
+		@Override
+		public boolean retainAll( final TIntCollection collection )
+		{
+			boolean changed = false;
+			final V ref = pool.createRef();
+			for ( final int entry : keys() )
+			{
+				if ( !collection.contains( entry ) )
+				{
+					final V removed = IntPoolObjectArrayMap.this.remove( entry, ref );
+					if ( removed != null )
+						changed = true;
+				}
+			}
+			pool.releaseRef( ref );
+			return changed;
+		}
+
+		@Override
+		public boolean retainAll( final int[] array )
+		{
+			boolean changed = false;
+			final V ref = pool.createRef();
+			for ( final int entry : keys() )
+			{
+				boolean found = false;
+				for ( final int in : array )
+				{
+					if ( entry == in )
+					{
+						found = true;
+						break;
+					}
+				}
+				if ( !found )
+				{
+					final V removed = IntPoolObjectArrayMap.this.remove( entry, ref );
+					if ( removed != null )
+						changed = true;
+				}
+			}
+			pool.releaseRef( ref );
+			return changed;
+		}
+
+		@Override
+		public boolean removeAll( final Collection< ? > collection )
+		{
+			boolean changed = false;
+			for ( final Object obj : collection )
+			{
+				if ( obj instanceof Integer )
+				{
+					final boolean removed = remove( ( int ) obj );
+					if ( removed )
+						changed = true;
+				}
+			}
+			return changed;
+		}
+
+		@Override
+		public boolean removeAll( final TIntCollection collection )
+		{
+			boolean changed = false;
+			final TIntIterator it = collection.iterator();
+			while ( it.hasNext() )
+			{
+				final int entry = it.next();
+				final boolean removed = remove( entry );
+				if ( removed )
+					changed = true;
+			}
+			return changed;
+		}
+
+		@Override
+		public boolean removeAll( final int[] array )
+		{
+			boolean changed = false;
+			for ( final int entry : array )
+			{
+				final boolean removed = remove( entry );
+				if ( removed )
+					changed = true;
+			}
+			return changed;
+		}
+
+		@Override
+		public void clear()
+		{
+			IntPoolObjectArrayMap.this.clear();
+		}
+
+		@Override
+		public boolean forEach( final TIntProcedure procedure )
+		{
+			for ( int i = 0; i < keyToIndexMap.size(); i++ )
+			{
+				final int val = keyToIndexMap.get( i );
+				if ( val == NO_ENTRY_KEY )
+					continue;
+				final boolean ok = procedure.execute( val );
+				if ( !ok )
+					return false;
+			}
+			return true;
+		}
 	}
 }
