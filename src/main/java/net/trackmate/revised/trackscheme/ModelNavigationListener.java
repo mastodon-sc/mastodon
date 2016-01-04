0965416d609fe536d27360cd427bac9bcba83a6a
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-01-04 22:38:29 +0100

    javadoc

--- src/main/java/net/trackmate/revised/trackscheme/ModelNavigationListener.java
+++ src/main/java/net/trackmate/revised/trackscheme/ModelNavigationListener.java
@@ -1,8 +1,8 @@
 package net.trackmate.revised.trackscheme;
 
 /**
- *
- *
+ * A listener to navigation events. These are typically model navigation events
+ * forwarded by a {@link ModelNavigationProperties} implementation
  *
  * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
  */
