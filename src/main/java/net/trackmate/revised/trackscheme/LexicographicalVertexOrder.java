8aaa38afd644a3998fb6013c355bc6697297847b
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-03-03 13:28:21 +0100

    use AlphanumComparator for comparing root labels

--- src/main/java/net/trackmate/revised/trackscheme/LexicographicalVertexOrder.java
+++ src/main/java/net/trackmate/revised/trackscheme/LexicographicalVertexOrder.java
@@ -9,6 +9,7 @@ import gnu.trove.list.array.TIntArrayList;
 import net.trackmate.graph.PoolObjectList;
 import net.trackmate.graph.collection.RefCollection;
 import net.trackmate.graph.collection.RefList;
+import net.trackmate.revised.trackscheme.util.AlphanumComparator;
 
 /**
  * Lexicographically order {@link TrackSchemeVertex} for root sorting in
@@ -97,7 +98,8 @@ public class LexicographicalVertexOrder
 		@Override
 		public int compareTo( final VertexKey o )
 		{
-			final int rc = rootName.compareTo( o.rootName );
+			final int rc = AlphanumComparator.instance.compare( rootName, o.rootName );
+//			final int rc = rootName.compareTo( o.rootName );
 			if ( rc == 0 )
 			{
 				final TIntIterator it = edgeSequence.iterator();
---
+++ src/main/java/net/trackmate/revised/trackscheme/util/AlphanumComparator.java
@@ -0,0 +1,126 @@
+package net.trackmate.revised.trackscheme.util;
+
+import java.util.Comparator;
+
+/**
+ * The Alphanum Algorithm is an improved sorting algorithm for strings
+ * containing numbers. Instead of sorting numbers in ASCII order like a standard
+ * sort, this algorithm sorts numbers in numeric order.
+ * <p>
+ * The Alphanum Algorithm is discussed <a
+ * href=http://www.DaveKoelle.com>here</a>
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * <p>
+ * This is an updated version with enhancements made by Daniel Migowski, Andre
+ * Bogus, and David Koelle
+ * <p>
+ * To use this class: Use the static "sort" method from the
+ * java.util.Collections class: Collections.sort(your list, new
+ * AlphanumComparator());
+ */
+public class AlphanumComparator implements Comparator< String >
+{
+	public static final AlphanumComparator instance = new AlphanumComparator();
+
+	private final boolean isDigit( final char ch )
+	{
+		return ch >= 48 && ch <= 57;
+	}
+
+	// Singleton
+	private AlphanumComparator()
+	{}
+
+	/**
+	 * Length of string is passed in for improved efficiency (only need to
+	 * calculate it once)
+	 **/
+	private final String getChunk( final String s, final int slength, int marker )
+	{
+		final StringBuilder chunk = new StringBuilder();
+		char c = s.charAt( marker );
+		chunk.append( c );
+		marker++;
+		if ( isDigit( c ) )
+		{
+			while ( marker < slength )
+			{
+				c = s.charAt( marker );
+				if ( !isDigit( c ) )
+					break;
+				chunk.append( c );
+				marker++;
+			}
+		}
+		else
+		{
+			while ( marker < slength )
+			{
+				c = s.charAt( marker );
+				if ( isDigit( c ) )
+					break;
+				chunk.append( c );
+				marker++;
+			}
+		}
+		return chunk.toString();
+	}
+
+	@Override
+	public int compare( final String s1, final String s2 )
+	{
+
+		int thisMarker = 0;
+		int thatMarker = 0;
+		final int s1Length = s1.length();
+		final int s2Length = s2.length();
+
+		while ( thisMarker < s1Length && thatMarker < s2Length )
+		{
+			final String thisChunk = getChunk( s1, s1Length, thisMarker );
+			thisMarker += thisChunk.length();
+
+			final String thatChunk = getChunk( s2, s2Length, thatMarker );
+			thatMarker += thatChunk.length();
+
+			// If both chunks contain numeric characters, sort them numerically
+			int result = 0;
+			if ( isDigit( thisChunk.charAt( 0 ) ) && isDigit( thatChunk.charAt( 0 ) ) )
+			{
+				// Simple chunk comparison by length.
+				final int thisChunkLength = thisChunk.length();
+				result = thisChunkLength - thatChunk.length();
+				// If equal, the first different number counts
+				if ( result == 0 )
+				{
+					for ( int i = 0; i < thisChunkLength; i++ )
+					{
+						result = thisChunk.charAt( i ) - thatChunk.charAt( i );
+						if ( result != 0 ) { return result; }
+					}
+				}
+			}
+			else
+			{
+				result = thisChunk.compareTo( thatChunk );
+			}
+
+			if ( result != 0 )
+				return result;
+		}
+
+		return s1Length - s2Length;
+	}
+}
