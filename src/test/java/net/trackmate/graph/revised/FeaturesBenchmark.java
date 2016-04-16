657e443d3f371e3e37bb39504213ba7816af148e
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2016-04-16 23:52:57 +0200

    Simple benchmark for feature system.

---
+++ src/test/java/net/trackmate/graph/revised/FeaturesBenchmark.java
@@ -0,0 +1,81 @@
+package net.trackmate.graph.revised;
+
+import java.util.Random;
+
+import net.trackmate.revised.model.IntVertexFeature;
+import net.trackmate.revised.model.mamut.Model;
+import net.trackmate.revised.model.mamut.Spot;
+
+public class FeaturesBenchmark
+{
+
+	private static final Random ran = new Random( 0l );
+
+	public static void main( final String[] args )
+	{
+		final int N_RUNS = 10;
+
+		System.out.println( "Creating model." );
+		final long s = System.currentTimeMillis();
+		final Model model = new CreateLargeModelExample().run( 6, 18, 8 );
+		final int size = model.getGraph().vertices().size();
+		final long e = System.currentTimeMillis();
+		System.out.println( String.format( "Created a model with %d spots in %.1f s.", size, ( ( double ) e - s ) / 1000. ) );
+
+		final int[] storage = new int[ size ];
+
+		final IntVertexFeature< Spot > POSITIVE_NUMBER = new IntVertexFeature< >( "POS_NUMBER", -1 );
+
+		final long s1 = System.currentTimeMillis();
+		for ( int i = 0; i < N_RUNS; i++ )
+		{
+			putFeatureValue( model, POSITIVE_NUMBER );
+		}
+		final long e1 = System.currentTimeMillis();
+		System.out.println( String.format( "Put a int feature value in %d spots in %.1f ms.", size, ( ( double ) e1 - s1 ) / N_RUNS ) );
+
+		final long s2 = System.currentTimeMillis();
+		for ( int i = 0; i < N_RUNS; i++ )
+		{
+			readGraphValue( model, storage );
+		}
+		final long e2 = System.currentTimeMillis();
+		System.out.println( String.format( "Read a int graph value in %d spots in %.1f ms.", size, ( ( double ) e2 - s2 ) / N_RUNS ) );
+
+		final long s3 = System.currentTimeMillis();
+		for ( int i = 0; i < N_RUNS; i++ )
+		{
+			readFeatureValue( model, storage, POSITIVE_NUMBER );
+		}
+		final long e3 = System.currentTimeMillis();
+		System.out.println( String.format( "Read a int feature value in %d spots in %.1f ms.", size, ( ( double ) e3 - s3 ) / N_RUNS ) );
+
+	}
+
+	private static void readFeatureValue( final Model model, final int[] storage, final IntVertexFeature< Spot > feature )
+	{
+		int index = 0;
+		for ( final Spot spot : model.getGraph().vertices() )
+		{
+			storage[ index++ ] = spot.feature( feature ).getInt();
+		}
+	}
+
+	private static void putFeatureValue( final Model model, final IntVertexFeature< Spot > feature )
+	{
+		for ( final Spot spot : model.getGraph().vertices() )
+		{
+			spot.feature( feature ).set( ran.nextInt() );
+		}
+	}
+
+	private static void readGraphValue( final Model model, final int[] storage )
+	{
+		int index = 0;
+		for ( final Spot spot : model.getGraph().vertices() )
+		{
+			storage[ index++ ] = spot.getTimepoint();
+		}
+	}
+
+}
