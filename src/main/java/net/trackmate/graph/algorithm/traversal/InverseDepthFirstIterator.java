ba565e999f5d3b1bb4d2ba278bae83c8096717e6
Jean-Yves Tinevez <tinevez@pasteur.fr>
2016-06-06 18:26:49 +0200

    An inverted depth-first graph iterator.

---
+++ src/main/java/net/trackmate/graph/algorithm/traversal/InverseDepthFirstIterator.java
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package net.trackmate.graph.algorithm.traversal;
+
+import net.trackmate.graph.Edge;
+import net.trackmate.graph.Graph;
+import net.trackmate.graph.Vertex;
+
+/**
+ * Same as {@link DepthFirstIterator} on a graph where all directed edges are
+ * pointing in the opposite direction.
+ *
+ * @author Jean-Yves Tinevez &ltjeanyves.tinevez@gmail.com&gt
+ */
+public class InverseDepthFirstIterator< V extends Vertex< E >, E extends Edge< V > > extends DepthFirstIterator< V, E >
+{
+	public InverseDepthFirstIterator( final V root, final Graph< V, E > graph )
+	{
+		super( root, graph );
+	}
+
+	@Override
+	protected Iterable< E > neighbors( final V vertex )
+	{
+		return vertex.incomingEdges();
+	}
+
+	@Override
+	protected V targetOf( E edge, V ref )
+	{
+		return edge.getSource( ref );
+	}
+}
