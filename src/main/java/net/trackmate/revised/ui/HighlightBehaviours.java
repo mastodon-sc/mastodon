50be2cb3d8f4559f4cea3f9312bbe7482674bf29
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-07-01 18:32:28 +0200

    Introduce GraphChangeNotifier interface and let HighlightBehaviours depend on that. Also Let HighlightBehaviours take a String[] keyConfigContexts argument such that triggers can be overridden and configured independently for "bdv" and "ts" contexts.

--- src/main/java/net/trackmate/revised/ui/HighlightBehaviours.java
+++ src/main/java/net/trackmate/revised/ui/HighlightBehaviours.java
@@ -5,6 +5,7 @@ import org.scijava.ui.behaviour.io.InputTriggerConfig;
 
 import bdv.viewer.TriggerBehaviourBindings;
 import net.trackmate.graph.Edge;
+import net.trackmate.graph.GraphChangeNotifier;
 import net.trackmate.graph.ListenableGraph;
 import net.trackmate.graph.Vertex;
 import net.trackmate.revised.bdv.AbstractBehaviours;
@@ -21,8 +22,8 @@ public class HighlightBehaviours< V extends Vertex< E >, E extends Edge< V > >
 		extends AbstractBehaviours
 {
 
-	private static final String REMOVE_HIGHLIGHTED_VERTEX = "remove highlight vertex";
-	private static final String REMOVE_HIGHLIGHTED_EDGE = "remove highlight edge";
+	private static final String REMOVE_HIGHLIGHTED_VERTEX = "remove highlighted vertex";
+	private static final String REMOVE_HIGHLIGHTED_EDGE = "remove highlighted edge";
 
 	private static final String[] REMOVE_HIGHLIGHTED_VERTEX_KEYS = new String[] { "D" };
 	private static final String[] REMOVE_HIGHLIGHTED_EDGE_KEYS = new String[] { "D" };
@@ -30,77 +31,73 @@ public class HighlightBehaviours< V extends Vertex< E >, E extends Edge< V > >
 	public static < V extends Vertex< E >, E extends Edge< V > > void installActionBindings(
 			final TriggerBehaviourBindings triggerBehaviourBindings,
 			final InputTriggerConfig config,
+			final String[] keyConfigContexts,
 			final ListenableGraph< V, E > graph,
+			final GraphChangeNotifier notify,
 			final HighlightModel< V, E > highlight,
 			final UndoPointMarker undo )
 	{
-		new HighlightBehaviours<>( triggerBehaviourBindings, config, graph, highlight, undo );
+		new HighlightBehaviours<>( triggerBehaviourBindings, config, keyConfigContexts, graph, notify, highlight, undo );
 	}
 
-	private final UndoPointMarker undo;
+	private final ListenableGraph< V, E > graph;
+
+	private final GraphChangeNotifier notify;
 
 	private final HighlightModel< V, E > highlight;
 
-	private final ListenableGraph< V, E > graph;
+	private final UndoPointMarker undo;
 
 	private HighlightBehaviours(
 			final TriggerBehaviourBindings triggerBehaviourBindings,
 			final InputTriggerConfig config,
+			final String[] keyConfigContexts,
 			final ListenableGraph< V, E > graph,
+			final GraphChangeNotifier notify,
 			final HighlightModel< V, E > highlight,
 			final UndoPointMarker undo )
 	{
-		super( triggerBehaviourBindings, "highlight", config, new String[] {} );
+		super( triggerBehaviourBindings, "highlight", config, keyConfigContexts );
 		this.graph = graph;
+		this.notify = notify;
 		this.highlight = highlight;
 		this.undo = undo;
 
-		// Behaviours
 		behaviour( new RemoveHighlightedVertex(), REMOVE_HIGHLIGHTED_VERTEX, REMOVE_HIGHLIGHTED_VERTEX_KEYS );
 		behaviour( new RemoveHighlightedEdge(), REMOVE_HIGHLIGHTED_EDGE, REMOVE_HIGHLIGHTED_EDGE_KEYS );
 	}
 
 	private class RemoveHighlightedVertex implements ClickBehaviour
 	{
-		private final V ref;
-
-		public RemoveHighlightedVertex()
-		{
-			ref = graph.vertexRef();
-		}
-
 		@Override
 		public void click( final int x, final int y )
 		{
+			final V ref = graph.vertexRef();
 			final V v = highlight.getHighlightedVertex( ref );
 			if ( v != null )
 			{
 				graph.remove( v );
 				undo.setUndoPoint();
-				graph.notifyGraphChanged();
+				notify.notifyGraphChanged();
 			}
+			graph.releaseRef( ref );
 		}
 	}
 
 	private class RemoveHighlightedEdge implements ClickBehaviour
 	{
-		private final E ref;
-
-		public RemoveHighlightedEdge()
-		{
-			ref = graph.edgeRef();
-		}
-
 		@Override
 		public void click( final int x, final int y )
 		{
+			final E ref = graph.edgeRef();
 			final E e = highlight.getHighlightedEdge( ref );
 			if ( e != null )
 			{
 				graph.remove( e );
 				undo.setUndoPoint();
-				graph.notifyGraphChanged();
+				notify.notifyGraphChanged();
 			}
+			graph.releaseRef( ref );
 		}
 	}
 
