ec86d98f2521057d30a401bcbc6adc7513f22fe4
Jean-Yves Tinevez <tinevez@pasteur.fr>
2016-07-05 00:06:56 -0400

    Return a Selection with example dummy graphs.

--- src/main/java/net/trackmate/revised/trackscheme/display/ui/dummygraph/DummyGraph.java
+++ src/main/java/net/trackmate/revised/trackscheme/display/ui/dummygraph/DummyGraph.java
@@ -1,13 +1,17 @@
 package net.trackmate.revised.trackscheme.display.ui.dummygraph;
 
+import gnu.trove.map.hash.TIntObjectHashMap;
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 
-import gnu.trove.map.hash.TIntObjectHashMap;
 import net.trackmate.graph.GraphChangeListener;
 import net.trackmate.graph.GraphIdBimap;
 import net.trackmate.graph.GraphListener;
 import net.trackmate.graph.ListenableGraph;
 import net.trackmate.graph.object.AbstractObjectGraph;
+import net.trackmate.revised.ui.selection.Selection;
 
 public class DummyGraph extends AbstractObjectGraph< DummyVertex, DummyEdge > implements ListenableGraph< DummyVertex, DummyEdge >
 {
@@ -100,71 +104,108 @@ public class DummyGraph extends AbstractObjectGraph< DummyVertex, DummyEdge > im
 	 * STATIC EXAMPLE
 	 */
 
-	public static DummyGraph example()
+	public static enum Examples
 	{
-		final DummyGraph graph = new DummyGraph();
+		CELEGANS( CElegansExample.graph, CElegansExample.selectedVertices, CElegansExample.selectedEdges );
 
-		final DummyVertex AB = graph.addVertex().init( "AB", 0 );
+		private final DummyGraph graph;
 
-		final DummyVertex ABa = graph.addVertex().init( "AB.a", 1 );
-		final DummyVertex ABp = graph.addVertex().init( "AB.p", 1 );
-		graph.addEdge( AB, ABa );
-		graph.addEdge( AB, ABp );
+		private Selection< DummyVertex, DummyEdge > selection;
 
-		final DummyVertex ABal = graph.addVertex().init( "AB.al", 2 );
-		final DummyVertex ABar = graph.addVertex().init( "AB.ar", 2 );
-		graph.addEdge( ABa, ABal );
-		graph.addEdge( ABa, ABar );
+		private Examples( DummyGraph graph, Collection< DummyVertex > vertices, Collection< DummyEdge > edges )
+		{
+			this.graph = graph;
+			this.selection = new Selection<>( graph, graph.idmap );
+			selection.setEdgesSelected( edges, true );
+			selection.setVerticesSelected( vertices, true );
+		}
 
-		final DummyVertex ABpl = graph.addVertex().init( "AB.pl", 2 );
-		final DummyVertex ABpr = graph.addVertex().init( "AB.pr", 2 );
-		graph.addEdge( ABp, ABpl );
-		graph.addEdge( ABp, ABpr );
+		public DummyGraph getGraph()
+		{
+			return graph;
+		}
 
-		final DummyVertex ABala = graph.addVertex().init( "AB.ala", 3 );
-		final DummyVertex ABalp = graph.addVertex().init( "AB.alp", 3 );
-		graph.addEdge( ABal, ABala );
-		graph.addEdge( ABal, ABalp );
-
-		final DummyVertex ABara = graph.addVertex().init( "AB.ara", 3 );
-		final DummyVertex ABarp = graph.addVertex().init( "AB.arp", 3 );
-		graph.addEdge( ABar, ABara );
-		graph.addEdge( ABar, ABarp );
-
-		final DummyVertex ABpla = graph.addVertex().init( "AB.pla", 3 );
-		final DummyVertex ABplp = graph.addVertex().init( "AB.plp", 3 );
-		graph.addEdge( ABpl, ABpla );
-		graph.addEdge( ABpl, ABplp );
-
-		final DummyVertex ABpra = graph.addVertex().init( "AB.pra", 3 );
-		final DummyVertex ABprp = graph.addVertex().init( "AB.prp", 3 );
-		graph.addEdge( ABpr, ABpra );
-		graph.addEdge( ABpr, ABprp );
-
-		final DummyVertex P1 = graph.addVertex().init( "P1", 0 );
-
-		final DummyVertex P2 = graph.addVertex().init( "P2", 1 );
-		final DummyVertex EMS = graph.addVertex().init( "EMS", 1 );
-		graph.addEdge( P1, P2 );
-		graph.addEdge( P1, EMS );
-
-		final DummyVertex P3 = graph.addVertex().init( "P3", 2 );
-		graph.addEdge( P2, P3 );
-		final DummyVertex P4 = graph.addVertex().init( "P4", 3 );
-		graph.addEdge( P3, P4 );
-		final DummyVertex Z2 = graph.addVertex().init( "Z2", 4 );
-		graph.addEdge( P4, Z2 );
-		final DummyVertex Z2a = graph.addVertex().init( "Z2.a", 5 );
-		graph.addEdge( Z2, Z2a );
-
-		final DummyVertex E = graph.addVertex().init( "E", 3 );
-		final DummyVertex MS = graph.addVertex().init( "MS", 3 );
-		graph.addEdge( EMS, E );
-		graph.addEdge( EMS, MS );
-
-		return graph;
-
+		public Selection< DummyVertex, DummyEdge > getSelection()
+		{
+			return selection;
+		}
 	}
+
+	private static final class CElegansExample
+	{
+		private static final DummyGraph graph;
+
+		private static final Collection< DummyVertex > selectedVertices;
+
+		private static final Collection< DummyEdge > selectedEdges;
+
+		static
+		{
+			graph = new DummyGraph();
+
+			final DummyVertex AB = graph.addVertex().init( "AB", 0 );
+
+			final DummyVertex ABa = graph.addVertex().init( "AB.a", 1 );
+			final DummyVertex ABp = graph.addVertex().init( "AB.p", 1 );
+			graph.addEdge( AB, ABa );
+			graph.addEdge( AB, ABp );
+
+			final DummyVertex ABal = graph.addVertex().init( "AB.al", 2 );
+			final DummyVertex ABar = graph.addVertex().init( "AB.ar", 2 );
+			final DummyEdge l1 = graph.addEdge( ABa, ABal );
+			final DummyEdge l2 = graph.addEdge( ABa, ABar );
+
+			final DummyVertex ABpl = graph.addVertex().init( "AB.pl", 2 );
+			final DummyVertex ABpr = graph.addVertex().init( "AB.pr", 2 );
+			graph.addEdge( ABp, ABpl );
+			graph.addEdge( ABp, ABpr );
+
+			final DummyVertex ABala = graph.addVertex().init( "AB.ala", 3 );
+			final DummyVertex ABalp = graph.addVertex().init( "AB.alp", 3 );
+			final DummyEdge l3 = graph.addEdge( ABal, ABala );
+			final DummyEdge l4 = graph.addEdge( ABal, ABalp );
+
+			final DummyVertex ABara = graph.addVertex().init( "AB.ara", 3 );
+			final DummyVertex ABarp = graph.addVertex().init( "AB.arp", 3 );
+			final DummyEdge l5 = graph.addEdge( ABar, ABara );
+			final DummyEdge l6 = graph.addEdge( ABar, ABarp );
+
+			final DummyVertex ABpla = graph.addVertex().init( "AB.pla", 3 );
+			final DummyVertex ABplp = graph.addVertex().init( "AB.plp", 3 );
+			graph.addEdge( ABpl, ABpla );
+			graph.addEdge( ABpl, ABplp );
+
+			final DummyVertex ABpra = graph.addVertex().init( "AB.pra", 3 );
+			final DummyVertex ABprp = graph.addVertex().init( "AB.prp", 3 );
+			graph.addEdge( ABpr, ABpra );
+			graph.addEdge( ABpr, ABprp );
+
+			final DummyVertex P1 = graph.addVertex().init( "P1", 0 );
+
+			final DummyVertex P2 = graph.addVertex().init( "P2", 1 );
+			final DummyVertex EMS = graph.addVertex().init( "EMS", 1 );
+			graph.addEdge( P1, P2 );
+			graph.addEdge( P1, EMS );
+
+			final DummyVertex P3 = graph.addVertex().init( "P3", 2 );
+			graph.addEdge( P2, P3 );
+			final DummyVertex P4 = graph.addVertex().init( "P4", 3 );
+			graph.addEdge( P3, P4 );
+			final DummyVertex Z2 = graph.addVertex().init( "Z2", 4 );
+			graph.addEdge( P4, Z2 );
+			final DummyVertex Z2a = graph.addVertex().init( "Z2.a", 5 );
+			graph.addEdge( Z2, Z2a );
+
+			final DummyVertex E = graph.addVertex().init( "E", 3 );
+			final DummyVertex MS = graph.addVertex().init( "MS", 3 );
+			graph.addEdge( EMS, E );
+			graph.addEdge( EMS, MS );
+
+			selectedVertices = Arrays.asList( new DummyVertex[] { ABa, ABal, ABala, ABalp, ABar, ABara, ABarp } );
+			selectedEdges = Arrays.asList( new DummyEdge[] { l1, l2, l3, l4, l5, l6 } );
+		}
+	}
+
 
 	@Override
 	public boolean addGraphListener( final GraphListener< DummyVertex, DummyEdge > listener )
