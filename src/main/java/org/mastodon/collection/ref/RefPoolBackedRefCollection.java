b5eab12f54b0a04b7c04e268377359b256828afa
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-09-05 14:07:34 +0200

    javadoc

--- src/main/java/org/mastodon/collection/ref/RefPoolBackedRefCollection.java
+++ src/main/java/org/mastodon/collection/ref/RefPoolBackedRefCollection.java
@@ -3,6 +3,14 @@ package org.mastodon.collection.ref;
 import org.mastodon.RefPool;
 import org.mastodon.collection.RefCollection;
 
+/**
+ * A {@link RefCollection} that contains objects stored in a {@link RefPool}.
+ *
+ * @param <O>
+ *            the type of object stored in the collection.
+ *
+ * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
+ */
 public interface RefPoolBackedRefCollection< O > extends RefCollection< O >
 {
 	public RefPool< O > getRefPool();
