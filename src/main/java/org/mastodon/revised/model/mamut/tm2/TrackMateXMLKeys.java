f5b28e5cf72e572b7fa5abe485755707aa1347e5
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2016-09-17 00:00:00 +0200

    Hardcode some XML keys required to read TrackMate files.
    Not to depend on TrackMate.

---
+++ src/main/java/org/mastodon/revised/model/mamut/tm2/TrackMateXMLKeys.java
@@ -0,0 +1,34 @@
+package org.mastodon.revised.model.mamut.tm2;
+
+public class TrackMateXMLKeys
+{
+
+	public static final String TRACKMATE_TAG = "TrackMate";
+
+	public static final String MODEL_TAG = "Model";
+
+	public static final String SPOT_COLLECTION_TAG = "AllSpots";
+
+	public static final String SPOT_FRAME_COLLECTION_TAG = "SpotsInFrame";
+
+	public static final String SPOT_ELEMENT_TAG = "Spot";
+
+	public static final String VISIBILITY_ATTRIBUTE_NAME = "VISIBILITY";
+
+	public static final String X_ATTRIBUTE_NAME = "POSITION_X";
+
+	public static final String Y_ATTRIBUTE_NAME = "POSITION_Y";
+
+	public static final String Z_ATTRIBUTE_NAME = "POSITION_Z";
+
+	public static final String RADIUS_ATTRIBUTE_NAME = "RADIUS";
+
+	public static final String FRAME_ATTRIBUTE_NAME = "FRAME";
+
+	public static final String ID_ATTRIBUTE_NAME = "ID";
+
+	public static final String LABEL_ATTRIBUTE_NAME = "name";
+
+	private TrackMateXMLKeys()
+	{}
+}
