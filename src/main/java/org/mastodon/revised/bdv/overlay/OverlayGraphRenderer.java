c75b314adc86b192559ddff479bd6100f9ef3b10
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-09-05 14:11:41 +0200

    Move ConvexPolytope methods to utility class.
    # Conflicts:
    #	src/main/java/org/mastodon/revised/bdv/overlay/OverlayGraphRenderer.java

--- src/main/java/org/mastodon/revised/bdv/overlay/OverlayGraphRenderer.java
+++ src/main/java/org/mastodon/revised/bdv/overlay/OverlayGraphRenderer.java
@@ -23,15 +23,14 @@ import org.mastodon.model.HighlightModel;
 import org.mastodon.model.SelectionModel;
 import org.mastodon.revised.Util;
 import org.mastodon.revised.bdv.overlay.ScreenVertexMath.Ellipse;
+import org.mastodon.revised.bdv.overlay.util.BdvRendererUtil;
 import org.mastodon.spatial.SpatialIndex;
 import org.mastodon.spatial.SpatioTemporalIndex;
 
 import bdv.util.Affine3DHelpers;
 import bdv.viewer.TimePointListener;
-import net.imglib2.RealInterval;
 import net.imglib2.RealPoint;
 import net.imglib2.algorithm.kdtree.ConvexPolytope;
-import net.imglib2.algorithm.kdtree.HyperPlane;
 import net.imglib2.neighborsearch.NearestNeighborSearch;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.ARGBType;
@@ -349,67 +348,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 	}
 
 	/**
-	 * Gets the {@link ConvexPolytope} described by the specified interval in
-	 * viewer coordinates, transformed to global coordinates.
-	 *
-	 * @param transform
-	 *            the transform to transform viewer coordinates back in global
-	 *            coordinates.
-	 * @param viewerInterval
-	 *            the view interval.
-	 * @return {@code viewerInterval} transformed to global coordinates.
-	 */
-	// TODO: move to Utility class
-	public static ConvexPolytope getPolytopeGlobal(
-			final AffineTransform3D transform,
-			final RealInterval viewerInterval )
-	{
-		return getPolytopeGlobal( transform,
-				viewerInterval.realMin( 0 ), viewerInterval.realMax( 0 ),
-				viewerInterval.realMin( 1 ), viewerInterval.realMax( 1 ),
-				viewerInterval.realMin( 2 ), viewerInterval.realMax( 2 ) );
-	}
-
-	/**
-	 * Gets the {@link ConvexPolytope} described by the specified interval in
-	 * viewer coordinates, transformed to global coordinates.
-	 *
-	 * @param transform
-	 *            the transform to transform viewer coordinates back in global
-	 *            coordinates.
-	 * @param viewerMinX
-	 *            the x min bound of the view interval.
-	 * @param viewerMaxX
-	 *            the x max bound of the view interval.
-	 * @param viewerMinY
-	 *            the y min bound of the view interval.
-	 * @param viewerMaxY
-	 *            the y max bound of the view interval.
-	 * @param viewerMinZ
-	 *            the z min bound of the view interval.
-	 * @param viewerMaxZ
-	 *            the z max bound of the view interval.
-	 * @return the specified viewer interval, transformed to global coordinates.
-	 */
-	// TODO: move to Utility class
-	public static ConvexPolytope getPolytopeGlobal(
-			final AffineTransform3D transform,
-			final double viewerMinX, final double viewerMaxX,
-			final double viewerMinY, final double viewerMaxY,
-			final double viewerMinZ, final double viewerMaxZ )
-	{
-		final ConvexPolytope polytopeViewer = new ConvexPolytope(
-				new HyperPlane(  1,  0,  0, viewerMinX ),
-				new HyperPlane( -1,  0,  0, -viewerMaxX ),
-				new HyperPlane(  0,  1,  0, viewerMinY ),
-				new HyperPlane(  0, -1,  0, -viewerMaxY ),
-				new HyperPlane(  0,  0,  1, viewerMinZ),
-				new HyperPlane(  0,  0, -1, -viewerMaxZ ) );
-		final ConvexPolytope polytopeGlobal = ConvexPolytope.transform( polytopeViewer, transform.inverse() );
-		return polytopeGlobal;
-	}
-
-	/**
 	 * Get the {@link ConvexPolytope} around the specified viewer coordinate
 	 * range that is large enough border to ensure that it contains center of
 	 * every ellipsoid touching the specified coordinate range.
@@ -439,7 +377,7 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 				? focusLimit
 				: focusLimit * globalToViewerScale;
 		final double border = globalToViewerScale * Math.sqrt( graph.getMaxBoundingSphereRadiusSquared( timepoint ) );
-		return getPolytopeGlobal( transform,
+		return BdvRendererUtil.getPolytopeGlobal( transform,
 				xMin - border, xMax + border,
 				yMin - border, yMax + border,
 				-maxDepth - border, maxDepth + border );
