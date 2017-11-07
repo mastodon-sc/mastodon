c2040afb16cb5abd090f4ef3a8adba1d52641946
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2017-11-08 00:53:25 +0100

    Fix concurrent modification of TrackSchemeStyle update listeners.
    Some listeners fire events that end up in removing them from the
    listener list. For instance when:
    - a branch graph has a certain style
    - this style is modified in the style editor
    - listeners are notified
    - the style setter action of the branch graph is notified
    - the modification makes the style unsuited for branch graph
    - the branch graph then defaults to the default sttyle
    - doing so it de-register itself as a listener of the initial style
    - which triggers a concurrent modification while iterating through
    all listeners.
    
    To avoid this, we simply duplicate the list of listeners before
    iterating the copy.

--- src/main/java/org/mastodon/revised/trackscheme/display/laf/TrackSchemeStyle.java
+++ src/main/java/org/mastodon/revised/trackscheme/display/laf/TrackSchemeStyle.java
@@ -682,7 +682,8 @@ public class TrackSchemeStyle
 
 	private void notifyListeners()
 	{
-		for ( final UpdateListener l : updateListeners )
+		final ArrayList< UpdateListener > ul = new ArrayList<>( updateListeners ); // TODO: bake into Listeners class
+		for ( final UpdateListener l : ul )
 			l.trackSchemeStyleChanged();
 	}
 
