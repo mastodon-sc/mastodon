a5e1fe430eeb29efbb3f7d8de7d417a096b8a3ae
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-11-15 01:40:26 +0100

    PreferencesDialog behaves correctly when OK/Cancel are clicked

--- src/main/java/org/mastodon/revised/mamut/PreferencesDialog.java
+++ src/main/java/org/mastodon/revised/mamut/PreferencesDialog.java
@@ -2,8 +2,11 @@ package org.mastodon.revised.mamut;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JDialog;
+import javax.swing.WindowConstants;
 
 import org.mastodon.app.ui.settings.SettingsPage;
 import org.mastodon.app.ui.settings.SettingsPanel;
@@ -18,6 +21,19 @@ public class PreferencesDialog extends JDialog
 	{
 		super( owner, "Preferences", false );
 		settingsPanel = new SettingsPanel();
+		settingsPanel.onOk( () -> setVisible( false ) );
+		settingsPanel.onCancel( () -> setVisible( false ) );
+
+		setDefaultCloseOperation( WindowConstants.DO_NOTHING_ON_CLOSE );
+		addWindowListener( new WindowAdapter()
+		{
+			@Override
+			public void windowClosing( final WindowEvent e )
+			{
+				settingsPanel.cancel();
+			}
+		} );
+
 		getContentPane().add( settingsPanel, BorderLayout.CENTER );
 		pack();
 	}
