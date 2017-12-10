189c083cfdd84715d2a0b5f46bfab9adfdf2b790
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-12-10 13:31:33 +0100

    Add select + navigate FocusActions

--- src/main/java/org/mastodon/revised/ui/FocusActions.java
+++ src/main/java/org/mastodon/revised/ui/FocusActions.java
@@ -10,9 +10,21 @@ import org.mastodon.graph.Vertex;
 import org.mastodon.model.FocusModel;
 import org.mastodon.model.NavigationHandler;
 import org.mastodon.model.SelectionModel;
+import org.mastodon.revised.trackscheme.display.TrackSchemeNavigationActions.NavigatorEtiquette;
 import org.scijava.ui.behaviour.util.Actions;
 import org.scijava.ui.behaviour.util.RunnableAction;
 
+/**
+ * Generic tree navigation actions.
+ * Implements {@link NavigatorEtiquette#FINDER_LIKE} flavour.
+ *
+ * @param <V>
+ *     vertex type.
+ * @param <E>
+ *     edge type.
+ *
+ * @author Tobias Pietzsch
+ */
 public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V > & Ref< E > >
 {
 	public static final String NAVIGATE_CHILD = "navigate to child";
@@ -20,22 +32,33 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 	public static final String NAVIGATE_SIBLING = "navigate to sibling";
 	public static final String NAVIGATE_BRANCH_CHILD = "navigate to branch child";
 	public static final String NAVIGATE_BRANCH_PARENT = "navigate to branch parent";
+	public static final String SELECT_NAVIGATE_CHILD = "select navigate to child";
+	public static final String SELECT_NAVIGATE_PARENT = "select navigate to parent";
+	public static final String SELECT_NAVIGATE_SIBLING = "select navigate to sibling";
+	public static final String SELECT_NAVIGATE_BRANCH_CHILD = "select navigate to branch child";
+	public static final String SELECT_NAVIGATE_BRANCH_PARENT = "select navigate to branch parent";
 
-	public static final String[] NAVIGATE_CHILD_KEYS = new String[] { "ctrl DOWN" };
-	public static final String[] NAVIGATE_PARENT_KEYS = new String[] { "ctrl UP" };
-	public static final String[] NAVIGATE_SIBLING_KEYS = new String[] { "ctrl LEFT", "ctrl RIGHT" };
-	public static final String[] NAVIGATE_BRANCH_CHILD_KEYS = new String[] { "ctrl shift DOWN" };
-	public static final String[] NAVIGATE_BRANCH_PARENT_KEYS = new String[] { "ctrl shift UP" };
+	private static final String[] NAVIGATE_CHILD_KEYS = new String[] { "ctrl DOWN" };
+	private static final String[] NAVIGATE_PARENT_KEYS = new String[] { "ctrl UP" };
+	private static final String[] NAVIGATE_SIBLING_KEYS = new String[] { "ctrl LEFT", "ctrl RIGHT" };
+	private static final String[] NAVIGATE_BRANCH_CHILD_KEYS = new String[] { "ctrl alt DOWN" };
+	private static final String[] NAVIGATE_BRANCH_PARENT_KEYS = new String[] { "ctrl alt UP" };
+	private static final String[] SELECT_NAVIGATE_CHILD_KEYS = new String[] { "shift ctrl DOWN" };
+	private static final String[] SELECT_NAVIGATE_PARENT_KEYS = new String[] { "shift ctrl UP" };
+	private static final String[] SELECT_NAVIGATE_SIBLING_KEYS = new String[] { "shift ctrl LEFT", "shift ctrl RIGHT" };
+	private static final String[] SELECT_NAVIGATE_BRANCH_CHILD_KEYS = new String[] { "shift ctrl alt DOWN" };
+	private static final String[] SELECT_NAVIGATE_BRANCH_PARENT_KEYS = new String[] { "shift ctrl alt UP" };
 
 	private final RunnableAction navigateToChildAction;
-
 	private final RunnableAction navigateToParentAction;
-
 	private final RunnableAction navigateToSiblingAction;
-
 	private final RunnableAction navigateToBranchChildAction;
-
 	private final RunnableAction navigateToBranchParentAction;
+	private final RunnableAction selectNavigateToChildAction;
+	private final RunnableAction selectNavigateToParentAction;
+	private final RunnableAction selectNavigateToSiblingAction;
+	private final RunnableAction selectNavigateToBranchChildAction;
+	private final RunnableAction selectNavigateToBranchParentAction;
 
 	// TODO: rename to "install" (in all similar classes)
 	public static < V extends Vertex< E > & Ref< V >, E extends Edge< V > & Ref< E > > void install(
@@ -53,6 +76,11 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 		actions.namedAction( fa.navigateToSiblingAction, NAVIGATE_SIBLING_KEYS );
 		actions.namedAction( fa.navigateToBranchChildAction, NAVIGATE_BRANCH_CHILD_KEYS );
 		actions.namedAction( fa.navigateToBranchParentAction, NAVIGATE_BRANCH_PARENT_KEYS );
+		actions.namedAction( fa.selectNavigateToChildAction, SELECT_NAVIGATE_CHILD_KEYS );
+		actions.namedAction( fa.selectNavigateToParentAction, SELECT_NAVIGATE_PARENT_KEYS );
+		actions.namedAction( fa.selectNavigateToSiblingAction, SELECT_NAVIGATE_SIBLING_KEYS );
+		actions.namedAction( fa.selectNavigateToBranchChildAction, SELECT_NAVIGATE_BRANCH_CHILD_KEYS );
+		actions.namedAction( fa.selectNavigateToBranchParentAction, SELECT_NAVIGATE_BRANCH_PARENT_KEYS );
 	}
 
 	private enum Direction
@@ -89,22 +117,27 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 		navigateToChildAction = new RunnableAction( NAVIGATE_CHILD, () -> selectAndFocusNeighbor( Direction.CHILD, false ) );
 		navigateToParentAction = new RunnableAction( NAVIGATE_PARENT, () -> selectAndFocusNeighbor( Direction.PARENT, false ) );
 		navigateToSiblingAction = new RunnableAction( NAVIGATE_SIBLING, () -> selectAndFocusNeighbor( Direction.SIBLING, false ) );
-		navigateToBranchChildAction = new RunnableAction( NAVIGATE_BRANCH_CHILD, () -> selectAndFocusBranchNeighbor( Direction.CHILD, false )  );
-		navigateToBranchParentAction = new RunnableAction( NAVIGATE_BRANCH_PARENT, () -> selectAndFocusBranchNeighbor( Direction.PARENT, false )  );
+		navigateToBranchChildAction = new RunnableAction( NAVIGATE_BRANCH_CHILD, () -> selectAndFocusBranchNeighbor( Direction.CHILD, false ) );
+		navigateToBranchParentAction = new RunnableAction( NAVIGATE_BRANCH_PARENT, () -> selectAndFocusBranchNeighbor( Direction.PARENT, false ) );
+		selectNavigateToChildAction = new RunnableAction( SELECT_NAVIGATE_CHILD, () -> selectAndFocusNeighbor( Direction.CHILD, true ) );
+		selectNavigateToParentAction = new RunnableAction( SELECT_NAVIGATE_PARENT, () -> selectAndFocusNeighbor( Direction.PARENT, true ) );
+		selectNavigateToSiblingAction = new RunnableAction( SELECT_NAVIGATE_SIBLING, () -> selectAndFocusNeighbor( Direction.SIBLING, true ) );
+		selectNavigateToBranchChildAction = new RunnableAction( SELECT_NAVIGATE_BRANCH_CHILD, () -> selectAndFocusBranchNeighbor( Direction.CHILD, true ) );
+		selectNavigateToBranchParentAction = new RunnableAction( SELECT_NAVIGATE_BRANCH_PARENT, () -> selectAndFocusBranchNeighbor( Direction.PARENT, true ) );
 	}
 
 	/**
-	 * Focus a neighbor (parent, child, sibling) of the
-	 * currently focused vertex. Possibly, the currently focused vertex is added
-	 * to the selection.
+	 * Focus and select a neighbor (parent, child, sibling)
+	 * of the currently focused vertex. The selection can be cleared before
+	 * moving focus.
 	 *
 	 * @param direction
 	 *            which neighbor to focus.
-	 * @param select
-	 *            if {@code true}, the currently focused vertex is added to the
-	 *            selection (before moving the focus).
+	 * @param expandSelection
+	 *            if {@code false}, the selection is cleared before moving the
+	 *            focus (and selecting the newly focused vertex).
 	 */
-	private void selectAndFocusNeighbor( final Direction direction, final boolean select )
+	private void selectAndFocusNeighbor( final Direction direction, final boolean expandSelection )
 	{
 		final V ref1 = graph.vertexRef();
 		final V ref2 = graph.vertexRef();
@@ -113,9 +146,6 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 			final V vertex = focus.getFocusedVertex( ref1 );
 			if ( vertex == null )
 				return;
-
-			if ( select )
-				selection.setSelected( vertex, true );
 
 			final V current;
 			switch ( direction )
@@ -126,13 +156,28 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 			case PARENT:
 				current = firstParent( vertex, ref2 );
 				break;
-			case SIBLING: default:
+			case SIBLING:
 				current = nextSibling( vertex, ref2 );
 				break;
+			default:
+				current = null;
 			}
 
 			if ( current != null )
-				navigation.notifyNavigateToVertex( current );
+			{
+				selection.pauseListeners();
+
+				// TODO!!!!!!!!!!!
+//				focus.focusVertex( current ); // TODO!!!!!!!!!!!
+				navigation.notifyNavigateToVertex( current ); // TODO!!!!!!!!!!!
+				// TODO!!!!!!!!!!!
+
+				if ( !expandSelection )
+					selection.clearSelection();
+				selection.setSelected( current, true );
+
+				selection.resumeListeners();
+			}
 		}
 		finally
 		{
@@ -141,18 +186,16 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 		}
 	}
 
-	private void selectAndFocusBranchNeighbor( final Direction direction, final boolean select )
+	private void selectAndFocusBranchNeighbor( final Direction direction, final boolean expandSelection )
 	{
 		final V ref1 = graph.vertexRef();
 		final V ref2 = graph.vertexRef();
+		final V ref3 = graph.vertexRef();
 		try
 		{
 			final V vertex = focus.getFocusedVertex( ref1 );
 			if ( vertex == null )
 				return;
-
-			if ( select )
-				selection.setSelected( vertex, true );
 
 			final V current;
 			switch ( direction )
@@ -160,18 +203,44 @@ public class FocusActions< V extends Vertex< E > & Ref< V >, E extends Edge< V >
 			case CHILD:
 				current = firstBranchChild( vertex, ref2 );
 				break;
-			case PARENT: default:
+			case PARENT:
 				current = firstBranchParent( vertex, ref2 );
 				break;
+			default:
+				current = null;
 			}
 
 			if ( current != null )
-				navigation.notifyNavigateToVertex( current );
+			{
+				selection.pauseListeners();
+
+				// TODO!!!!!!!!!!!
+//				focus.focusVertex( current ); // TODO!!!!!!!!!!!
+				navigation.notifyNavigateToVertex( current ); // TODO!!!!!!!!!!!
+				// TODO!!!!!!!!!!!
+
+				if ( expandSelection )
+				{
+					final V child = ( direction == Direction.PARENT ) ? vertex : current;
+					final V parent = ( direction == Direction.PARENT ) ? current : vertex;
+					for( V v = child; !v.equals( parent ); v = firstParent( v, ref3 ) )
+						selection.setSelected( v, true );
+					selection.setSelected( parent, true );
+				}
+				else
+				{
+					selection.clearSelection();
+					selection.setSelected( current, true );
+				}
+
+				selection.resumeListeners();
+			}
 		}
 		finally
 		{
 			graph.releaseRef( ref1 );
 			graph.releaseRef( ref2 );
+			graph.releaseRef( ref3 );
 		}
 	}
 
