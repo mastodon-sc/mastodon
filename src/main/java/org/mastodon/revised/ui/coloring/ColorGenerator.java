23952acd3bdfc8b797dda2b5b3055f6978762e08
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-02-19 14:36:51 +0100

    WIP: ColorGenerator

---
+++ src/main/java/org/mastodon/revised/ui/coloring/ColorGenerator.java
@@ -0,0 +1,26 @@
+package org.mastodon.revised.ui.coloring;
+
+/**
+ * Interface that can associate colors to objects.
+ *
+ * @param <T>
+ *            the type of objects to color.
+ *
+ * @author Jean-Yves Tinevez.
+ * @author Tobias Pietzsch
+ */
+public interface ColorGenerator< T >
+{
+	/**
+	 * Gets the color for the specified object (ARGB bytes packed into
+	 * {@code int}).
+	 * <p>
+	 * The special value {@code 0x00000000} is used to denote that no color is
+	 * assigned to the object (which should be drawn in default color then).
+	 *
+	 * @param object
+	 *            the object.
+	 * @return a color (as ARGB bytes packed into {@code int}).
+	 */
+	public int color( T object );
+}
