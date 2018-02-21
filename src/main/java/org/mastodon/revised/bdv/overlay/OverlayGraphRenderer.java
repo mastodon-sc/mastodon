ed496e1e59a78fe746698c819e371efc9f316e5c
Jean-Yves Tinevez <tinevez@pasteur.fr>
2018-02-21 14:57:58 +0100

    Do not lock the graph in getVertexAt or getEdgeAt methods of OverlayGraphRenderer.
    Only callers of these methods should lock the graph.

--- src/main/java/org/mastodon/revised/bdv/overlay/OverlayGraphRenderer.java
+++ src/main/java/org/mastodon/revised/bdv/overlay/OverlayGraphRenderer.java
@@ -12,7 +12,6 @@ import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.mastodon.collection.RefCollection;
 import org.mastodon.collection.RefCollections;
@@ -80,8 +79,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 
 	private final OverlayGraph< V, E > graph;
 
-	private final ReentrantReadWriteLock lock;
-
 	private final SpatioTemporalIndex< V > index;
 
 	private final HighlightModel< V, E > highlight;
@@ -103,7 +100,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 		this.focus = focus;
 		this.selection = selection;
 		index = graph.getIndex();
-		lock = graph.getLock();
 		renderTransform = new AffineTransform3D();
 		setRenderSettings( RenderSettings.defaultStyle() ); // default RenderSettings
 	}
@@ -411,7 +407,7 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 
 				for ( final E edge : vertex.incomingEdges() )
 				{
-					V source = edge.getSource( ref );
+					final V source = edge.getSource( ref );
 					source.localize( gPos );
 					transform.apply( gPos, lPos );
 					final int x0 = ( int ) lPos[ 0 ];
@@ -668,7 +664,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 		};
 		final Op op = new Op();
 
-		lock.readLock().lock();
 		index.readLock().lock();
 		try
 		{
@@ -677,7 +672,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 		finally
 		{
 			index.readLock().unlock();
-			lock.readLock().unlock();
 		}
 		return op.found ? ref : null;
 	}
@@ -738,7 +732,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 
 		boolean found = false;
 
-		lock.readLock().lock();
 		index.readLock().lock();
 
 		// TODO: cache searches? --> take into account that indexdata might change
@@ -817,8 +810,6 @@ public class OverlayGraphRenderer< V extends OverlayVertex< V, E >, E extends Ov
 		}
 
 		index.readLock().unlock();
-		lock.readLock().unlock();
-
 		return found ? ref : null;
 	}
 
