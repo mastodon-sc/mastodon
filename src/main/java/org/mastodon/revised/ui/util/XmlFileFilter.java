82817c24403220a1bfc8170d3bc8bca18853ce6a
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2018-03-09 14:52:07 +0100

    Add ExtensionFileFilter and make XmlFileFilter a special case

--- src/main/java/org/mastodon/revised/ui/util/XmlFileFilter.java
+++ src/main/java/org/mastodon/revised/ui/util/XmlFileFilter.java
@@ -1,32 +1,9 @@
 package org.mastodon.revised.ui.util;
 
-import java.io.File;
-
-import javax.swing.filechooser.FileFilter;
-
-public class XmlFileFilter extends FileFilter
+public class XmlFileFilter extends ExtensionFileFilter
 {
-	@Override
-	public String getDescription()
+	public XmlFileFilter()
 	{
-		return "xml files";
-	}
-
-	@Override
-	public boolean accept( final File f )
-	{
-		if ( f.isDirectory() )
-			return true;
-		if ( f.isFile() )
-		{
-			final String s = f.getName();
-			final int i = s.lastIndexOf( '.' );
-			if ( i > 0 && i < s.length() - 1 )
-			{
-				final String ext = s.substring( i + 1 ).toLowerCase();
-				return ext.equals( "xml" );
-			}
-		}
-		return false;
+		super( "xml" );
 	}
 }
