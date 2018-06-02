9494a619b2b3bd5506c7aed8c7ed86b2cfa88023
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-06-02 17:23:40 +0200

    Change the MaMut-specific feature computers.
    We want to write feature values that depend on image data (for instance
    something like spot mean intensity). For this the feature computer must
    access the BDV data. Since FeatureComputers are SciJava plugin, they must
    receive it after instantiation from a method that will be called by the
    feature computer service.
    
    - So now there is a MamutFeatureComputer abstract class that does this.
    
    - And it has facilities to generate unit strings for quantities in
    FeatureProjections, using the spatial units stored in the first Source.
    
    - And we had utility abstract classes that cover common use cases (int
    and double, scalar or array feature computers) with deserialization.

--- src/main/java/org/mastodon/revised/mamut/feature/LinkFeatureComputer.java
+++ src/main/java/org/mastodon/revised/mamut/feature/LinkFeatureComputer.java
@@ -1,13 +1,15 @@
 package org.mastodon.revised.mamut.feature;
 
-import org.mastodon.revised.model.feature.FeatureComputer;
-import org.mastodon.revised.model.mamut.Model;
-
 /**
- * Marker interface for MaMuT feature computers to sort between different
- * targets.
+ * Marker class for MaMuT feature computers to sort between different targets.
  *
  * @author Jean-Yves Tinevez
  */
-public interface LinkFeatureComputer extends FeatureComputer< Model >
-{}
+public abstract class LinkFeatureComputer extends MamutFeatureComputer
+{
+
+	protected LinkFeatureComputer( final String key )
+	{
+		super( key );
+	}
+}
