1af8678fdd3d7abe609d73715ed4f536e6c65afe
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-06-02 21:47:46 +0200

    Classes to generate color from feature values.
    - Assign default color to any objects: DefaultColorGenerator.
    
    - Assign a color to an object from a feature projection value of
    this object: FeatureColorGenerator.
    
    - Assign a color to a vertex from a feature projection value of its
    incoming or outgoing edge (if only 1): FeatureColorGeneratorIncomingEdge
    and FeatureColorGeneratorOutgoingEdge
    
    - Assign a color to an edge from a feature projection value of its
    source or target vertex: FeatureColorGeneratorSourceVertex and
    FeatureColorGeneratorTargetVertex

---
+++ src/main/java/org/mastodon/revised/ui/coloring/DefaultColorGenerator.java
@@ -0,0 +1,10 @@
+package org.mastodon.revised.ui.coloring;
+
+public class DefaultColorGenerator< T > implements ColorGenerator< T >
+{
+	@Override
+	public int color( final T t )
+	{
+		return 0;
+	}
+}
