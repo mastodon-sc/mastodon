b61531dba8edd5f36ff8abb7b3d561f8f291863d
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-06-02 17:45:25 +0200

    Fix existing Mamut feature computers.

--- src/main/java/org/mastodon/revised/mamut/feature/SpotNLinksComputer.java
+++ src/main/java/org/mastodon/revised/mamut/feature/SpotNLinksComputer.java
@@ -1,23 +1,29 @@
 package org.mastodon.revised.mamut.feature;
 
 import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 
 import org.mastodon.pool.PoolCollectionWrapper;
 import org.mastodon.properties.IntPropertyMap;
-import org.mastodon.revised.model.feature.Feature;
-import org.mastodon.revised.model.feature.FeatureProjection;
-import org.mastodon.revised.model.feature.FeatureProjectors;
+import org.mastodon.revised.model.feature.FeatureUtil;
+import org.mastodon.revised.model.feature.FeatureUtil.Dimension;
+import org.mastodon.revised.model.feature.IntScalarFeature;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.revised.model.mamut.Spot;
 import org.scijava.plugin.Plugin;
 
 @Plugin( type = SpotFeatureComputer.class, name = "Spot N links" )
-public class SpotNLinksComputer implements SpotFeatureComputer
+public class SpotNLinksComputer extends SpotIntScalarFeatureComputer
 {
 
-	private static final String KEY = "Spot N links";
+	public static final String KEY = "Spot N links";
+
+	private static final String HELP_STRING = "Computes the number of links that touch a spot.";
+
+	public SpotNLinksComputer()
+	{
+		super( KEY );
+	}
 
 	@Override
 	public Set< String > getDependencies()
@@ -32,7 +38,7 @@ public class SpotNLinksComputer implements SpotFeatureComputer
 	}
 
 	@Override
-	public Feature< Spot, IntPropertyMap< Spot > > compute( final Model model )
+	public IntScalarFeature< Spot > compute( final Model model )
 	{
 		final PoolCollectionWrapper< Spot > vertices = model.getGraph().vertices();
 		final IntPropertyMap< Spot > pm = new IntPropertyMap<>( vertices, -1 );
@@ -40,8 +46,12 @@ public class SpotNLinksComputer implements SpotFeatureComputer
 		for ( final Spot spot : vertices )
 			pm.set( spot, spot.edges().size() );
 
-		final Map< String, FeatureProjection< Spot > > projections = Collections.singletonMap( KEY, FeatureProjectors.project( pm ) );
-		final Feature< Spot, IntPropertyMap< Spot > > feature = new Feature<>( KEY, Spot.class, pm, projections );
-		return feature;
+		return new IntScalarFeature<>( KEY, Spot.class, pm, FeatureUtil.dimensionToUnits( Dimension.NONE, spaceUnits, timeUnits ) );
+	}
+
+	@Override
+	public String getHelpString()
+	{
+		return HELP_STRING;
 	}
 }
