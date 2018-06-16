3329449f4d6d1ed4b4df016a4942a82ee4a9c520
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2018-06-16 18:34:57 +0200

    Add some icons for buttons.

---
+++ src/main/java/org/mastodon/revised/mamut/MastodonIcons.java
@@ -0,0 +1,28 @@
+package org.mastodon.revised.mamut;
+
+import javax.swing.ImageIcon;
+
+public class MastodonIcons
+{
+
+	public static final ImageIcon TITLE_ICON = new ImageIcon( Mastodon.class.getResource( "Mastodon-logo-01-128x128.png" ) );
+
+	public static final ImageIcon BDV_ICON = new ImageIcon( Mastodon.class.getResource( "BDV-logo-32x32.png" ) );
+
+	public static final ImageIcon TRACKSCHEME_ICON = new ImageIcon( Mastodon.class.getResource( "TrackScheme-logo2-32x32.png" ) );
+
+	public static final ImageIcon TABLE_ICON = new ImageIcon( Mastodon.class.getResource( "Table-icon2-32x32.png" ) );
+
+	public static final ImageIcon TAG_ICON = new ImageIcon( Mastodon.class.getResource( "Tag-icon-32x32.png" ) );
+
+	public static final ImageIcon FEATURE_ICON = new ImageIcon( Mastodon.class.getResource( "Feature-icon-32x32.png" ) );
+
+	public static final ImageIcon SAVE_ICON = new ImageIcon( Mastodon.class.getResource( "Save-icon-32x32.png" ) );
+
+	public static final ImageIcon SAVEAS_ICON = new ImageIcon( Mastodon.class.getResource( "SaveAs-icon-32x32.png" ) );
+
+	public static final ImageIcon MAMUT_ICON = new ImageIcon( Mastodon.class.getResource( "MaMuT-icon-32x32.png" ) );
+
+	private MastodonIcons()
+	{}
+}
