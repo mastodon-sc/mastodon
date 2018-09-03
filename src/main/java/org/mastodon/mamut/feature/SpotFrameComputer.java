bcbb79379cad0ecf627b177332337518d2cc9459
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2018-09-03 21:28:56 +0200

    Put the feature specs in the feature class.

--- src/main/java/org/mastodon/mamut/feature/SpotFrameComputer.java
+++ src/main/java/org/mastodon/mamut/feature/SpotFrameComputer.java
@@ -2,8 +2,6 @@ package org.mastodon.mamut.feature;
 
 import static org.scijava.ItemIO.OUTPUT;
 
-import org.mastodon.feature.FeatureSpec;
-import org.mastodon.revised.model.mamut.Spot;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
@@ -11,19 +9,8 @@ import org.scijava.plugin.Plugin;
 public class SpotFrameComputer implements MamutFeatureComputer
 {
 
-	private static final String KEY = "Spot frame";
-
 	@Parameter( type = OUTPUT )
 	private SpotFrameFeature output;
-
-	@Plugin( type = FeatureSpec.class )
-	public static class Spec extends FeatureSpec< SpotFrameFeature, Spot >
-	{
-		public Spec()
-		{
-			super( KEY, SpotFrameFeature.class, Spot.class, KEY );
-		}
-	}
 
 	@Override
 	public void createOutput()
--- src/main/java/org/mastodon/mamut/feature/SpotFrameFeature.java
+++ src/main/java/org/mastodon/mamut/feature/SpotFrameFeature.java
@@ -2,13 +2,27 @@ package org.mastodon.mamut.feature;
 
 import org.mastodon.feature.Feature;
 import org.mastodon.feature.FeatureProjection;
+import org.mastodon.feature.FeatureSpec;
 import org.mastodon.feature.IntFeatureProjection;
 import org.mastodon.revised.model.mamut.Spot;
+import org.scijava.plugin.Plugin;
 
 public class SpotFrameFeature implements Feature< Spot >
 {
+
+	private static final String KEY = "Spot frame";
+
 	SpotFrameFeature()
 	{}
+
+	@Plugin( type = FeatureSpec.class )
+	public static class Spec extends FeatureSpec< SpotFrameFeature, Spot >
+	{
+		public Spec()
+		{
+			super( KEY, SpotFrameFeature.class, Spot.class, KEY );
+		}
+	}
 
 	@Override
 	public FeatureProjection< Spot > project( final String projectionKey )
