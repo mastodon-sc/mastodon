e9d6a6685dcbe69b40c4f6478ed83803f92471ae
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-09-04 11:56:14 +0200

    Add FeatureSpecsService as a supported parameter for feature computers.
    Some feature computers might want to update the projection keys AFTER
    computation. For instance: You want to compute the mean intensity in a
    spot, but your source have 5 channels. So you need 5 projections, but
    you know that you have 5 only after the feature computer has been
    run at least once.

--- src/main/java/org/mastodon/mamut/feature/MamutFeatureComputerService.java
+++ src/main/java/org/mastodon/mamut/feature/MamutFeatureComputerService.java
@@ -5,11 +5,13 @@ import java.util.Map;
 import org.mastodon.feature.Feature;
 import org.mastodon.feature.FeatureComputerService;
 import org.mastodon.feature.FeatureSpec;
+import org.mastodon.feature.FeatureSpecsService;
 import org.mastodon.revised.bdv.SharedBigDataViewerData;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.revised.model.mamut.ModelGraph;
 import org.scijava.command.CommandModule;
 import org.scijava.module.ModuleItem;
+import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 @Plugin( type = MamutFeatureComputerService.class )
@@ -19,6 +21,9 @@ public class MamutFeatureComputerService extends FeatureComputerService
 	private SharedBigDataViewerData sharedBdvData;
 
 	private Model model;
+
+	@Parameter
+	private FeatureSpecsService featureSpecsService;
 
 	@Override
 	protected void provideParameters( final ModuleItem< ? > item, final CommandModule module, final Class< ? > parameterClass, final Map< FeatureSpec< ?, ? >, Feature< ? > > featureModel )
@@ -44,6 +49,14 @@ public class MamutFeatureComputerService extends FeatureComputerService
 			@SuppressWarnings( "unchecked" )
 			final ModuleItem< SharedBigDataViewerData > bdvItem = ( ModuleItem< SharedBigDataViewerData > ) item;
 			bdvItem.setValue( module, sharedBdvData );
+			return;
+		}
+
+		if ( FeatureSpecsService.class.isAssignableFrom( parameterClass ) )
+		{
+			@SuppressWarnings( "unchecked" )
+			final ModuleItem< FeatureSpecsService > specServiceItem = ( ModuleItem< FeatureSpecsService > ) item;
+			specServiceItem.setValue( module, featureSpecsService );
 			return;
 		}
 
