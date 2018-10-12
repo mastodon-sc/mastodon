25ffe08389ddde8f095514b97ae7e349e46c75d1
Jean-Yves Tinevez <tinevez@pasteur.fr>
2018-10-12 17:25:10 +0200

    MamutFeatureComputerService provides incremental update of features.

--- src/main/java/org/mastodon/mamut/feature/MamutFeatureComputerService.java
+++ src/main/java/org/mastodon/mamut/feature/MamutFeatureComputerService.java
@@ -1,21 +1,26 @@
 package org.mastodon.mamut.feature;
 
+import java.util.Collection;
 import java.util.Map;
 
+import org.mastodon.feature.DefaultFeatureComputerService;
 import org.mastodon.feature.Feature;
-import org.mastodon.feature.FeatureComputerService;
 import org.mastodon.feature.FeatureSpec;
 import org.mastodon.feature.FeatureSpecsService;
+import org.mastodon.feature.update.GraphUpdateStack;
 import org.mastodon.revised.bdv.SharedBigDataViewerData;
+import org.mastodon.revised.model.mamut.Link;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.revised.model.mamut.ModelGraph;
+import org.mastodon.revised.model.mamut.Spot;
+import org.mastodon.revised.model.mamut.SpotPool;
 import org.scijava.command.CommandModule;
 import org.scijava.module.ModuleItem;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 @Plugin( type = MamutFeatureComputerService.class )
-public class MamutFeatureComputerService extends FeatureComputerService
+public class MamutFeatureComputerService extends DefaultFeatureComputerService
 {
 
 	private SharedBigDataViewerData sharedBdvData;
@@ -24,6 +29,25 @@ public class MamutFeatureComputerService extends FeatureComputerService
 
 	@Parameter
 	private FeatureSpecsService featureSpecsService;
+
+	private GraphUpdateStack< Spot, Link > graphUpdate;
+
+	public MamutFeatureComputerService()
+	{
+		super( MamutFeatureComputer.class );
+	}
+
+	@Override
+	public Map< FeatureSpec< ?, ? >, Feature< ? > > compute( final Collection< String > featureKeys )
+	{
+		final Map< FeatureSpec< ?, ? >, Feature< ? > > results = super.compute( featureKeys );
+		if ( isCanceled() )
+			return null;
+
+		// Store updates.
+		graphUpdate.commit( featureKeys );
+		return results;
+	}
 
 	@Override
 	protected void provideParameters( final ModuleItem< ? > item, final CommandModule module, final Class< ? > parameterClass, final Map< FeatureSpec< ?, ? >, Feature< ? > > featureModel )
@@ -52,6 +76,14 @@ public class MamutFeatureComputerService extends FeatureComputerService
 			return;
 		}
 
+		if ( GraphUpdateStack.class.isAssignableFrom( parameterClass ) )
+		{
+			@SuppressWarnings( "unchecked" )
+			final ModuleItem< GraphUpdateStack< Spot, Link > > updateItem = ( ModuleItem< GraphUpdateStack< Spot, Link > > ) item;
+			updateItem.setValue( module, graphUpdate );
+			return;
+		}
+
 		super.provideParameters( item, module, parameterClass, featureModel );
 	}
 
@@ -59,7 +91,7 @@ public class MamutFeatureComputerService extends FeatureComputerService
 	 * Sets the image data to be used by the feature computers.
 	 *
 	 * @param sharedBdvData
-	 *                          the image data.
+	 *            the image data.
 	 */
 	public void setSharedBdvData( final SharedBigDataViewerData sharedBdvData )
 	{
@@ -70,10 +102,18 @@ public class MamutFeatureComputerService extends FeatureComputerService
 	 * Sets the model to be used by the feature computers.
 	 *
 	 * @param model
-	 *                  the model.
+	 *            the model.
 	 */
 	public void setModel( final Model model )
 	{
 		this.model = model;
+		// Listen to graph changes to support incremental computation.
+		final ModelGraph graph = model.getGraph();
+		this.graphUpdate = new GraphUpdateStack<>( graph );
+		graph.addGraphListener( graphUpdate.graphListener() );
+		// Listen to changes in spot properties.
+		final SpotPool spotPool = ( SpotPool ) graph.vertices().getRefPool();
+		spotPool.covarianceProperty().addPropertyChangeListener( graphUpdate.vertexPropertyChangeListener() );
+		spotPool.positionProperty().addPropertyChangeListener( graphUpdate.vertexPropertyChangeListener() );
 	}
 }
