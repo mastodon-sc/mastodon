18322ffcf674a89e8a0e9cbacb7a04ad4eb6237f
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2018-11-04 23:21:23 +0100

    Multiplicity enum can be used to deconstruct a projection key.
    - in its base name.
    - and source indices.
    
    Example: a projection key "Ratio ch4 ch9" on ON_SOURCE_PAIRS
    multiplicity would be decomposed in
    - "Ratio" for the name;
    - [3 8] for the indices.

--- src/main/java/org/mastodon/feature/Multiplicity.java
+++ src/main/java/org/mastodon/feature/Multiplicity.java
@@ -1,9 +1,12 @@
 package org.mastodon.feature;
 
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public enum Multiplicity
 {
+
 	/**
 	 * For projections that do not have multiplicity.
 	 */
@@ -17,9 +20,14 @@ public enum Multiplicity
 	 */
 	ON_SOURCE_PAIRS( 2, ( indices ) -> " ch" + ( indices[ 0 ] + 1 ) + " ch" + ( indices[ 1 ] + 1 ) );
 
+	private static final Pattern SOURCE_PATTERN = Pattern.compile( " ch(?<digit1>\\d+)" );
+
+	private static final Pattern SOURCE_PAIR_PATTERN = Pattern.compile( " ch(?<digit1>\\d+) ch(?<digit2>\\d+)" );
+
 	private final int requiredNArgs;
 
 	private final Function< int[], String > suffixGenerator;
+
 
 	private Multiplicity( final int requiredNArgs, final Function< int[], String > suffixGenerator )
 	{
@@ -35,5 +43,84 @@ public enum Multiplicity
 					+ this + ". Got " + sourceIndices.length + "." );
 
 		return suffixGenerator.apply( sourceIndices );
+	}
+
+	/**
+	 * Returns the name of a projection with this multiplicity from the specified
+	 * key. For instance: "Mean ch1" with a multiplicity of {@link #ON_SOURCES}
+	 * would return "Mean".
+	 * 
+	 * @param key
+	 *                the projection key.
+	 * @return the projection name.
+	 */
+	public String nameFromKey( final String key )
+	{
+		switch ( this )
+		{
+		case SINGLE:
+			return key;
+		case ON_SOURCES:
+			final int l1 = key.lastIndexOf( " ch" );
+			return key.substring( 0, l1 );
+		case ON_SOURCE_PAIRS:
+			final Matcher matcher = SOURCE_PAIR_PATTERN.matcher( key );
+			if ( matcher.find() )
+			{
+				final int l2 = matcher.start();
+				return key.substring( 0, l2 );
+			}
+			else
+			{
+				return key;
+			}
+		default:
+			throw new IllegalArgumentException( "Unsupported multiplicity :" + this );
+		}
+	}
+
+	/**
+	 * Returns the indices stored in the specified projection key, according to this
+	 * multiplicity. Warning, we assume that the key indices are 1-based, but the
+	 * indices themselves are 0-based. For instance "Ratio ch3 ch9" will return
+	 * <code>[2, 8]</code>. An empty array is returned if a match could not be
+	 * found.
+	 * 
+	 * @param key
+	 *                the projection key.
+	 * @return the indices array.
+	 */
+	public int[] indicesFromKey( final String key )
+	{
+		switch ( this )
+		{
+		case SINGLE:
+			return new int[] {};
+		case ON_SOURCES:
+			final Matcher matcher1 = SOURCE_PATTERN.matcher( key );
+			if ( matcher1.find() )
+			{
+				final String digits = matcher1.group( "digit1" );
+				return new int[] { Integer.parseInt( digits ) - 1 };
+			}
+			else
+			{
+				return new int[] {};
+			}
+		case ON_SOURCE_PAIRS:
+			final Matcher matcher2 = SOURCE_PAIR_PATTERN.matcher( key );
+			if ( matcher2.find() )
+			{
+				final String digits1 = matcher2.group( "digit1" );
+				final String digits2 = matcher2.group( "digit2" );
+				return new int[] { Integer.parseInt( digits1 ) - 1, Integer.parseInt( digits2 ) - 1 };
+			}
+			else
+			{
+				return new int[] {};
+			}
+		default:
+			throw new IllegalArgumentException( "Unsupported multiplicity :" + this );
+		}
 	}
 }
