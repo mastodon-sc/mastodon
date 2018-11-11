f97d50f98c6789f0bf72411006a46e72f706c3c5
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2018-11-11 23:15:01 +0100

    A settings page can be removed from the preferences dialog.

--- src/main/java/org/mastodon/app/ui/settings/SettingsPanel.java
+++ src/main/java/org/mastodon/app/ui/settings/SettingsPanel.java
@@ -105,6 +105,47 @@ public class SettingsPanel extends JPanel
 		pages.repaint();
 	}
 
+	/**
+	 * Removes the settings page with the specified path. Does nothing if there is
+	 * not settings page for the path.
+	 * 
+	 * @param path
+	 *                 the path of the settings page to remove. Example: "Analyze >
+	 *                 Tables"
+	 */
+	public void removePage( final String path )
+	{
+		final String[] parts = path.split( ">" );
+		DefaultMutableTreeNode current = root;
+		for ( final String part : parts )
+		{
+			final String text = part.trim();
+			DefaultMutableTreeNode next = null;
+			for ( int i = 0; i < current.getChildCount(); ++i )
+			{
+				final DefaultMutableTreeNode child = ( DefaultMutableTreeNode ) current.getChildAt( i );
+				final SettingsNodeData data = ( SettingsNodeData ) child.getUserObject();
+				if ( text.equals( data.name ) )
+				{
+					next = child;
+					break;
+				}
+			}
+			current = next;
+		}
+		if ( null == current )
+			return; // Path not found in the tree.
+
+		model.removeNodeFromParent( current );
+		for ( final SettingsPage page : getPages() )
+		{
+			if ( page.getTreePath().equals( path ) )
+				pages.remove( page.getJPanel() );
+		}
+		pages.revalidate();
+		pages.repaint();
+	}
+
 	public SettingsPanel()
 	{
 		root = new DefaultMutableTreeNode( new SettingsNodeData( "root", null ) );
