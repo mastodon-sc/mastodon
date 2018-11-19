42d79bf656d436759cd2980e722872f550ef8854
tpietzsch <tobias.pietzsch@gmail.com>
2018-11-19 16:01:04 +0100

    javadoc

--- src/main/java/org/mastodon/revised/ui/coloring/feature/AvailableFeatureProjections.java
+++ src/main/java/org/mastodon/revised/ui/coloring/feature/AvailableFeatureProjections.java
@@ -1,16 +1,58 @@
 package org.mastodon.revised.ui.coloring.feature;
 
-import gnu.trove.list.TIntList;
 import java.util.Collection;
+
 import org.mastodon.feature.Multiplicity;
 
+import gnu.trove.list.TIntList;
+
+/**
+ * Lists the features and projections (as their respective ({@code String}
+ * keys), as well as available source indices (for features with multiplicity).
+ *
+ * @author Tobias Pietzsch
+ */
 public interface AvailableFeatureProjections
 {
+	/**
+	 * Get ordered list of source indices available for selection as feature
+	 * source.
+	 *
+	 * @return ordered list of source indices
+	 */
 	public TIntList getSourceIndices();
 
-	public Collection< String > featureKeys( TargetType targetType );
+	/**
+	 * Get ordered list of feature keys for the given {@code targetType}.
+	 *
+	 * @param targetType
+	 *            whether vertex or edge features should be returned
+	 * @return ordered list of feature keys
+	 */
+	public Collection< String > featureKeys( final TargetType targetType );
 
-	public Collection< String > projectionKeys( TargetType targetType, String featureKey );
+	/**
+	 * Get ordered list of projection keys available for the feature with the
+	 * specified {@code targetType} and {@code String} key.
+	 *
+	 * @param targetType
+	 *            whether a vertex or edge feature is queried
+	 * @param featureKey
+	 *            key of the feature to query
+	 * @return ordered list of projection keys
+	 */
+	public Collection< String > projectionKeys( final TargetType targetType, final String featureKey );
 
-	public Multiplicity multiplicity( TargetType targetType, String featureKey );
+
+	/**
+	 * Get the multiplicity of the feature with the specified {@code targetType}
+	 * and {@code String} key.
+	 *
+	 * @param targetType
+	 *            whether a vertex or edge feature is queried
+	 * @param featureKey
+	 *            key of the feature to query
+	 * @return multiplicity of specified feature
+	 */
+	public Multiplicity multiplicity( final TargetType targetType, final String featureKey );
 }
