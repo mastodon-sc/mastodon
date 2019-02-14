77b82df34bcdd5e2ed5e686ea2ff74046510b822
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-02-14 22:08:10 +0100

    basic merging works, but not tags, and need better matching criteria

--- src/main/java/org/mastodon/tomancak/MetteMerging.java
+++ src/main/java/org/mastodon/tomancak/MetteMerging.java
@@ -8,12 +8,15 @@ import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 import org.mastodon.collection.RefCollections;
 import org.mastodon.collection.RefList;
+import org.mastodon.collection.RefMaps;
+import org.mastodon.collection.RefRefMap;
 import org.mastodon.graph.algorithm.traversal.UndirectedDepthFirstIterator;
 import org.mastodon.kdtree.IncrementalNearestNeighborSearch;
 import org.mastodon.project.MamutProject;
 import org.mastodon.project.MamutProjectIO;
 import org.mastodon.revised.mamut.Mastodon;
 import org.mastodon.revised.mamut.WindowManager;
+import org.mastodon.revised.model.mamut.Link;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.revised.model.mamut.ModelGraph;
 import org.mastodon.revised.model.mamut.Spot;
@@ -99,15 +102,12 @@ public class MetteMerging
 
 		private final TagSetStructure tagSetStructure;
 
-		private final TagSet sourceTagSet;
-
 		private final TagSet conflictTagSet;
 
 		public OutputDataSet()
 		{
 			model = new Model();
 			tagSetStructure = new TagSetStructure();
-			sourceTagSet = tagSetStructure.createTagSet( "Merge Source" );
 			conflictTagSet = tagSetStructure.createTagSet( "Merge Conflict" );
 		}
 
@@ -140,7 +140,8 @@ public class MetteMerging
 
 		public Tag addSourceTag( String name, int color )
 		{
-			final Tag tag = sourceTagSet.createTag( name, color );
+			final TagSet ts = tagSetStructure.createTagSet( "Merge Source " + name );
+			final Tag tag = ts.createTag( name, color );
 			model.getTagSetModel().setTagSetStructure( tagSetStructure );
 			return tag;
 		}
@@ -153,12 +154,12 @@ public class MetteMerging
 		}
 	}
 
-	static void match( final Dataset dsA, final Dataset dsB, final OutputDataSet output )
+	static void matchz( final Dataset dsA, final Dataset dsB, final OutputDataSet output )
 	{
-		match( dsA, dsB, output,-1 );
+		matchz( dsA, dsB, output,-1 );
 	}
 
-	static void match( final Dataset dsA, final Dataset dsB, final OutputDataSet output, final int tp )
+	static void matchz( final Dataset dsA, final Dataset dsB, final OutputDataSet output, final int tp )
 	{
 		final int minTimepoint = tp >= 0 ? tp : 0;
 		final int maxTimepoint = tp >= 0 ? tp : Math.max( dsA.maxNonEmptyTimepoint(), dsB.maxNonEmptyTimepoint() );
@@ -208,6 +209,174 @@ public class MetteMerging
 		}
 	}
 
+	static void merge( final Dataset dsA, final Dataset dsB, final OutputDataSet output )
+	{
+		final int minTimepoint = 0;
+		final int maxTimepoint = Math.max( dsA.maxNonEmptyTimepoint(), dsB.maxNonEmptyTimepoint() );
+		final MatchingGraph matching = buildMatchingGraph( dsA, dsB, minTimepoint, maxTimepoint );
+		final MatchingGraphUtils utils = new MatchingGraphUtils( matching );
+
+		final Tag tagA = output.addSourceTag( "A", 0xffffff00 );
+		final Tag tagB = output.addSourceTag( "B", 0xffff00ff );
+		final Tag tagSingletonA = output.addConflictTag( "Singleton A", 0xffffff00 );
+		final Tag tagSingletonB = output.addConflictTag( "Singleton B", 0xffff00ff );
+		final Tag tagMatchAB = output.addConflictTag( "MatchAB", 0xff8888ff );
+		final Tag tagConflict = output.addConflictTag( "Conflict", 0xffff0000 );
+
+		final ModelGraph graph = output.getModel().getGraph();
+		final ObjTags< Spot > vertexTags = output.getModel().getTagSetModel().getVertexTags();
+
+		final double[] pos = new double[ 3 ];
+		final double[][] cov = new double[ 3 ][ 3 ];
+		final Spot vref = graph.vertexRef();
+
+/*
+		for every spot a in A:
+			add a' with shape and translated a tags
+			add mapping MA: a --> a'
+*/
+		final ModelGraph graphA = dsA.model().getGraph();
+		final RefRefMap< Spot, Spot > mapAtoDest = RefMaps.createRefRefMap( graphA.vertices(), graph.vertices() );
+		for ( Spot spotA : graphA.vertices() )
+		{
+			final int tp = spotA.getTimepoint();
+			spotA.localize( pos );
+			spotA.getCovariance( cov );
+			final Spot destSpot = graph.addVertex( vref ).init( tp, pos, cov );
+			vertexTags.set( destSpot, tagA );
+			vertexTags.set( destSpot, tagSingletonA );
+			mapAtoDest.put( spotA, destSpot );
+		}
+
+/*
+		for every spot b in B:
+			if singleton (b):
+				add b' with shape and translated b tags
+				add mapping MB: b --> b'
+			else if perfect match (a,b):
+				get a'
+				add translated b tags, checking for conflicts
+				add mapping MB: b --> a'
+			else
+				add b' with shape and translated b tags
+				add "conflict" tag to b' and any connected (and already present) c'
+				add mapping MB: b --> b'
+*/
+		final ModelGraph graphB = dsB.model().getGraph();
+		final RefRefMap< Spot, Spot > mapBtoDest = RefMaps.createRefRefMap( graphA.vertices(), graph.vertices() );
+		final UndirectedDepthFirstIterator< MatchingVertex, MatchingEdge > miter = new UndirectedDepthFirstIterator<>( matching );
+		for ( Spot spotB : graphB.vertices() )
+		{
+			final MatchingVertex mvB = matching.getVertex( spotB );
+
+			if ( utils.isUnmatched( mvB ) )
+			{
+				final int tp = spotB.getTimepoint();
+				spotB.localize( pos );
+				spotB.getCovariance( cov );
+				final Spot destSpot = graph.addVertex( vref ).init( tp, pos, cov );
+				vertexTags.set( destSpot, tagB );
+				vertexTags.set( destSpot, tagSingletonB );
+				mapBtoDest.put( spotB, destSpot );
+			}
+			else if ( utils.isPerfectlyMatched( mvB ) )
+			{
+				final MatchingVertex mvA = mvB.outgoingEdges().get( 0 ).getTarget();
+				final Spot spotA = mvA.getSpot( vref );
+				final Spot destSpot = mapAtoDest.get( spotA );
+				vertexTags.set( destSpot, tagB );
+				vertexTags.set( destSpot, tagMatchAB );
+				mapBtoDest.put( spotB, destSpot );
+			}
+			else
+			{
+				final int tp = spotB.getTimepoint();
+				spotB.localize( pos );
+				spotB.getCovariance( cov );
+				final Spot destSpot = graph.addVertex( vref ).init( tp, pos, cov );
+				vertexTags.set( destSpot, tagB );
+				mapBtoDest.put( spotB, destSpot );
+
+				miter.reset( mvB );
+				while ( miter.hasNext() )
+				{
+					final MatchingVertex mv = miter.next();
+					final Spot sourceSpot = mv.getSpot();
+					final Spot spot;
+					if ( sourceSpot.getModelGraph() == graphA )
+						spot = mapAtoDest.get( sourceSpot );
+					else
+						spot = mapBtoDest.get( sourceSpot );
+					if ( spot != null )
+						vertexTags.set( spot, tagConflict );
+				}
+			}
+		}
+
+/*
+		for every edge (a1,a2) in A
+			get a1', a2' from mapping MA
+			add edge (a1',a2') and translated (a1,a2) tags
+*/
+		for ( Link linkA : graphA.edges() )
+		{
+			final Spot source = mapAtoDest.get( linkA.getSource() );
+			final Spot target = mapAtoDest.get( linkA.getTarget() );
+			graph.addEdge( source, target );
+		}
+
+/*
+		for every edge (b1,b2) in B
+			get b1', b2' from mapping MB
+			add edge (b1',b2') if not exists
+			add translated (b1,b2) tags, checking for conflicts
+*/
+		for ( Link linkB : graphB.edges() )
+		{
+			final Spot source = mapBtoDest.get( linkB.getSource() );
+			final Spot target = mapBtoDest.get( linkB.getTarget() );
+			if ( graph.getEdge( source, target ) == null )
+				graph.addEdge( source, target );
+		}
+	}
+
+
+	static class MatchingGraphUtils
+	{
+		private final MatchingGraph matchingGraph;
+
+		private final MatchingEdge eref1;
+		private final MatchingEdge eref2;
+		private final MatchingVertex vref1;
+		private final MatchingVertex vref2;
+
+		public MatchingGraphUtils( final MatchingGraph matchingGraph )
+		{
+			this.matchingGraph = matchingGraph;
+			eref1 = matchingGraph.edgeRef();
+			eref2 = matchingGraph.edgeRef();
+			vref1 = matchingGraph.vertexRef();
+			vref2 = matchingGraph.vertexRef();
+		}
+
+		public boolean isUnmatched( MatchingVertex mv )
+		{
+			return mv.edges().isEmpty();
+		}
+
+		public boolean isPerfectlyMatched( MatchingVertex mv )
+		{
+			if ( mv.outgoingEdges().size() != 1 )
+				return false;
+			if ( mv.incomingEdges().size() != 1 )
+				return false;
+			final MatchingVertex vTo = mv.outgoingEdges().get( 0, eref1 ).getTarget( vref1 );
+			final MatchingVertex vFrom = mv.incomingEdges().get( 0, eref2 ).getSource( vref2 );
+			return vTo.equals( vFrom );
+		}
+	}
+
+
 	public static void main( String[] args ) throws IOException
 	{
 //		for ( String path : paths )
@@ -229,7 +398,7 @@ public class MetteMerging
 		final Dataset ds2 = new Dataset( path2 );
 
 		final OutputDataSet output = new OutputDataSet();
-		match( ds1, ds2, output );
+		merge( ds1, ds2, output );
 		output.setDatasetXmlFile( ds1.project().getDatasetXmlFile() );
 
 		final String resultPath = "/Users/pietzsch/Desktop/Mastodon/merging/conflicts.mastodon";
