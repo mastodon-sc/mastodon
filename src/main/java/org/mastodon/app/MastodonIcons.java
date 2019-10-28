b598cf6f3380d07cb88588755fc001baa733e5b0
Jean-Yves Tinevez <tinevez@pasteur.fr>
2019-10-28 18:11:19 +0100

    Import icons made by IlluScienta.com.
    PNG batch.
    Two sizes: 16x16 and 512x512.

---
+++ src/main/java/org/mastodon/app/MastodonIcons.java
@@ -0,0 +1,50 @@
+package org.mastodon.app;
+
+import javax.swing.ImageIcon;
+
+/**
+ * Collection of static fields pointing to the icons to use in the Mastodon-app.
+ *
+ * @author Jean-Yves Tinevez
+ *
+ */
+public class MastodonIcons
+{
+
+	public static final ImageIcon BVV_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-BDV-volume-16x16.png" ) );
+
+	public static final ImageIcon BDV_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-BDV-slicing-16x16.png" ) );
+
+	public static final ImageIcon FEATURES_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-compute-values-16x16.png" ) );
+
+	public static final ImageIcon TRACKSCHEME_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-lineage-view-16x16.png" ) );
+
+	public static final ImageIcon LOAD_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-load-new-project-16x16.png" ) );
+
+	public static final ImageIcon SAVE_AS_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-save-as-16x16.png" ) );
+
+	public static final ImageIcon SAVE_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-save-16x16.png" ) );
+
+	public static final ImageIcon TABLE_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-table-16x16.png" ) );
+
+	public static final ImageIcon TAGS_ICON_SMALL = new ImageIcon( MastodonIcons.class.getResource( "icon-tag-16x16.png" ) );
+
+	public static final ImageIcon BVV_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-BDV-volume-512x512.png" ) );
+
+	public static final ImageIcon BDV_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-BDV-slicing-512x512.png" ) );
+
+	public static final ImageIcon FEATURES_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-compute-values-512x512.png" ) );
+
+	public static final ImageIcon TRACKSCHEME_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-lineage-view-512x512.png" ) );
+
+	public static final ImageIcon LOAD_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-load-new-project-512x512.png" ) );
+
+	public static final ImageIcon SAVE_AS_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-save-as-512x512.png" ) );
+
+	public static final ImageIcon SAVE_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-save-512x512.png" ) );
+
+	public static final ImageIcon TABLE_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-table-512x512.png" ) );
+
+	public static final ImageIcon TAGS_ICON_LARGE = new ImageIcon( MastodonIcons.class.getResource( "icon-tag-512x512.png" ) );
+
+}
