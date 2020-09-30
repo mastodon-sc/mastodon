eb57163b591d4503b69ba8c995660e53e5bcba8b
tpietzsch <tobias.pietzsch@gmail.com>
2020-09-30 23:54:42 +0200

    Remove unnecessary modifiers

--- src/main/java/org/mastodon/ui/keymap/KeyConfigContexts.java
+++ src/main/java/org/mastodon/ui/keymap/KeyConfigContexts.java
@@ -6,20 +6,18 @@ package org.mastodon.ui.keymap;
 // TODO move to somewhere in mamut package
 public interface KeyConfigContexts
 {
-
 	/**
 	 * The action or behaviour applies to the whole app.
 	 */
-	public static final String MASTODON = "mastodon";
+	String MASTODON = "mastodon";
 
 	/**
 	 * The action or behaviour applies to the TrackScheme views.
 	 */
-	public static final String TRACKSCHEME = "ts";
+	String TRACKSCHEME = "ts";
 
 	/**
 	 * The action or behaviour applies to the BDV views.
 	 */
-	public static final String BIGDATAVIEWER = "bdv";
-
+	String BIGDATAVIEWER = "bdv";
 }
