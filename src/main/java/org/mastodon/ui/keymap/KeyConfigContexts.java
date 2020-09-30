e097920e5e2d01097a36a39c6fd8d743651d6dc0
tpietzsch <tobias.pietzsch@gmail.com>
2020-09-30 23:54:28 +0200

    Add back TODO

--- src/main/java/org/mastodon/ui/keymap/KeyConfigContexts.java
+++ src/main/java/org/mastodon/ui/keymap/KeyConfigContexts.java
@@ -3,6 +3,7 @@ package org.mastodon.ui.keymap;
 /**
  * Constants that specify to what context an action or a behaviour applies.
  */
+// TODO move to somewhere in mamut package
 public interface KeyConfigContexts
 {
 
