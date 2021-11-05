ad717234408ab757fa1f71aa61b079c8193b20b0
Jean-Yves Tinevez <tinevez@pasteur.fr>
2021-11-05 18:47:43 +0100

    Two make JFree charts easily exportable to PDF and so on.

---
+++ src/main/java/org/mastodon/ui/util/ChartExporter.java
@@ -0,0 +1,118 @@
+/*-
+ * #%L
+ * Fiji distribution of ImageJ for the life sciences.
+ * %%
+ * Copyright (C) 2010 - 2021 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package org.mastodon.ui.util;
+
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.jfree.chart.JFreeChart;
+
+import com.itextpdf.awt.PdfGraphics2D;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
+
+/**
+ * A collection of static utilities made to export a JPanel to various scalable
+ * file format.
+ * 
+ * @author Jean-Yves Tinevez, 2011 - 2021
+ */
+public class ChartExporter
+{
+
+	/**
+	 * Export a JFreeChart to SVG.
+	 * 
+	 * @param svgFile
+	 *            the target svg file.
+	 * @param chart
+	 *            the chart to export.
+	 * @param width
+	 *            the width of the panel the chart is painted in.
+	 * @param height
+	 *            the height of the panel the chart is painted in.
+	 * @throws UnsupportedEncodingException
+	 * @throws IOException
+	 */
+	public static void exportChartAsSVG( final File svgFile, final JFreeChart chart, final int width, final int height ) throws UnsupportedEncodingException, IOException
+	{
+		// Get a DOMImplementation and create an XML document
+		final org.w3c.dom.DOMImplementation domImpl = GenericDOMImplementation.getDOMImplementation();
+		final org.w3c.dom.Document document = domImpl.createDocument( null, "svg", null );
+
+		// Create an instance of the SVG Generator
+		final SVGGraphics2D svgGenerator = new SVGGraphics2D( document );
+		// draw the chart in the SVG generator
+		chart.draw( svgGenerator, new Rectangle( width, height ) );
+
+		// Write svg file
+		try (final OutputStream outputStream = new FileOutputStream( svgFile );
+				final Writer out = new OutputStreamWriter( outputStream, "UTF-8" ))
+		{
+			svgGenerator.stream( out, true );
+		}
+	}
+
+	/**
+	 * Export a JFreeChart to PDF.
+	 * 
+	 * @param pdfFile
+	 *            the target pdf file.
+	 * @param chart
+	 *            the chart to export.
+	 * @param width
+	 *            the width of the panel the chart is painted in.
+	 * @param height
+	 *            the height of the panel the chart is painted in.
+	 * @throws FileNotFoundException
+	 *             if the file to write cannot be found.
+	 * @throws DocumentException
+	 *             on error.
+	 */
+	public static void exportChartAsPDF( final File pdfFile, final JFreeChart chart, final int width, final int height ) throws FileNotFoundException, DocumentException
+	{
+		// step 1
+		final com.itextpdf.text.Rectangle pageSize = new com.itextpdf.text.Rectangle( 0, 0, width, height );
+		final com.itextpdf.text.Document document = new com.itextpdf.text.Document( pageSize );
+		// step 2
+		final PdfWriter writer = PdfWriter.getInstance( document, new FileOutputStream( pdfFile ) );
+		// step 3
+		document.open();
+		// step 4
+		final PdfContentByte cb = writer.getDirectContent();
+		final PdfGraphics2D g2 = new PdfGraphics2D( cb, width, height );
+		chart.draw( g2, new Rectangle( 0, 0, width, height ) );
+		g2.dispose();
+		// step 5
+		document.close();
+	}
+}
---
+++ src/main/java/org/mastodon/ui/util/ExportableChartPanel.java
@@ -0,0 +1,174 @@
+/*-
+ * #%L
+ * Fiji distribution of ImageJ for the life sciences.
+ * %%
+ * Copyright (C) 2010 - 2021 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package org.mastodon.ui.util;
+
+import java.awt.Container;
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+
+import com.itextpdf.text.DocumentException;
+
+public class ExportableChartPanel extends ChartPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private static File currentDir;
+
+	/*
+	 * CONSTRUCTORS
+	 */
+
+	public ExportableChartPanel( final JFreeChart chart )
+	{
+		super( chart );
+	}
+
+	public ExportableChartPanel(
+			final JFreeChart chart,
+			final boolean properties,
+			final boolean save,
+			final boolean print,
+			final boolean zoom,
+			final boolean tooltips )
+	{
+		super( chart, properties, save, print, zoom, tooltips );
+	}
+
+	public ExportableChartPanel( final JFreeChart chart, final int width, final int height,
+			final int minimumDrawWidth, final int minimumDrawHeight, final int maximumDrawWidth,
+			final int maximumDrawHeight, final boolean useBuffer, final boolean properties,
+			final boolean save, final boolean print, final boolean zoom, final boolean tooltips )
+	{
+		super( chart, width, height, minimumDrawWidth, minimumDrawHeight,
+				maximumDrawWidth, maximumDrawHeight,
+				useBuffer, properties, save, print, zoom, tooltips );
+	}
+
+	public ExportableChartPanel( final JFreeChart chart, final int width, final int height,
+			final int minimumDrawWidth, final int minimumDrawHeight, final int maximumDrawWidth,
+			final int maximumDrawHeight, final boolean useBuffer, final boolean properties,
+			final boolean copy, final boolean save, final boolean print, final boolean zoom,
+			final boolean tooltips )
+	{
+		super( chart, width, height, minimumDrawWidth, minimumDrawHeight,
+				maximumDrawWidth, maximumDrawHeight,
+				useBuffer, properties, copy, save, print, zoom, tooltips );
+	}
+
+	/*
+	 * METHODS
+	 */
+
+	@Override
+	protected JPopupMenu createPopupMenu( final boolean properties, final boolean copy, final boolean save, final boolean print, final boolean zoom )
+	{
+		final JPopupMenu menu = super.createPopupMenu( properties, copy, false, print, zoom );
+
+		menu.addSeparator();
+
+		final JMenuItem exportToFile = new JMenuItem( "Export plot to file" );
+		exportToFile.addActionListener( e -> doSaveAs() );
+		menu.add( exportToFile );
+
+		return menu;
+	}
+
+	/**
+	 * Opens a file chooser and gives the user an opportunity to save the chart
+	 * in PNG, PDF or SVG format.
+	 */
+	@Override
+	public void doSaveAs()
+	{
+		if ( null == currentDir )
+			currentDir = getDefaultDirectoryForSaveAs();
+
+		final File file;
+		if ( FileChooser.isMac() )
+		{
+			Container dialogParent = getParent();
+			while ( !( dialogParent instanceof Frame ) )
+				dialogParent = dialogParent.getParent();
+
+			final Frame frame = ( Frame ) dialogParent;
+			final FileDialog dialog = new FileDialog( frame, "Export chart to PNG, PDF or SVG", FileDialog.SAVE );
+			final FilenameFilter filter = ( dir, name ) -> name.endsWith( ".png" ) || name.endsWith( ".pdf" ) || name.endsWith( ".svg" );
+			dialog.setFilenameFilter( filter );
+			dialog.setDirectory( currentDir == null ? null : currentDir.getAbsolutePath() );
+			dialog.setFile( getChart().getTitle().getText().replaceAll( "\\.+$", "" ) + ".pdf" );
+			dialog.setVisible( true );
+			final String selectedFile = dialog.getFile();
+			if ( null == selectedFile )
+				return;
+
+			file = new File( dialog.getDirectory(), selectedFile );
+			currentDir = new File( dialog.getDirectory() );
+		}
+		else
+		{
+			final JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setDialogTitle( "Export chart to PNG, PDF or SVG" );
+			fileChooser.setCurrentDirectory( currentDir );
+			fileChooser.addChoosableFileFilter( new FileNameExtensionFilter("PNG Image File", "png" ) );
+			fileChooser.addChoosableFileFilter( new FileNameExtensionFilter("Portable Document File (PDF)", "pdf" ) );
+			fileChooser.addChoosableFileFilter( new FileNameExtensionFilter( "Scalable Vector Graphics (SVG)", "svg" ) );
+			fileChooser.setSelectedFile( new File( currentDir, getChart().getTitle().getText().replaceAll( "\\.+$", "" ) + ".pdf" ) );
+			final int option = fileChooser.showSaveDialog( this );
+			if ( option != JFileChooser.APPROVE_OPTION )
+				return;
+
+			file = fileChooser.getSelectedFile();
+			currentDir = fileChooser.getCurrentDirectory();
+		}
+		try
+		{
+			if ( file.getPath().endsWith( ".png" ) )
+				ChartUtils.saveChartAsPNG( file, getChart(), getWidth(), getHeight() );
+			else if ( file.getPath().endsWith( ".pdf" ) )
+				ChartExporter.exportChartAsPDF( file, getChart(), getWidth(), getHeight() );
+			else if ( file.getPath().endsWith( ".svg" ) )
+				ChartExporter.exportChartAsSVG( file, getChart(), getWidth(), getHeight() );
+			else
+				JOptionPane.showMessageDialog( SwingUtilities.getWindowAncestor( this ), "Invalid file extension.\n"
+						+ "Please choose a filename with one of the 3 supported extension: .png, .pdf or .svg." );
+		}
+		catch ( final IOException | DocumentException e )
+		{
+			e.printStackTrace();
+		}
+	}
+}
