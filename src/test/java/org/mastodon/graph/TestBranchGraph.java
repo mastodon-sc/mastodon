b1e96f749d1a6ce0439a46cec82450ce9e16143b
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-04-21 19:41:45 +0200

    Test to reproduce the branch graph building bug.

--- src/test/java/org/mastodon/graph/TestBranchGraph.java
+++ src/test/java/org/mastodon/graph/TestBranchGraph.java
@@ -8,6 +8,7 @@ import org.mastodon.mamut.model.Link;
 import org.mastodon.mamut.model.Model;
 import org.mastodon.mamut.model.ModelGraph;
 import org.mastodon.mamut.model.Spot;
+import org.mastodon.mamut.model.branch.ModelBranchGraph;
 import org.mastodon.mamut.project.MamutProject;
 import org.scijava.Context;
 
@@ -24,7 +25,7 @@ public class TestBranchGraph
 
 		final Model model = wm.getAppModel().getModel();
 		final ModelGraph graph = model.getGraph();
-		
+
 		final Spot s0 = graph.addVertex().init( 0, new double[] { 0., 0., 0. }, 1. );
 		final Spot s1 = graph.addVertex().init( 1, new double[] { 0., 0., 0. }, 1. );
 		graph.addEdge( s0, s1 ).init();
@@ -49,14 +50,46 @@ public class TestBranchGraph
 		final Spot s6 = graph.addVertex().init( 4, new double[] { 0., 0., 0. }, 1. );
 		graph.addEdge( s5, s6 ).init();
 		System.out.println( model.getBranchGraph() );
-		
+
 		graph.remove( e23 );
 		System.out.println( model.getBranchGraph() );
 
-		System.out.println( "Finished!" ); 
-		System.out.println( graph ); 
+		System.out.println( "Finished!" );
+		System.out.println( graph );
 
 		wm.createBranchTrackScheme();
 
 	}
+
+	public static void main2( final String[] args ) throws IOException, SpimDataException
+	{
+
+		final WindowManager wm = new WindowManager( new Context() );
+		wm.getProjectManager().open( new MamutProject( null, new File( "x=100 y=100 z=100 sx=1 sy=1 sz=1 t=10.dummy" ) ) );
+
+		final Model model = wm.getAppModel().getModel();
+		final ModelGraph graph = model.getGraph();
+		final ModelBranchGraph branchGraph = model.getBranchGraph();
+		graph.removeGraphListener( branchGraph );
+		
+		final Spot s0 = graph.addVertex().init( 0, new double[] { 0., 0., 0. }, 1. );
+		final Spot s1 = graph.addVertex().init( 1, new double[] { 0., 0., 0. }, 1. );
+		graph.addEdge( s0, s1 ).init();
+
+		final Spot s2 = graph.addVertex().init( 2, new double[] { 0., 0., 0. }, 1. );
+		graph.addEdge( s1, s2 ).init();
+
+		final Spot s3 = graph.addVertex().init( 3, new double[] { 0., 0., 0. }, 1. );
+		graph.addEdge( s2, s3 ).init();
+
+		final Spot s4 = graph.addVertex().init( 3, new double[] { 0., 0., 0. }, 1. );
+		graph.addEdge( s2, s4 ).init();
+
+		System.out.println( "Finished!" ); 
+		System.out.println( graph ); 
+
+		branchGraph.graphRebuilt();
+		wm.createBranchTrackScheme();
+	}
+
 }
