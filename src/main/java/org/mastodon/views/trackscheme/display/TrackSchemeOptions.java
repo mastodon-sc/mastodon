881376fe6aca42081074acb735b7c73e45688010
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2022-05-01 18:01:16 +0200

    TrackSchemeOptions can specify what layout to use.
    For long edges, or classical layout.

--- src/main/java/org/mastodon/views/trackscheme/display/TrackSchemeOptions.java
+++ src/main/java/org/mastodon/views/trackscheme/display/TrackSchemeOptions.java
@@ -196,6 +196,13 @@ public class TrackSchemeOptions
 		return this;
 	}
 
+	public TrackSchemeOptions paintLongEdges( final boolean paintLongEdges )
+	{
+		values.paintLongEdges = paintLongEdges;
+		return this;
+	}
+
+
 	/**
 	 * Read-only {@link TrackSchemeOptions} values.
 	 */
@@ -221,6 +228,8 @@ public class TrackSchemeOptions
 
 		private GraphColorGenerator< TrackSchemeVertex, TrackSchemeEdge > graphColorGenerator = new DefaultGraphColorGenerator<>();
 
+		private boolean paintLongEdges = false;
+
 		public TrackSchemeOptions optionsFromValues()
 		{
 			return new TrackSchemeOptions().
@@ -232,7 +241,8 @@ public class TrackSchemeOptions
 				navigationEtiquette( navigationEtiquette ).
 				style( style ).
 				trackSchemeOverlayFactory( trackSchemeOverlayFactory ).
-				graphColorGenerator( graphColorGenerator );
+				graphColorGenerator( graphColorGenerator ).
+				paintLongEdges( paintLongEdges );
 		}
 
 		public int getX()
@@ -283,6 +293,11 @@ public class TrackSchemeOptions
 		public GraphColorGenerator< TrackSchemeVertex, TrackSchemeEdge > getGraphColorGenerator()
 		{
 			return graphColorGenerator;
+		}
+
+		public boolean isPaintLongEdges()
+		{
+			return paintLongEdges;
 		}
 	}
 }
