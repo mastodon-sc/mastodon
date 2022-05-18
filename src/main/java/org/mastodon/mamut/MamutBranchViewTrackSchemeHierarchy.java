691c8c1e13913294cd938f46154436f384012ea3
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2022-05-18 12:23:39 +0200

    Rework the branch hierarchy view.
    - paint with roundish edge.
    - reflect change of hierarchy when the branch graph changes.
    - remove the time-point listener.
    - set the y range based on depth.

--- src/main/java/org/mastodon/mamut/MamutBranchViewTrackSchemeHierarchy.java
+++ src/main/java/org/mastodon/mamut/MamutBranchViewTrackSchemeHierarchy.java
@@ -3,13 +3,24 @@ package org.mastodon.mamut;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.mastodon.graph.GraphChangeListener;
 import org.mastodon.graph.GraphIdBimap;
 import org.mastodon.graph.algorithm.traversal.InverseDepthFirstIterator;
 import org.mastodon.mamut.model.Model;
 import org.mastodon.mamut.model.branch.BranchLink;
 import org.mastodon.mamut.model.branch.BranchSpot;
 import org.mastodon.mamut.model.branch.ModelBranchGraph;
+import org.mastodon.model.FocusModel;
+import org.mastodon.model.HighlightModel;
+import org.mastodon.views.trackscheme.TrackSchemeEdge;
 import org.mastodon.views.trackscheme.TrackSchemeGraph;
+import org.mastodon.views.trackscheme.TrackSchemeVertex;
+import org.mastodon.views.trackscheme.display.PaintBranchGraphRound;
+import org.mastodon.views.trackscheme.display.PaintDecorations;
+import org.mastodon.views.trackscheme.display.TrackSchemeFrame;
+import org.mastodon.views.trackscheme.display.TrackSchemeOptions;
+import org.mastodon.views.trackscheme.display.TrackSchemeOverlay;
+import org.mastodon.views.trackscheme.display.TrackSchemeOverlay.TrackSchemeOverlayFactory;
 import org.mastodon.views.trackscheme.wrap.DefaultModelGraphProperties;
 import org.mastodon.views.trackscheme.wrap.ModelGraphProperties;
 
@@ -23,7 +34,32 @@ public class MamutBranchViewTrackSchemeHierarchy extends MamutBranchViewTrackSch
 
 	public MamutBranchViewTrackSchemeHierarchy( final MamutAppModel appModel, final Map< String, Object > guiState )
 	{
-		super( appModel, guiState, new BranchHierarchyTrackSchemeFactory() );
+		super( appModel, guiState, new BranchHierarchyTrackSchemeFactory(), new HierarchyTrackSchemeOverlayFactory() );
+
+		// Window title.
+		final TrackSchemeFrame frame = getFrame();
+		frame.setTitle( "TrackScheme Hierarchy" );
+
+		// Remove timepoint listener.
+		timepointModel.listeners().removeAll();
+
+		// Min & max levels.
+		final GraphChangeListener gcl = () -> {
+			int minT = Integer.MAX_VALUE;
+			int maxT = Integer.MIN_VALUE;
+			for ( final TrackSchemeVertex v : viewGraph.vertices() )
+			{
+				final int t = v.getTimepoint();
+				if ( t > maxT )
+					maxT = t;
+				if ( t < minT )
+					minT = t;
+			}
+			frame.getTrackschemePanel().setTimepointRange( minT, maxT );
+			frame.getTrackschemePanel().graphChanged();
+		};
+		viewGraph.graphChangeListeners().add( gcl );
+		gcl.graphChanged();
 	}
 
 	/**
@@ -41,7 +77,7 @@ public class MamutBranchViewTrackSchemeHierarchy extends MamutBranchViewTrackSch
 			final GraphIdBimap< BranchSpot, BranchLink > idmap = graph.getGraphIdBimap();
 			final ModelGraphProperties< BranchSpot, BranchLink > properties = new MyModelGraphProperties( graph );
 			final TrackSchemeGraph< BranchSpot, BranchLink > trackSchemeGraph =
-					new TrackSchemeGraph< BranchSpot, BranchLink >( graph, idmap, properties );
+					new TrackSchemeGraph<>( graph, idmap, properties );
 			return trackSchemeGraph;
 		}
 
@@ -64,14 +100,25 @@ public class MamutBranchViewTrackSchemeHierarchy extends MamutBranchViewTrackSch
 			public int getTimepoint( final BranchSpot v )
 			{
 				it.reset( v );
-				int level = -1;
-				do
-				{
+				int level = 0;
+				while ( it.hasNext() && it.next().incomingEdges().size() > 0 )
 					level++;
-				}
-				while ( it.hasNext() && it.next().incomingEdges().size() > 0 );
 				return level;
 			}
 		}
 	}
+
+	public static class HierarchyTrackSchemeOverlayFactory extends TrackSchemeOverlayFactory
+	{
+		@Override
+		public TrackSchemeOverlay create(
+				final TrackSchemeGraph< ?, ? > graph,
+				final HighlightModel< TrackSchemeVertex, TrackSchemeEdge > highlight,
+				final FocusModel< TrackSchemeVertex, TrackSchemeEdge > focus,
+				final TrackSchemeOptions options )
+		{
+			return new TrackSchemeOverlay( graph, highlight, focus, new PaintDecorations(), new PaintBranchGraphRound(), options );
+		}
+	}
+
 }
