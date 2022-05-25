a9b000e5bc5f33a4601198b6263fc5c2ba5f8ce2
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-05-25 18:21:35 +0200

    Display messages in the BDV for longer before fading them.

--- src/main/java/org/mastodon/mamut/ProjectManager.java
+++ src/main/java/org/mastodon/mamut/ProjectManager.java
@@ -94,6 +94,7 @@ import org.scijava.ui.behaviour.util.RunnableAction;
 
 import bdv.spimdata.XmlIoSpimDataMinimal;
 import bdv.viewer.ViewerOptions;
+import bdv.viewer.animate.MessageOverlayAnimator;
 import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.SpimDataIOException;
@@ -532,7 +533,9 @@ public class ProjectManager
 		final KeymapManager keymapManager = windowManager.getKeymapManager();
 		final MamutPlugins plugins = windowManager.getPlugins();
 		final Actions globalAppActions = windowManager.getGlobalAppActions();
-		final ViewerOptions options = ViewerOptions.options().shareKeyPressedEvents( keyPressedManager );
+		final ViewerOptions options = ViewerOptions.options()
+				.shareKeyPressedEvents( keyPressedManager )
+				.msgOverlay( new MessageOverlayAnimator( 1500, 0.005, 0.02 ) );
 		final SharedBigDataViewerData sharedBdvData = new SharedBigDataViewerData(
 				spimDataXmlFilename,
 				spimData,
