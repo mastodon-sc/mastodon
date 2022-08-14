ac897034c830cdec8e5f42d1ece190bb927f7182
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-08-14 14:32:03 +0200

    Code style changes.

--- src/main/java/org/mastodon/views/trackscheme/display/BranchTrackSchemeEditLabelAction.java
+++ src/main/java/org/mastodon/views/trackscheme/display/BranchTrackSchemeEditLabelAction.java
@@ -1,6 +1,5 @@
 package org.mastodon.views.trackscheme.display;
 
-import net.imglib2.util.Cast;
 import org.mastodon.mamut.model.Model;
 import org.mastodon.mamut.model.ModelGraph;
 import org.mastodon.mamut.model.Spot;
@@ -13,19 +12,21 @@ import org.mastodon.views.trackscheme.TrackSchemeGraph;
 import org.mastodon.views.trackscheme.TrackSchemeVertex;
 import org.scijava.ui.behaviour.util.Actions;
 
+import net.imglib2.util.Cast;
+
 public class BranchTrackSchemeEditLabelAction extends EditFocusVertexLabelAction
 {
 	private static final String[] EDIT_FOCUS_LABEL_KEYS = new String[] { "ENTER" };
 
 	private final Model model;
 
-	private final TrackSchemeGraph<BranchSpot, BranchLink> trackSchemeGraph;
+	private final TrackSchemeGraph< BranchSpot, BranchLink > trackSchemeGraph;
 
 	private final ModelBranchGraph branchGraph;
 
 	private final ModelGraph graph;
 
-	public static void install( final Actions actions, final TrackSchemePanel panel, final FocusModel<TrackSchemeVertex, TrackSchemeEdge> focus, final Model model )
+	public static void install( final Actions actions, final TrackSchemePanel panel, final FocusModel< TrackSchemeVertex, TrackSchemeEdge > focus, final Model model )
 	{
 		final EditFocusVertexLabelAction editFocusVertexLabelAction = new BranchTrackSchemeEditLabelAction( focus, model, panel );
 		panel.getScreenTransform().listeners().add( editFocusVertexLabelAction );
@@ -33,27 +34,27 @@ public class BranchTrackSchemeEditLabelAction extends EditFocusVertexLabelAction
 		actions.namedAction( editFocusVertexLabelAction, EDIT_FOCUS_LABEL_KEYS );
 	}
 
-	protected BranchTrackSchemeEditLabelAction( FocusModel<TrackSchemeVertex, TrackSchemeEdge> focus, Model model, TrackSchemePanel panel )
+	protected BranchTrackSchemeEditLabelAction( final FocusModel< TrackSchemeVertex, TrackSchemeEdge > focus, final Model model, final TrackSchemePanel panel )
 	{
 		super( focus, model, panel );
 		this.model = model;
 		this.graph = model.getGraph();
 		this.branchGraph = model.getBranchGraph();
-		this.trackSchemeGraph = Cast.unchecked(panel.getGraph());
+		this.trackSchemeGraph = Cast.unchecked( panel.getGraph() );
 	}
 
 	@Override
-	protected void changeLabel( TrackSchemeVertex vertex, String label )
+	protected void changeLabel( final TrackSchemeVertex vertex, final String label )
 	{
 		vertex.setLabel( label );
-		BranchSpot branchSpot = trackSchemeGraph.getVertexMap().getLeft( vertex );
-		Spot vertexRef = graph.vertexRef();
+		final BranchSpot branchSpot = trackSchemeGraph.getVertexMap().getLeft( vertex );
+		final Spot vertexRef = graph.vertexRef();
 		Spot spot = branchGraph.getLinkedVertex( branchSpot, vertexRef );
-		while(spot.incomingEdges().size() == 1)
+		while ( spot.incomingEdges().size() == 1 )
 		{
 			spot.setLabel( label );
 			spot = spot.incomingEdges().iterator().next().getSource( spot );
-			if(spot.outgoingEdges().size() != 1)
+			if ( spot.outgoingEdges().size() != 1 )
 				break;
 		}
 		graph.releaseRef( spot );
