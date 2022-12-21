96af8c0239c92fc874b466c2369f4526f62fdbbd
Matthias Arzt <arzt@mpi-cbg.de>
2022-12-21 15:10:07 +0100

    Change MamutProjectIO to correctly use Windows file paths on Linux
    Windows, Linux & MacOS use different slashes in file paths. Windows
    uses backward slashes, Linux & MacOS use forward slashes.
    
    If a Mastodon project is saved on Windows, the project.xml will have
    a dataset path with backward slashes like: ..\dataset.xml.
    On Linux this path is not interpreted correctly. We need to manually
    replace the backward slashes with forward slashes.
    
    This commit moves the code that replaces the slashes into the class
    MamutProjectIO. This makes a lot of sense, as the backward slashes
    are introduced when reading the project.xml.

---
+++ src/test/java/org/mastodon/mamut/project/MamutProjectIOTest.java
@@ -0,0 +1,59 @@
+/*-
+ * #%L
+ * Mastodon
+ * %%
+ * Copyright (C) 2014 - 2022 Tobias Pietzsch, Jean-Yves Tinevez
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package org.mastodon.mamut.project;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class MamutProjectIOTest
+{
+	@Test
+	public void testProjectWithWindowsPath() throws IOException
+	{
+		testCanFindDatasetXmlFile( "/org/mastodon/mamut/examples/tiny-os/tiny-project-windows.mastodon" );
+	}
+
+	@Test
+	public void testPorjectWithLinuxPath() throws IOException
+	{
+		testCanFindDatasetXmlFile( "/org/mastodon/mamut/examples/tiny-os/tiny-project-linux.mastodon" );
+	}
+
+	private void testCanFindDatasetXmlFile( String resource ) throws IOException
+	{
+		final String projectFile = MamutProjectIOTest.class.getResource( resource ).getPath();
+		MamutProject project = new MamutProjectIO().load( projectFile );
+		File datasetXmlFile = project.getDatasetXmlFile();
+		assertTrue( datasetXmlFile.exists() );
+	}
+}
