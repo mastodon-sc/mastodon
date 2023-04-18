5f50fbe17728e042322f698a355effd80e8b1d0c
Matthias Arzt <arzt@mpi-cbg.de>
2023-04-18 14:43:07 +0200

    WIP: make the test fail to see if the CI reports the failure

--- src/test/java/org/mastodon/RunTest.java
+++ src/test/java/org/mastodon/RunTest.java
@@ -1,2 +1,14 @@
-package org.mastodon;public class RunTest {
+package org.mastodon;
+
+import org.junit.Test;
+
+/**
+ * This is just a dummy.
+ */
+public class RunTest
+{
+	@Test
+	public void test() {
+		throw new RuntimeException("This is just a dummy test, to see if github runs the unit tests."
+	}
 }
