3c3dc8b11b90ba67aca52739d1f3850c7a7d0b75
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-04-21 13:40:14 +0200

    Add getFeature method to FeatureComputerTestUtils

--- src/test/java/org/mastodon/mamut/feature/branch/FeatureComputerTestUtils.java
+++ src/test/java/org/mastodon/mamut/feature/branch/FeatureComputerTestUtils.java
@@ -1,5 +1,6 @@
 package org.mastodon.mamut.feature.branch;
 
+import net.imglib2.util.Cast;
 import org.mastodon.feature.Feature;
 import org.mastodon.feature.FeatureProjection;
 import org.mastodon.feature.FeatureProjectionKey;
@@ -9,32 +10,41 @@ import org.mastodon.mamut.feature.MamutFeatureComputerService;
 import org.mastodon.mamut.feature.branch.exampleGraph.AbstractExampleGraph;
 import org.mastodon.mamut.model.Spot;
 import org.mastodon.mamut.model.branch.BranchSpot;
-import org.mastodon.views.bdv.SharedBigDataViewerData;
 import org.scijava.Context;
 
 import javax.annotation.Nonnull;
 
 public class FeatureComputerTestUtils
 {
+
 	@Nonnull
-	public static FeatureProjection< BranchSpot > getBranchSpotFeatureProjection( @Nonnull Context context, @Nonnull AbstractExampleGraph exampleGraph, @Nonnull FeatureSpec< ? extends Feature< BranchSpot >, BranchSpot > spec, @Nonnull FeatureProjectionSpec featureProjectionSpec )
+	public static Feature< BranchSpot > getBranchSpotFeature( @Nonnull Context context,
+			@Nonnull AbstractExampleGraph exampleGraph, @Nonnull FeatureSpec< ? extends Feature< BranchSpot >, BranchSpot > spec )
 	{
 		final MamutFeatureComputerService featureComputerService = getMamutFeatureComputerService( context, exampleGraph );
-		Feature< BranchSpot > feature = ( Feature< BranchSpot > ) featureComputerService.compute( spec ).get( spec );
+		return Cast.unchecked( featureComputerService.compute( true, spec ).get( spec ) );
+	}
 
+	@Nonnull
+	public static Feature< Spot > getSpotFeature( @Nonnull Context context, @Nonnull AbstractExampleGraph exampleGraph,
+			@Nonnull FeatureSpec< ? extends Feature< Spot >, Spot > spec )
+	{
+		final MamutFeatureComputerService featureComputerService = getMamutFeatureComputerService( context, exampleGraph );
+		return Cast.unchecked( featureComputerService.compute( true, spec ).get( spec ) );
+	}
+
+	@Nonnull
+	public static FeatureProjection< BranchSpot > getBranchSpotFeatureProjection( @Nonnull Context context,
+			@Nonnull AbstractExampleGraph exampleGraph, @Nonnull FeatureSpec< ? extends Feature< BranchSpot >, BranchSpot > spec,
+			@Nonnull FeatureProjectionSpec featureProjectionSpec )
+	{
+		Feature< BranchSpot > feature = getBranchSpotFeature( context, exampleGraph, spec );
 		return feature.project( FeatureProjectionKey.key( featureProjectionSpec ) );
 	}
 
 	@Nonnull
-	public static FeatureProjection< Spot > getSpotFeatureProjection( @Nonnull Context context, @Nonnull AbstractExampleGraph exampleGraph, @Nonnull FeatureSpec< ? extends Feature< Spot >, Spot > spec, @Nonnull FeatureProjectionSpec featureProjectionSpec )
-	{
-		final MamutFeatureComputerService featureComputerService = getMamutFeatureComputerService( context, exampleGraph );
-		Feature< Spot > feature = ( Feature< Spot > ) featureComputerService.compute( spec ).get( spec );
-		return feature.project( FeatureProjectionKey.key( featureProjectionSpec ) );
-	}
-
-	@Nonnull
-	private static MamutFeatureComputerService getMamutFeatureComputerService( @Nonnull Context context, @Nonnull AbstractExampleGraph exampleGraph )
+	private static MamutFeatureComputerService getMamutFeatureComputerService( @Nonnull Context context,
+			@Nonnull AbstractExampleGraph exampleGraph )
 	{
 		final MamutFeatureComputerService featureComputerService = context.getService( MamutFeatureComputerService.class );
 		featureComputerService.setModel( exampleGraph.getModel() );
