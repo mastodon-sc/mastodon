53348e7c6877093e8d46c8dd56b2510a06864a08
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-04-26 14:57:12 +0200

    Add method to FeatureSerializerTestUtils to check for equality of FeatureProjections

--- src/test/java/org/mastodon/mamut/feature/FeatureSerializerTestUtils.java
+++ src/test/java/org/mastodon/mamut/feature/FeatureSerializerTestUtils.java
@@ -1,6 +1,7 @@
 package org.mastodon.mamut.feature;
 
 import org.mastodon.feature.Feature;
+import org.mastodon.feature.FeatureProjection;
 import org.mastodon.graph.io.RawGraphIO;
 import org.mastodon.mamut.model.Link;
 import org.mastodon.mamut.model.Model;
@@ -11,6 +12,7 @@ import org.scijava.Context;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Set;
 
 public class FeatureSerializerTestUtils
 {
@@ -36,9 +38,6 @@ public class FeatureSerializerTestUtils
 		reloadProject = new MamutProject( projectRoot, datasetXmlFile );
 		Model modelReloaded = new Model();
 
-		// Add all vertices of the given model to the reloaded model.
-		model.getGraph().vertices().forEach( spot -> modelReloaded.getGraph().addVertex( spot ) );
-
 		// reload the model from temporary files
 		try (MamutProject.ProjectReader reader = reloadProject.openForReading())
 		{
@@ -56,5 +55,43 @@ public class FeatureSerializerTestUtils
 			}
 		}
 		return modelReloaded.getFeatureModel().getFeature( feature.getSpec() );
+	}
+
+	/**
+	 * Checks, if the two features have the same spec and the same projection values for a given object.
+	 * <p>
+	 * The latter is done by comparing the values of the projections
+	 * of the two given features having the same key for the given object.
+	 *
+	 * @param feature1 first feature to compare
+	 * @param feature2 second feature to compare
+	 * @param object object to compare the projections on
+	 * @return {@code true} if the two features have the same spec and the same projection values for the given object,
+	 * 		   {@code false} otherwise.
+	 * @param <T> the type of the object
+	 */
+	public static < T > boolean checkFeatureProjectionEquality( Feature< T > feature1, Feature< T > feature2, T object )
+	{
+		if ( feature1 == null || feature2 == null )
+			return false;
+		if ( !feature1.getSpec().equals( feature2.getSpec() ) )
+			return false;
+
+		Set< FeatureProjection< T > > featureProjections1 = feature1.projections();
+		Set< FeatureProjection< T > > featureProjections2 = feature2.projections();
+		for ( FeatureProjection< T > featureProjection1 : featureProjections1 )
+		{
+			for ( FeatureProjection< T > featureProjection2 : featureProjections2 )
+			{
+				if ( featureProjection1.getKey().equals( featureProjection2.getKey() ) )
+				{
+					double value1 = featureProjection1.value( object );
+					double value2 = featureProjection2.value( object );
+					if ( value1 != value2 )
+						return false;
+				}
+			}
+		}
+		return true;
 	}
 }
