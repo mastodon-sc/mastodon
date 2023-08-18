de709b13de80bad1cce4b253bbd33a885c0382bd
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-08-18 17:06:54 +0200

    Interface for views that some special components.

---
+++ src/main/java/org/mastodon/ui/coloring/HasColorBarOverlay.java
@@ -0,0 +1,16 @@
+package org.mastodon.ui.coloring;
+
+/**
+ * Interface for classes that have {@link ColorBarOverlay}.
+ */
+public interface HasColorBarOverlay
+{
+
+	/**
+	 * Returns the {@link ColorBarOverlay} of this view.
+	 * 
+	 * @return the {@link ColorBarOverlay}.
+	 */
+	public ColorBarOverlay getColorBarOverlay();
+
+}
