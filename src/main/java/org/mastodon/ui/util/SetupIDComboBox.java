7d344a43ba5710c89880fc10b3ffd95eff4a5f9a
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-12-01 13:56:24 +0100

    Update license blurbs.

--- src/main/java/org/mastodon/ui/util/SetupIDComboBox.java
+++ src/main/java/org/mastodon/ui/util/SetupIDComboBox.java
@@ -1,8 +1,8 @@
 /*-
  * #%L
- * mastodon-tracking
+ * Mastodon
  * %%
- * Copyright (C) 2017 - 2022 Tobias Pietzsch, Jean-Yves Tinevez
+ * Copyright (C) 2014 - 2023 Tobias Pietzsch, Jean-Yves Tinevez
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
