81b26c3a3bcf53687c8d0da22a0a5a957f8f6162
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-06-11 14:34:05 +0200

    Builder pattern for the command finder.
    So that we can manage registering several actions, and registering
    the action to show the command finder to a separate one.

--- src/main/java/org/mastodon/ui/commandfinder/CommandFinder.java
+++ src/main/java/org/mastodon/ui/commandfinder/CommandFinder.java
@@ -1,11 +1,18 @@
 package org.mastodon.ui.commandfinder;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
 
+import javax.swing.Action;
 import javax.swing.ActionMap;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
@@ -13,15 +20,18 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 
 import org.mastodon.app.ui.CloseWindowActions;
-import org.mastodon.mamut.ProjectModel;
 import org.mastodon.ui.keymap.KeyConfigContexts;
 import org.mastodon.ui.keymap.KeyConfigScopes;
+import org.scijava.Context;
 import org.scijava.plugin.Plugin;
+import org.scijava.ui.behaviour.BehaviourMap;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
 import org.scijava.ui.behaviour.io.gui.Command;
 import org.scijava.ui.behaviour.io.gui.CommandDescriptionProvider;
 import org.scijava.ui.behaviour.io.gui.CommandDescriptions;
 import org.scijava.ui.behaviour.io.gui.CommandDescriptionsBuilder;
 import org.scijava.ui.behaviour.util.Actions;
+import org.scijava.ui.behaviour.util.Behaviours;
 
 import bdv.tools.ToggleDialogAction;
 
@@ -32,36 +42,33 @@ public class CommandFinder
 
 	private static final String[] SHOW_COMMAND_FINDER_KEYS = new String[] { "ctrl shift F" };
 
-	private final ProjectModel appModel;
-
 	private final JDialog dialog;
 
-	/**
-	 * Create an action that toggles the visibility of the command finder and
-	 * register it in the specified actions.
-	 *
-	 * @param actions
-	 *            Actions are added here.
-	 * @param keyConfigContexts
-	 */
-	public static void install( final Actions actions, final ProjectModel appModel, final JFrame parent, final String[] keyConfigContexts )
+	private final List< Actions > acs;
+
+	public static Builder build()
 	{
-		final CommandFinder cf = new CommandFinder( actions, appModel, parent, keyConfigContexts );
-		actions.namedAction( new ToggleDialogAction( SHOW_COMMAND_FINDER, cf.dialog ), SHOW_COMMAND_FINDER_KEYS );
+		return new Builder();
 	}
 
-	public CommandFinder( final Actions actions, final ProjectModel appModel, final JFrame parent, final String[] keyConfigContexts )
+	private CommandFinder( 
+			final List< Actions > acs, 
+			final InputTriggerConfig config, 
+			final Map< Command, String > commandMap, 
+			final String[] keyConfigContexts, 
+			final JFrame parent )
 	{
-		this.appModel = appModel;
+		this.acs = acs;
 		this.dialog = new JDialog( parent, "Command finder" );
 
-		final Map< Command, String > commandMap = buildCommandDescriptions( actions.getActionMap(), keyConfigContexts );
-		final CommandFinderPanel gui = new CommandFinderPanel( appModel.getKeymap().getConfig(), commandMap, actions );
+		final Consumer< String > runner = ( actionName ) -> run( actionName );
+		final CommandFinderPanel gui = new CommandFinderPanel( runner, commandMap, config );
 
 		dialog.getContentPane().add( gui );
 		dialog.pack();
 		dialog.setLocationByPlatform( true );
 		dialog.setLocationRelativeTo( null );
+		// Give focus to text after being made visible.
 		dialog.addComponentListener( new ComponentAdapter()
 		{
 			@Override
@@ -75,6 +82,26 @@ public class CommandFinder
 		final InputMap im = dialog.getRootPane().getInputMap( JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT );
 		final Actions actionsDialog = new Actions( im, am, null, keyConfigContexts );
 		CloseWindowActions.install( actionsDialog, dialog );
+	}
+
+	/**
+	 * Runs the 'first' action of behavior we can find with the specified name.
+	 * 
+	 * @param actionName
+	 *            the action or behavior name.
+	 */
+	private void run( final String actionName )
+	{
+		for ( final Actions ac : acs )
+		{
+			final Action action = ac.getActionMap().get( actionName );
+			if ( action != null )
+			{
+				final ActionEvent event = new ActionEvent( dialog, 0, actionName );
+				action.actionPerformed( event );
+				return;
+			}
+		}
 	}
 
 	/*
@@ -95,48 +122,154 @@ public class CommandFinder
 		}
 	}
 
-	/**
-	 * Discovers and build the command description map.
-	 * 
-	 * @param actionMap
-	 *            the command descriptions will be filtered to only include the
-	 *            commands present in the specified action map.
-	 * @param keyConfigContexts
-	 *            the command descriptions will be filtered to only include
-	 *            those with the context in the specified array.
-	 * @return the command descriptions map as a new map.
-	 */
-	private Map< Command, String > buildCommandDescriptions( final ActionMap actionMap, final String[] keyConfigContexts )
+	public static class Builder
 	{
-		final CommandDescriptionsBuilder builder = new CommandDescriptionsBuilder();
-		appModel.getContext().inject( builder );
-		builder.discoverProviders();
-		final CommandDescriptions cd = builder.build();
-		final Map< Command, String > map = cd.createCommandDescriptionsMap();
 
-		// Build list of commands in the action map.
-		final Object[] objs = actionMap.allKeys();
-		final String[] allKeys = new String[ objs.length ];
-		for ( int i = 0; i < objs.length; i++ )
-			allKeys[ i ] = ( String ) objs[ i ];
-		Arrays.sort( allKeys );
+		private final List< Actions > acs = new ArrayList<>();
 
-		// Copy and sort key contexts.
-		final String[] contexts = Arrays.copyOf( keyConfigContexts, keyConfigContexts.length );
-		Arrays.sort( contexts );
+		private final List< Behaviours > bhs = new ArrayList<>();
 
-		// Create new command map filtered by the command.
-		final Map< Command, String > filteredMap = new HashMap<>( allKeys.length );
-		for ( final Command command : map.keySet() )
+		private JFrame parent;
+
+		private final Set< String > keyConfigContexts = new HashSet<>();
+
+		private InputTriggerConfig config;
+
+		private Context context;
+
+		public Builder context( final Context context )
 		{
-			if ( Arrays.binarySearch( contexts, command.getContext() ) < 0 )
-				continue;
-
-			if ( Arrays.binarySearch( allKeys, command.getName() ) < 0 )
-				continue;
-
-			filteredMap.put( command, map.get( command ) );
+			this.context = context;
+			return this;
 		}
-		return filteredMap;
+
+		public Builder inputTriggerConfig( final InputTriggerConfig config )
+		{
+			this.config = config;
+			return this;
+		}
+
+		public Builder register( final Actions actions )
+		{
+			this.acs.add( actions );
+			return this;
+		}
+
+		public Builder parent( final JFrame parent )
+		{
+			this.parent = parent;
+			return this;
+		}
+
+		public Builder keyConfigContext( final String kcc )
+		{
+			this.keyConfigContexts.add( kcc );
+			return this;
+		}
+
+		public Builder keyConfigContexts( final String[] keyConfigContexts )
+		{
+			for ( final String kcc : keyConfigContexts )
+				this.keyConfigContexts.add( kcc );
+			return this;
+		}
+
+		public CommandFinder get()
+		{
+			if ( config == null )
+				throw new IllegalArgumentException( "The InputTriggerConfig cannot be null." );
+			if ( context == null )
+				throw new IllegalArgumentException( "The Context cannot be null." );
+
+			final Map< Command, String > commandDescriptions = buildCommandDescriptions();
+			final CommandFinder cf = new CommandFinder(
+					acs,
+					config,
+					commandDescriptions,
+					keyConfigContexts.toArray( new String[] {} ),
+					parent );
+			return cf;
+		}
+
+		public void installOn( final Actions installOn )
+		{
+			final CommandFinder cf = get();
+			installOn.namedAction( new ToggleDialogAction( SHOW_COMMAND_FINDER, cf.dialog ), SHOW_COMMAND_FINDER_KEYS );
+		}
+
+		/**
+		 * Discovers and build the command vs description map.
+		 * <p>
+		 * Important: in this implementation of a command finder, derived from
+		 * the keymap editor, that commands are listed and filtered from the
+		 * command descriptions. Which means that only the commands that have a
+		 * description, provided with a CommandDescriptionProvider, will appear
+		 * in the command list.
+		 * 
+		 * @param actionMap
+		 *            the command descriptions will be filtered to only include
+		 *            the commands present in the specified action map.
+		 * @param keyConfigContexts
+		 *            the command descriptions will be filtered to only include
+		 *            those with the context in the specified array.
+		 * @return the command descriptions map as a new map.
+		 */
+		private Map< Command, String > buildCommandDescriptions()
+		{
+			final CommandDescriptionsBuilder builder = new CommandDescriptionsBuilder();
+			context.inject( builder );
+			builder.discoverProviders();
+			final CommandDescriptions cd = builder.build();
+			final Map< Command, String > map = cd.createCommandDescriptionsMap();
+
+			// Copy and sort key contexts.
+			final String[] contexts = Arrays.copyOf( keyConfigContexts.toArray( new String[] {} ), keyConfigContexts.size() );
+			Arrays.sort( contexts );
+
+			// Create new command map, filtered.
+			final Map< Command, String > filteredMap = new HashMap<>();
+
+			/*
+			 * Build array of keys (command name) that are in one of the actions
+			 * or behaviors we were build with.
+			 */
+
+			// Loop over actions.
+			final Set< String > allKeySet = new HashSet<>();
+			for ( final Actions ac : acs )
+			{
+				// Build list of commands in the action map.
+				final ActionMap actionMap = ac.getActionMap();
+				final Object[] objs = actionMap.allKeys();
+				for ( int i = 0; i < objs.length; i++ )
+					allKeySet.add( ( String ) objs[ i ] );
+			}
+			// Loop over behaviors.
+			for ( final Behaviours bh : bhs )
+			{
+				final BehaviourMap behaviourMap = bh.getBehaviourMap();
+				allKeySet.addAll( behaviourMap.getAllBindings().keySet() );
+			}
+			final String[] allKeys = allKeySet.toArray( new String[] {} );
+			Arrays.sort( allKeys );
+
+			/*
+			 * Loop over all command descriptions, and retain those 1/ which
+			 * have a context that we manage 2/ which are in one of the action
+			 * map of behavior map that was provided to us.
+			 */
+			for ( final Command command : map.keySet() )
+			{
+				if ( Arrays.binarySearch( contexts, command.getContext() ) < 0 )
+					continue; // not in our contexts.
+
+				if ( Arrays.binarySearch( allKeys, command.getName() ) < 0 )
+					continue; // not a command we know.
+
+				// Add it.
+				filteredMap.put( command, map.get( command ) );
+			}
+			return filteredMap;
+		}
 	}
 }
