72fdc044ddee273fdb08789a7f4cdf728f4174a4
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2024-09-22 16:43:39 +0200

    Display the colorbar on BDV.
    It was never put on ?!?!

--- src/main/java/org/mastodon/mamut/views/bdv/MamutViewBdv.java
+++ src/main/java/org/mastodon/mamut/views/bdv/MamutViewBdv.java
@@ -189,6 +189,7 @@ public class MamutViewBdv
 						selectionModel,
 						coloring );
 
+		viewer.getDisplay().overlays().add( colorBarOverlay );
 		viewer.getDisplay().overlays().add( tracksOverlay );
 		viewer.renderTransformListeners().add( tracksOverlay );
 		viewer.timePointListeners().add( tracksOverlay );
