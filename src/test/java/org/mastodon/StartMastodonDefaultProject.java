2306611d9c74dd9a0f706df07c11810f01bc7c09
Jean-Yves Tinevez <tinevez@pasteur.fr>
2025-01-16 17:05:03 +0100

    Tweak a test drive.

--- src/test/java/org/mastodon/StartMastodonDefaultProject.java
+++ src/test/java/org/mastodon/StartMastodonDefaultProject.java
@@ -33,8 +33,9 @@ public class StartMastodonDefaultProject
 
 	public static void main( final String[] args )
 	{
-		final String projectPath = "samples/MaMuT_Parhyale_small.mastodon";
+//		final String projectPath = "samples/MaMuT_Parhyale_small.mastodon";
 //		final String projectPath = "/Users/tinevez/Google Drive/Mastodon/Datasets/Remote/ParhyaleHawaiensis/MaMuT_Parhyale_demo-mamut.mastodon";
+		final String projectPath = "samples/drosophila_crop_LONG_NAME_LONGLONGLONG_SUPERLONG.mastodon";
 //		final String projectPath = "samples/drosophila_crop.mastodon";
 		StartMastodonOnProject.launch( projectPath );
 	}
