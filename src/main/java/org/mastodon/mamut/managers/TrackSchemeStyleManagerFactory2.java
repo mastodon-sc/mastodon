927ca8b4e25f06b031528aea6d319f01e12ecfba
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2025-09-05 10:15:25 +0200

    Fix TrackSchemeStyleManagerFactory2
    Was not properly discoverable.

--- src/main/java/org/mastodon/mamut/managers/TrackSchemeStyleManagerFactory2.java
+++ src/main/java/org/mastodon/mamut/managers/TrackSchemeStyleManagerFactory2.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -36,12 +36,12 @@ import org.scijava.plugin.Plugin;
 
 import bdv.ui.settings.SettingsPage;
 
-@Plugin( type = StyleManagerFactory.class, priority = Priority.NORMAL - 1 )
+@Plugin( type = StyleManagerFactory2.class, priority = Priority.NORMAL - 1 )
 public class TrackSchemeStyleManagerFactory2 implements StyleManagerFactory2< TrackSchemeStyleManager >
 {
 
 	@Override
-	public TrackSchemeStyleManager create( final UIModel uiModel )
+	public TrackSchemeStyleManager create( final UIModel< ? > uiModel )
 	{
 		return new TrackSchemeStyleManager();
 	}
