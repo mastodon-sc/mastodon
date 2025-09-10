6ec88229dd55a9a9673ead00535a8e6c7e60ea44
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2025-09-10 13:35:20 +0200

    Add TODO

--- src/main/java/org/mastodon/app/plugin/MastodonAppPluginModel.java
+++ src/main/java/org/mastodon/app/plugin/MastodonAppPluginModel.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -32,6 +32,8 @@ package org.mastodon.app.plugin;
  * Interface for model classes that will be used to pass information about the
  * app to plugins of the app. An app plugin model contains for instance a
  * reference to the app model and to the window manager of the app.
+ *
+ * TODO Remove. This can be replaced by AppModel
  */
 public interface MastodonAppPluginModel
 {}
