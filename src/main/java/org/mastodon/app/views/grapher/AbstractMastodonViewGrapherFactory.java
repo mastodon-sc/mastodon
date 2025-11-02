2878636fd4807bbf7299d36fbf8d970ac9f8eb77
Jean-Yves Tinevez <tinevez@pasteur.fr>
2025-11-02 13:09:44 -0600

    Base class for grapher view factories.

---
+++ src/main/java/org/mastodon/app/views/grapher/AbstractMastodonViewGrapherFactory.java
@@ -0,0 +1,92 @@
+package org.mastodon.app.views.grapher;
+
+import java.util.Map;
+
+import org.mastodon.app.AppModel;
+import org.mastodon.app.views.AbstractMastodonViewFactory;
+import org.mastodon.app.views.MastodonViewFactory;
+import org.mastodon.graph.ListenableReadOnlyGraph;
+import org.mastodon.ui.coloring.ColorBarOverlay.Position;
+import org.mastodon.views.grapher.display.DataDisplayFrame;
+import org.mastodon.views.trackscheme.ScreenTransform;
+
+/**
+ * Base class for view factories that create Grapher views. This abstract class
+ * is specific to a view (Grapher) and to a model type.
+ * <p>
+ * The factory has still a generic type for the view it creates, that must
+ * extends {@link MastodonViewGrapher}. This is required to have app-specific
+ * factories, discoverable separately.
+ * <p>
+ * The GUI state is specified as a map of strings to objects. The accepted key
+ * and value types are:
+ * <ul>
+ * <li><code>'FramePosition'</code> &rarr; an <code>int[]</code> array of 4
+ * elements: x, y, width and height.
+ * <li><code>'LockGroupId'</code> &rarr; an integer that specifies the lock
+ * group id.
+ * <li><code>'SettingsPanelVisible'</code> &rarr; a boolean that specifies
+ * whether the settings panel is visible on this view.
+ * <li><code>'TrackSchemeTransform'</code> &rarr; a {@link ScreenTransform} that
+ * defines the starting view zone in TrackScheme.
+ * <li><code>'NoColoring'</code> &rarr; a boolean; if <code>true</code>, the
+ * feature or tag coloring will be ignored.
+ * <li><code>'TagSet'</code> &rarr; a string specifying the name of the tag-set
+ * to use for coloring. If not <code>null</code>, the coloring will be done
+ * using the tag-set.
+ * <li><code>'FeatureColorMode'</code> &rarr; a @link String specifying the name
+ * of the feature color mode to use for coloring. If not <code>null</code>, the
+ * coloring will be done using the feature color mode.
+ * <li><code>'ColorbarVisible'</code> &rarr; a boolean specifying whether the
+ * colorbar is visible for tag-set and feature-based coloring.
+ * <li><code>'ColorbarPosition'</code> &rarr; a {@link Position} specifying the
+ * position of the colorbar.
+ * </ul>
+ *
+ * @author Jean-Yves Tinevez
+ *
+ */
+public abstract class AbstractMastodonViewGrapherFactory<
+		T extends MastodonViewGrapher< ?, ?, ?, ? >,
+		G extends ListenableReadOnlyGraph< ?, ? >,
+		AM extends AppModel< AM, ?, G, ?, ? > >
+		extends AbstractMastodonViewFactory< T, AM >
+		implements MastodonViewFactory< T, AM >
+{
+
+	@Override
+	public String getCommandDescription()
+	{
+		return "Open a new Grapher view.";
+	}
+
+	@Override
+	public String getCommandMenuText()
+	{
+		return "New Grapher";
+	}
+
+	@Override
+	public Map< String, Object > getGuiState( final T view )
+	{
+		final Map< String, Object > guiState = super.getGuiState( view );
+		final DataDisplayFrame< ?, ? > frame = ( DataDisplayFrame< ?, ? > ) view.getFrame();
+		GrapherGuiState.writeGuiState(
+				frame.getDataDisplayPanel().getScreenTransform(),
+				frame.getVertexSidePanel().getGraphConfig(),
+				guiState );
+		return guiState;
+	}
+
+	@Override
+	public void restoreGuiState( final T view, final Map< String, Object > guiState )
+	{
+		super.restoreGuiState( view, guiState );
+		final DataDisplayFrame< ?, ? > frame = ( DataDisplayFrame< ?, ? > ) view.getFrame();
+		GrapherGuiState.loadGuiState(
+				guiState,
+				frame.getDataDisplayPanel().getScreenTransform(),
+				frame.getVertexSidePanel(),
+				frame );
+	}
+}
---
+++ src/main/java/org/mastodon/app/views/grapher/GrapherGuiState.java
@@ -0,0 +1,202 @@
+/*-
+ * #%L
+ * Mastodon
+ * %%
+ * Copyright (C) 2014 - 2025 Tobias Pietzsch, Jean-Yves Tinevez
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package org.mastodon.app.views.grapher;
+
+import java.util.Map;
+
+import org.mastodon.feature.FeatureProjectionSpec;
+import org.mastodon.feature.FeatureSpec;
+import org.mastodon.mamut.feature.SpotFrameFeature;
+import org.mastodon.mamut.feature.SpotQuickMeanIntensityFeature;
+import org.mastodon.views.grapher.datagraph.ScreenTransform;
+import org.mastodon.views.grapher.display.FeatureGraphConfig;
+import org.mastodon.views.grapher.display.FeatureSpecPair;
+import org.mastodon.views.grapher.display.GrapherSidePanel;
+import org.mastodon.views.grapher.display.Plotable;
+import org.mastodon.views.grapher.display.ScreenTransformState;
+
+public class GrapherGuiState
+{
+	private GrapherGuiState()
+	{
+		// Prevent instantiation.
+	}
+
+	/**
+	 * Key for the transform in a Grapher view. Value is a Grapher ScreenTransform instance.
+	 */
+	public static final String GRAPHER_TRANSFORM_KEY = "GrapherTransform";
+
+	/**
+	 * Key for the selected feature for the X axis in a Grapher view. Value is a Boolean.
+	 */
+	public static final String GRAPHER_X_AXIS_FEATURE_IS_EDGE_KEY = "GrapherXAxisFeatureIsEdge";
+
+	/**
+	 * Key for the selected feature spec for the X axis in a Grapher view. Value is a string.
+	 */
+	public static final String GRAPHER_X_AXIS_FEATURE_SPEC_KEY = "GrapherXAxisFeatureSpec";
+
+	/**
+	 * Key for the selected projection for the X axis in a Grapher view. Value is a string.
+	 */
+	public static final String GRAPHER_X_AXIS_FEATURE_PROJECTION_KEY = "GrapherXAxisFeatureProjection";
+
+	/**
+	 * Key for the selected incoming edge for the X axis in a Grapher view. Value is a boolean.
+	 */
+	public static final String GRAPHER_X_AXIS_INCOMING_EDGE_KEY = "GrapherXAxisFeatureIsIncomingEdge";
+
+	/**
+	 * Key for the selected feature for the Y axis in a Grapher view. Value is a Boolean.
+	 */
+	public static final String GRAPHER_Y_AXIS_FEATURE_IS_EDGE_KEY = "GrapherYAxisFeatureIsEdge";
+
+	/**
+	 * Key for the selected feature spec for the Y axis in a Grapher view. Value is a string.
+	 */
+	public static final String GRAPHER_Y_AXIS_FEATURE_SPEC_KEY = "GrapherYAxisFeatureSpec";
+
+	/**
+	 * Key for the selected projection for the Y axis in a Grapher view. Value is a string.
+	 */
+	public static final String GRAPHER_Y_AXIS_FEATURE_PROJECTION_KEY = "GrapherYAxisFeatureProjection";
+
+	/**
+	 * Key for the selected incoming edge for the Y axis in a Grapher view. Value is a boolean.
+	 */
+	public static final String GRAPHER_Y_AXIS_INCOMING_EDGE_KEY = "GrapherYAxisFeatureIsIncomingEdge";
+
+	/**
+	 * Key for whether show edges is checked in a Grapher view. Value is a boolean.
+	 */
+	public static final String GRAPHER_SHOW_EDGES_KEY = "GrapherShowEdges";
+
+	public static void writeGuiState(
+			final ScreenTransformState screenTransformState,
+			final FeatureGraphConfig config,
+			final Map< String, Object > guiState )
+	{
+		// Transform.
+		final ScreenTransform transform = screenTransformState.get();
+		guiState.put( GRAPHER_TRANSFORM_KEY, transform );
+		// Feature graph config.
+		writeFeatureGraphConfig( config, guiState );
+	}
+
+	public static void loadGuiState(
+			final Map< String, Object > guiState,
+			final ScreenTransformState screenTransformState,
+			final GrapherSidePanel< ?, ? > sidePanel,
+			final Plotable plotable )
+	{
+		// Read Screen Transform.
+		final ScreenTransform screenTransform = ( ScreenTransform ) guiState.get( GRAPHER_TRANSFORM_KEY );
+		if ( null != screenTransform )
+			screenTransformState.set( screenTransform );
+
+		// Read Feature graph config.
+		final FeatureGraphConfig defaultConfig = getDefaultFeatureGraphConfig();
+		final FeatureGraphConfig config = loadFeatureGraphConfig( sidePanel, guiState, defaultConfig );
+		sidePanel.setGraphConfig( config );
+
+		// Plot with loaded transform and config.
+		if ( plotable != null )
+			plotable.plot( screenTransform );
+	}
+
+	/**
+	 * If they are available, set some sensible defaults for the feature.
+	 * 
+	 * @return a default feature graph config.
+	 */
+	public static FeatureGraphConfig getDefaultFeatureGraphConfig()
+	{
+		final FeatureSpecPair spvx = new FeatureSpecPair( SpotFrameFeature.SPEC,
+				SpotFrameFeature.SPEC.getProjectionSpecs().iterator().next(), false, false );
+		final FeatureSpecPair spvy = new FeatureSpecPair( SpotQuickMeanIntensityFeature.SPEC,
+				SpotQuickMeanIntensityFeature.PROJECTION_SPEC, 0, false, false );
+		return new FeatureGraphConfig( spvx, spvy, FeatureGraphConfig.GraphDataItemsSource.CONTEXT, false );
+	}
+
+	private static void writeFeatureGraphConfig(
+			final FeatureGraphConfig config,
+			final Map< String, Object > guiState )
+	{
+		// X-axis feature.
+		final FeatureSpecPair featureSpecPairX = config.getXFeature();
+		guiState.put( GRAPHER_X_AXIS_FEATURE_IS_EDGE_KEY, featureSpecPairX.isEdgeFeature() );
+		guiState.put( GRAPHER_X_AXIS_FEATURE_SPEC_KEY, featureSpecPairX.getFeatureSpecKey() );
+		guiState.put( GRAPHER_X_AXIS_FEATURE_PROJECTION_KEY, featureSpecPairX.projectionKey().toString() );
+		guiState.put( GRAPHER_X_AXIS_INCOMING_EDGE_KEY, featureSpecPairX.isIncomingEdge() );
+		// Y-axis feature.
+		final FeatureSpecPair featureSpecPairY = config.getYFeature();
+		guiState.put( GRAPHER_Y_AXIS_FEATURE_IS_EDGE_KEY, featureSpecPairY.isEdgeFeature() );
+		guiState.put( GRAPHER_Y_AXIS_FEATURE_SPEC_KEY, featureSpecPairY.getFeatureSpecKey() );
+		guiState.put( GRAPHER_Y_AXIS_FEATURE_PROJECTION_KEY, featureSpecPairY.projectionKey().toString() );
+		guiState.put( GRAPHER_Y_AXIS_INCOMING_EDGE_KEY, featureSpecPairY.isIncomingEdge() );
+		// Show edges.
+		guiState.put( GRAPHER_SHOW_EDGES_KEY, config.drawConnected() );
+	}
+
+	public static FeatureGraphConfig loadFeatureGraphConfig(
+			final GrapherSidePanel< ?, ? > sidePanel,
+			final Map< String, Object > guiState,
+			final FeatureGraphConfig defaultConfig
+			)
+	{
+		final FeatureSpecPair featureSpecPairX = loadFeatureSpecPair( guiState, sidePanel, GRAPHER_X_AXIS_FEATURE_IS_EDGE_KEY,
+				GRAPHER_X_AXIS_FEATURE_SPEC_KEY, GRAPHER_X_AXIS_FEATURE_PROJECTION_KEY, GRAPHER_X_AXIS_INCOMING_EDGE_KEY );
+		final FeatureSpecPair featureSpecPairY = loadFeatureSpecPair( guiState, sidePanel, GRAPHER_Y_AXIS_FEATURE_IS_EDGE_KEY,
+				GRAPHER_Y_AXIS_FEATURE_SPEC_KEY, GRAPHER_Y_AXIS_FEATURE_PROJECTION_KEY, GRAPHER_Y_AXIS_INCOMING_EDGE_KEY );
+		final Boolean showEdges = ( Boolean ) guiState.get( GRAPHER_SHOW_EDGES_KEY );
+		if ( featureSpecPairX == null || featureSpecPairY == null || showEdges == null )
+			return defaultConfig;
+		return new FeatureGraphConfig( featureSpecPairX, featureSpecPairY, FeatureGraphConfig.GraphDataItemsSource.CONTEXT, showEdges );
+	}
+
+	private static FeatureSpecPair loadFeatureSpecPair(
+			final Map< String, Object > guiState,
+			final GrapherSidePanel< ?, ? > sidePanel,
+			final String edgeKey,
+			final String featureSpecKey,
+			final String projectionKey,
+			final String incomingEdgeKey )
+	{
+		final Boolean isEdgeFeature = ( Boolean ) guiState.get( edgeKey );
+		final String yFeatureSpecKey = ( String ) guiState.get( featureSpecKey );
+		final String yProjectionKey = ( String ) guiState.get( projectionKey );
+		final Boolean incomingEdge = ( Boolean ) guiState.get( incomingEdgeKey );
+		final FeatureSpec< ?, ? > featureSpec = sidePanel.getFeatureSpec( yFeatureSpecKey );
+		final FeatureProjectionSpec projectionSpec = sidePanel.getFeatureProjectionSpec( yProjectionKey );
+		if ( isEdgeFeature == null || featureSpec == null || projectionSpec == null || incomingEdge == null )
+			return null;
+		return new FeatureSpecPair( featureSpec, projectionSpec, isEdgeFeature, incomingEdge );
+	}
+}
